ad52b3d900d7185dd8a7056f70a942affd374d11
Kenneth Russel <kbrussel@alum.mit.edu>
2003-07-14 10:01:41 +0000

    Updated jogl build documentation for Ant build. Deleted now-obsolete Makefiles and redundant gl.c and similar files from platform-specific stub_includes directories; the latter have been moved to stub_includes/common.
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/trunk@22 232f8b59-042b-4e1e-8c03-345bb8c30851

--- make/stub_includes/macosx/glu.c
+++
@@ -1,2 +0,0 @@
-#include <GL/glu.h>
-
--- make/stub_includes/x11/gl.c
+++
@@ -1,7 +0,0 @@
-#define GLAPI
-
-// Define GL_GLEXT_PROTOTYPES so that the OpenGL extension prototypes in
-// "glext.h" are parsed.
-#define GL_GLEXT_PROTOTYPES
-
-#include <GL/gl.h>
