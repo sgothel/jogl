46068b36e23b85e0db0ed22a6391aa6661ba8e43
Kenneth Russel <kbrussel@alum.mit.edu>
2003-10-03 05:51:26 +0000

    Added missing implementation of dispatch_glMapBufferARB on Linux after David Yazel pointed out problems with glBufferDataARB on Linux on JOGL forums.
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/trunk@65 232f8b59-042b-4e1e-8c03-345bb8c30851

--- make/gl-impl-CustomCCode.c
+++ make/gl-impl-CustomCCode.c
@@ -12,3 +12,18 @@ Java_net_java_games_jogl_impl_windows_WindowsGLImpl_dispatch_1glMapBufferARB(JNI
   _res = (* ptr_glMapBufferARB) ((GLenum) target, (GLenum) access);
   return (jlong) (intptr_t) _res;
 }
+
+/*   Java->C glue code:
+ *   Java package: net.java.games.jogl.impl.x11.X11GLImpl
+ *    Java method: long dispatch_glMapBufferARB(int target, int access)
+ *     C function: LPVOID glMapBufferARB(GLenum target, GLenum access);
+ */
+JNIEXPORT jlong JNICALL 
+Java_net_java_games_jogl_impl_x11_X11GLImpl_dispatch_1glMapBufferARB(JNIEnv *env, jobject _unused, jint target, jint access, jlong glProcAddress) {
+  PFNGLMAPBUFFERARBPROC ptr_glMapBufferARB;
+  LPVOID _res;
+  ptr_glMapBufferARB = (PFNGLMAPBUFFERARBPROC) (intptr_t) glProcAddress;
+  assert(ptr_glMapBufferARB != NULL);
+  _res = (* ptr_glMapBufferARB) ((GLenum) target, (GLenum) access);
+  return (jlong) (intptr_t) _res;
+}
