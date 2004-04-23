3b48c5696d5372fa48ebb2b3c0246a7980924c5d
Kenneth Russel <kbrussel@alum.mit.edu>
2004-04-23 00:11:48 +0000

    Updated webstart jogl.jar to 1.1b02 and bumped version number to 1.1b03
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/trunk@120 232f8b59-042b-4e1e-8c03-345bb8c30851

--- src/net/java/games/jogl/Version.java
+++ src/net/java/games/jogl/Version.java
@@ -89,7 +89,7 @@ public final class Version {
   /**
    * Version string of this build.
    */
-  private static final String version = "1.1.0-b02";
+  private static final String version = "1.1.0-b03";
 
   /**
    * Returns the verison string and build number of
