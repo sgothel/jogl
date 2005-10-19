03a9d177b0e680ebe059c79422ead584df3518a3
Kenneth Russel <kbrussel@alum.mit.edu>
2005-10-19 16:40:38 +0000

    Fixed Issue 178: Need to add extra sizeof consts to BufferUtils
    Added SIZEOF_BYTE and reordered existing constants.
    
    
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/branches/JSR-231@397 232f8b59-042b-4e1e-8c03-345bb8c30851

--- src/classes/com/sun/opengl/utils/BufferUtils.java
+++ src/classes/com/sun/opengl/utils/BufferUtils.java
@@ -45,11 +45,12 @@ import java.util.*;
 /** Utility routines for dealing with direct buffers. */
 
 public class BufferUtils {
-  public static final int SIZEOF_DOUBLE = 8;
-  public static final int SIZEOF_FLOAT = 4;
-  public static final int SIZEOF_INT = 4;
-  public static final int SIZEOF_LONG = 8;
+  public static final int SIZEOF_BYTE = 1;
   public static final int SIZEOF_SHORT = 2;
+  public static final int SIZEOF_INT = 4;
+  public static final int SIZEOF_FLOAT = 4;
+  public static final int SIZEOF_LONG = 8;
+  public static final int SIZEOF_DOUBLE = 8;
 
   //----------------------------------------------------------------------
   // Allocation routines
