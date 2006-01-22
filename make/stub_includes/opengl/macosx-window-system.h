ae26492c30051eb7c6c883b3c6397390f91afcae
Kenneth Russel <kbrussel@alum.mit.edu>
2006-01-22 00:26:44 +0000

    Cleaned up how declarations for Mac OS X window system wrapper functions are specified. Now have their own header (which unfortunately could not be placed in the macosx stub_includes subdirectory due to build problems) in make/stub_includes/opengl/macosx-window-system.h. Deleted manual declarations for these functions from cgl-macosx.cfg.
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/trunk@556 232f8b59-042b-4e1e-8c03-345bb8c30851

---
+++ make/stub_includes/opengl/macosx-window-system.h
@@ -0,0 +1,46 @@
+/* C routines encapsulating small amounts of Objective C code to allow
+   nsContext creation and manipulation to occur from Java
+
+   It's unfortunate this couldn't be placed in the macosx
+   stub_includes directory, but due to the presence of the jni.h stub
+   headers in that directory, if that is in the include path during
+   compilation then the build fails.
+*/
+
+typedef int Bool;
+
+void* createContext(void* shareContext, void* nsView,
+                    int doubleBuffer,
+                    int stereo,
+                    int redBits,
+                    int greenBits,
+                    int blueBits,
+                    int alphaBits,
+                    int depthBits,
+                    int stencilBits,
+                    int accumRedBits,
+                    int accumGreenBits,
+                    int accumBlueBits,
+                    int accumAlphaBits,
+                    int sampleBuffers,
+                    int numSamples,
+                    int pbuffer,
+                    int floatingPoint,
+                    int* viewNotReady);
+Bool  makeCurrentContext(void* nsContext, void* nsView);
+Bool  clearCurrentContext(void* nsContext, void* nsView);
+Bool  deleteContext(void* nsContext, void* nsView);
+Bool  flushBuffer(void* nsContext, void* nsView);
+void  updateContext(void* nsContext, void* nsView);
+
+void* updateContextRegister(void* nsContext, void* nsView);
+void  updateContextUnregister(void* nsContext, void* nsView, void* updater);
+
+void* createPBuffer(int renderTarget, int internalFormat, int width, int height);
+Bool destroyPBuffer(void* nsContext, void* pBuffer);
+void setContextPBuffer(void* nsContext, void* pBuffer);
+void setContextTextureImageToPBuffer(void* nsContext, void* pBuffer, int colorBuffer);
+
+void* getProcAddress(const char *procName);
+
+void setSwapInterval(void* nsContext, int interval);
