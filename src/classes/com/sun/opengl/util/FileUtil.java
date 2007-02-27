852c2ae4d31ccab56dce74b431fcd1df02be309c
Kenneth Russel <kbrussel@alum.mit.edu>
2007-02-27 22:31:39 +0000

    Fixed Issue 281: FileUtil doc enhancement
    Improved documentation as suggested.
    
    
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/trunk@1159 232f8b59-042b-4e1e-8c03-345bb8c30851

--- src/classes/com/sun/opengl/util/FileUtil.java
+++ src/classes/com/sun/opengl/util/FileUtil.java
@@ -47,10 +47,14 @@ public class FileUtil {
   private FileUtil() {}
 
   /**
-   * Returns the suffix of the given file name for identifying the
-   * file to the configured TextureProviders.
+   * Returns the lowercase suffix of the given file name (the text
+   * after the last '.' in the file name). Returns null if the file
+   * name has no suffix. Only operates on the given file name;
+   * performs no I/O operations.
    *
    * @param file name of the file
+   * @return lowercase suffix of the file name
+   * @throws NullPointerException if file is null
    */
 
   public static String getFileSuffix(File file) {
@@ -58,10 +62,14 @@ public class FileUtil {
   }
 
   /**
-   * Returns the suffix of the given file name for identifying the
-   * file to the configured TextureProviders.
+   * Returns the lowercase suffix of the given file name (the text
+   * after the last '.' in the file name). Returns null if the file
+   * name has no suffix. Only operates on the given file name;
+   * performs no I/O operations.
    *
    * @param filename name of the file
+   * @return lowercase suffix of the file name
+   * @throws NullPointerException if filename is null
    */
   public static String getFileSuffix(String filename) {
     int lastDot = filename.lastIndexOf('.');
