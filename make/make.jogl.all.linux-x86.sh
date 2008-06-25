eeeac3ac68f01056f60cf2944f5eeb3f0f950f6e
Sven Gothel <sgothel@jausoft.com>
2008-06-25 05:51:59 +0000

    build script props to force linux-x86 on linux-amd64
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/branches/JOGL_2_SANDBOX@1681 232f8b59-042b-4e1e-8c03-345bb8c30851

---
+++ make/make.jogl.all.linux-x86.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+. ../../setenv-build-jogl.sh
+
+ant -v \
+    -Dgluegen.cpptasks.detected.os=true \
+    -DisUnix=true \
+    -DisLinux=true \
+    -DisLinuxX86=true \
+    -DisX11=true \
+    $* 2>&1 | tee make.jogl.all.linux-x86.log
