8b08ffff07711d42fa6df65990bd3218efaa308d
Kenneth Russel <kbrussel@alum.mit.edu>
2008-06-26 02:25:17 +0000

    Implemented linker configurations for Solaris (32-bit currently -- sparcv9 and amd64 not yet tested). Deleted stub XF86VidMode entry points from glx-CustomCCode.c. ES1 and ES2 linker configurations on all platforms were unneeded because all entry points (EGL, GLES1, GLES2) are dynamically looked up; removed these. RedSquare demo is now working on Solaris/x86.
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/branches/JOGL_2_SANDBOX@1692 232f8b59-042b-4e1e-8c03-345bb8c30851

--- make/glx-CustomCCode.c
+++ make/glx-CustomCCode.c
@@ -7,41 +7,6 @@
 #include <dlfcn.h>
 #undef __USE_GNU
 
-/* Current versions of Solaris don't expose the XF86 extensions,
-   although with the recent transition to Xorg this will probably
-   happen in an upcoming release */
-#if !defined(__sun) && !defined(_HPUX)
-#include <X11/extensions/xf86vmode.h>
-#else
-/* Need to provide stubs for these */
-Bool XF86VidModeGetGammaRampSize(
-    Display *display,
-    int screen,
-    int* size)
-{
-  return False;
-}
-
-Bool XF86VidModeGetGammaRamp(
-    Display *display,
-    int screen,
-    int size,
-    unsigned short *red_array,
-    unsigned short *green_array,
-    unsigned short *blue_array) {
-  return False;
-}
-Bool XF86VidModeSetGammaRamp(
-    Display *display,
-    int screen,
-    int size,
-    unsigned short *red_array,
-    unsigned short *green_array,
-    unsigned short *blue_array) {
-  return False;
-}
-#endif
-
 /* HP-UX doesn't define RTLD_DEFAULT. */
 #if defined(_HPUX) && !defined(RTLD_DEFAULT)
 #define RTLD_DEFAULT NULL
