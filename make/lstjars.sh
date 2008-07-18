2d1e844b7e621cb7bce6576d559f297dc842f6ed
Sven Gothel <sgothel@jausoft.com>
2008-07-18 13:09:09 +0000

    tessellator now part of jogl.core
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/branches/JOGL_2_SANDBOX@1726 232f8b59-042b-4e1e-8c03-345bb8c30851

--- make/lstjars.sh
+++ make/lstjars.sh
@@ -1,6 +1,27 @@
 #! /bin/sh
 
-cd ../build
+THISDIR=$(pwd)
+
+function report() {
+    #ls -1 -s --block-size=1024 $*
+    #ls -1 -s --block-size=1024 $* | awk ' BEGIN { sum=0 ; } { sum=sum+$1; } END { printf("%d Total\n", sum); }'
+    du -ksc $*
+}
+
+cd $THISDIR/../build/obj
+mkdir -p tmp
+cd tmp
+
+rm -f *.so *.so.gz
+cp ../*.so .
+for i in *.so ; do
+    gzip $i
+done
+echo Native Libraries
+report *.gz
+echo
+
+cd $THISDIR/../build
 
 rm -f *.lst
 
@@ -25,21 +46,25 @@ echo
 diff -Nur jogl.allparts.lst jogl.all.lst
 
 echo JOGL ES1 NEWT CORE
-du -ksc jogl.core.jar jogl.egl.jar jogl.gles1.jar newt.jar
+report jogl.core.jar jogl.egl.jar jogl.gles1.jar newt.jar obj/tmp/libjogl_es1.so.gz obj/tmp/libnewt.so.gz
 echo
 
 echo JOGL ES2 NEWT CORE
-du -ksc jogl.core.jar jogl.egl.jar jogl.gles2.jar newt.jar
+report jogl.core.jar jogl.egl.jar jogl.gles2.jar newt.jar obj/tmp/libjogl_es2.so.gz obj/tmp/libnewt.so.gz
 echo
 
-echo JOGL GL2 OSWIN CORE no AWT
-du -ksc jogl.core.jar jogl.oswin.jar jogl.gl2.jar newt.jar
+echo JOGL GL2 NEWT CORE no AWT
+report jogl.core.jar jogl.oswin.jar jogl.gl2.jar newt.jar obj/tmp/libjogl_gl2.so.gz obj/tmp/libnewt.so.gz
+echo
+
+echo JOGL GL2 OSWIN no AWT
+report jogl.core.jar jogl.oswin.jar jogl.gl2.jar obj/tmp/libjogl_gl2.so.gz
 echo
 
 echo JOGL GL2 OSWIN with AWT
-du -ksc jogl.core.jar jogl.oswin.jar jogl.gl2.jar jogl.awt.jar 
+report jogl.core.jar jogl.oswin.jar jogl.gl2.jar jogl.awt.jar obj/tmp/libjogl_gl2.so.gz obj/tmp/libjogl_awt.so.gz
 echo
 
 echo JOGL EVERYTHING
-du -ksc jogl.all.jar
+report jogl.all.jar
 echo
