cbf1c157c25d064501104b269ed7e863c8c2bb8e
Sven Gothel <sgothel@jausoft.com>
2008-08-18 18:18:58 +0000

    Fixing repo (shader directory)
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/branches/JOGL_2_SANDBOX@1760 232f8b59-042b-4e1e-8c03-345bb8c30851

---
+++ src/classes/com/sun/opengl/impl/glsl/fixed/shader/mgl_const.glsl
@@ -0,0 +1,10 @@
+
+#ifndef mgl_const_glsl
+#define mgl_const_glsl
+
+#include es_precision.glsl
+
+const   LOWP int     MAX_TEXTURE_UNITS = 8; // <=gl_MaxTextureImageUnits 
+const   LOWP int     MAX_LIGHTS = 8; 
+
+#endif // mgl_const_glsl
