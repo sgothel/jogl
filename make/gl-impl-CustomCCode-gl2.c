9c1d79a157d43d0644b13d9886578348e98c3e97
Kenneth Russel <kbrussel@alum.mit.edu>
2008-12-16 01:27:42 +0000

    Fixed bug in glMapBuffer native code generation and bug in VertexBufferObject demo
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/branches/JOGL_2_SANDBOX@1821 232f8b59-042b-4e1e-8c03-345bb8c30851

--- make/gl-impl-CustomCCode-gl2.c
+++ make/gl-impl-CustomCCode-gl2.c
@@ -1,10 +1,10 @@
 /*   Java->C glue code:
- *   Java package: net.java.games.jogl.impl.GLImpl
+ *   Java package: com.sun.opengl.impl.gl2.GL2Impl
  *    Java method: long dispatch_glMapBuffer(int target, int access)
  *     C function: void * glMapBuffer(GLenum target, GLenum access);
  */
 JNIEXPORT jlong JNICALL 
-Java_com_sun_opengl_impl_GL2Impl_dispatch_1glMapBuffer(JNIEnv *env, jobject _unused, jint target, jint access, jlong glProcAddress) {
+Java_com_sun_opengl_impl_gl2_GL2Impl_dispatch_1glMapBuffer(JNIEnv *env, jobject _unused, jint target, jint access, jlong glProcAddress) {
   PFNGLMAPBUFFERPROC ptr_glMapBuffer;
   void * _res;
   ptr_glMapBuffer = (PFNGLMAPBUFFERPROC) (intptr_t) glProcAddress;
--- make/gl-impl-CustomCCode-gles2.c
+++ make/gl-impl-CustomCCode-gles2.c
@@ -1,11 +1,11 @@
 typedef GLvoid* (GL_APIENTRY* PFNGLMAPBUFFERPROC) (GLenum target, GLenum access);
 /*   Java->C glue code:
- *   Java package: net.java.games.jogl.impl.GLImpl
+ *   Java package: com.sun.opengl.impl.es2.GLES2Impl
  *    Java method: long dispatch_glMapBuffer(int target, int access)
  *     C function: void * glMapBuffer(GLenum target, GLenum access);
  */
 JNIEXPORT jlong JNICALL 
-Java_com_sun_opengl_impl_GLES2Impl_dispatch_1glMapBuffer(JNIEnv *env, jobject _unused, jint target, jint access, jlong glProcAddress) {
+Java_com_sun_opengl_impl_es2_GLES2Impl_dispatch_1glMapBuffer(JNIEnv *env, jobject _unused, jint target, jint access, jlong glProcAddress) {
   PFNGLMAPBUFFERPROC ptr_glMapBuffer;
   void * _res;
   ptr_glMapBuffer = (PFNGLMAPBUFFERPROC) (intptr_t) glProcAddress;
