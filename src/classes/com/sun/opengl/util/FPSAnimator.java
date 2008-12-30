c4a88c8fa63ca94e50e4cd3d695b50563fee6ace
Kenneth Russel <kbrussel@alum.mit.edu>
2008-12-30 20:53:50 +0000

    Refactored AWT dependencies in Animator class differently to allow both Animator and FPSAnimator to pick up AWT behavior when available. Moved Animator and FPSAnimator back to com.sun.opengl.util package as it is unclear whether these classes are useful enough on all platforms and profiles to warrant inclusion in the public specification. Updated demos.
    git-svn-id: file:///usr/local/projects/SUN/JOGL/git-svn/svn-server-sync/jogl/branches/JOGL_2_SANDBOX@1835 232f8b59-042b-4e1e-8c03-345bb8c30851

--- src/classes/com/sun/opengl/util/FPSAnimator.java
+++ src/classes/com/sun/opengl/util/FPSAnimator.java
@@ -37,7 +37,7 @@
  * and developed by Kenneth Bradley Russell and Christopher John Kline.
  */
 
-package javax.media.opengl.util;
+package com.sun.opengl.util;
 
 import java.util.*;
 import javax.media.opengl.*;
