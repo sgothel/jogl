253dfdf460e842447947bb7d0a71582c6cd62ec5
Sven Gothel <sgothel@jausoft.com>
2009-10-19 05:03:55 -0700

    Add missing file .. sorry

---
+++ make/stub_includes/opengl/gl3bc.c
@@ -0,0 +1,18 @@
+#define GLAPI
+
+// Define GL_GLEXT_PROTOTYPES so that the OpenGL extension prototypes in
+// "glext.h" are parsed.
+#define GL_GLEXT_PROTOTYPES
+
+#include <GL/gl.h>
+
+// Define GL3_PROTOTYPES so that the OpenGL prototypes in
+// "gl3.h" are parsed.
+#define GL3_PROTOTYPES
+
+// Define GL_GL3EXT_PROTOTYPES so that the OpenGL extension prototypes in
+// "gl3ext.h" are parsed.
+#define GL_GL3EXT_PROTOTYPES
+
+#include <GL3/gl3ext.h>
+
