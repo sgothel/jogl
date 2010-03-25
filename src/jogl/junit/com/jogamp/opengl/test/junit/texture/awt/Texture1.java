def4bf0d2f6ff9bf77418d26807bdcb187a3b033
Sven Gothel <sgothel@jausoft.com>
2010-03-25 03:37:39 +0100

    http://www.jogamp.org/bugzilla/show_bug.cgi?id=378
    Test:
        Added JUNIT Test Environment:
            - tests: jogl.test.jar
            - run: 'ant junit.run'
                Currently only runs 1 test regarding this bug id.
                Adding PATH (windows) or LD_LIBRARY_PATH (unix).
    
        Test initialized AWTTextureData without a current GLContext
        and then uses it to render ..
    
    Solution:
        Pending initialization of GL depending data,
        offered in TextureData.glPostInit(),
        specialized in AWTTextureData.

---
+++ src/jogl/junit/com/jogamp/opengl/test/junit/texture/awt/Texture1.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2010 Sven Gothel. All Rights Reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * - Redistribution of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * 
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name Sven Gothel or the names of
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * This software is provided "AS IS," without a warranty of any kind. ALL
+ * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
+ * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN
+ * MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR
+ * ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR
+ * DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE
+ * DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY,
+ * ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF
+ * SVEN GOTHEL HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ */
+
+package com.jogamp.opengl.test.junit.texture.awt;
+
+import com.jogamp.opengl.test.junit.texture.util.gl2.TextureGL2ListenerDraw1;
+
+import javax.media.opengl.GL;
+import javax.media.opengl.GL2ES1;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.awt.GLCanvas;
+import com.sun.opengl.util.texture.Texture;
+import com.sun.opengl.util.texture.TextureCoords;
+import com.sun.opengl.util.texture.TextureData;
+import com.sun.opengl.util.texture.TextureIO;
+import com.sun.opengl.util.texture.awt.AWTTextureIO;
+import com.sun.opengl.util.Animator;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Texture1 {
+    Frame frame;
+    BufferedImage textureImage;
+
+    @Before
+    public void init() {
+        // create base image
+        BufferedImage baseImage = new BufferedImage(256, 256, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = baseImage.createGraphics();
+        g.setPaint(new GradientPaint(0, 0, Color.CYAN,
+                                 baseImage.getWidth(), baseImage.getHeight(), Color.BLUE));
+        g.fillRect(0, 0, baseImage.getWidth(), baseImage.getHeight());
+        g.dispose();
+
+        // create texture image
+        int imageType = BufferedImage.TYPE_INT_RGB;
+        textureImage = new BufferedImage(baseImage.getWidth(),
+                                         baseImage.getHeight(),
+                                         imageType);
+        g = textureImage.createGraphics();
+        g.setComposite(AlphaComposite.Src);
+        g.drawImage(baseImage, 0, 0, null);
+        g.dispose();
+
+        frame = new Frame("Texture Test");
+    }
+
+    @Test
+    public void test1() {
+        GLCanvas glCanvas = new GLCanvas();
+        frame.add(glCanvas);
+        frame.setSize(512, 512);
+
+        // create texture    
+        TextureData textureData = AWTTextureIO.newTextureData(textureImage, false);
+        glCanvas.addGLEventListener(new TextureGL2ListenerDraw1(textureData));
+
+        Animator animator = new Animator(glCanvas);
+        frame.setVisible(true);
+        animator.start();
+
+        try {
+            Thread.sleep(100); // 100 ms
+        } catch (Exception e) {}
+
+        animator.stop();
+        frame.setVisible(false);
+
+        frame.remove(glCanvas);
+        frame.dispose();
+        frame=null;
+    }
+}
