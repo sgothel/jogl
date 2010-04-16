sgothel/jogl

commit bd4904fb04ab2168aeaf76e74385b3991429289a

JOGL (Windows):     - WindowsWGLDrawableFactory is using [singleton] shared dummy resources for         - Drawable and Context       which are utilized in case they are needed ..       They are removed at shutdown call
    - GLCapabilities
        - Set pbuffer as the HW capabilities show,
          hence onscreen && pbuffer is valid

        - DefaultGLCapabilitiesChooser: Respect PBuffer selection (fixed)
          Only skip a config, if request is !onscreen && pbuffer, but pbuffer n/a

Tests:
    - JUnit Passed (Windows32: Chromium - Except PBuffer (n/a)

src/jogl/classes/com/jogamp/opengl/impl/egl/EGLGraphicsConfiguration.java
@@ -179,11 +179,7 @@ public class EGLGraphicsConfiguration extends DefaultGraphicsConfiguration imple
             } */
         }
         if(EGL.eglGetConfigAttrib(display, config, EGL.EGL_SURFACE_TYPE, val, 0)) {
-            if(EGLConfigDrawableTypeVerify(val[0], onscreen, usePBuffer)) {
-                caps.setDoubleBuffered(onscreen);
-                caps.setOnscreen(onscreen);
-                caps.setPBuffer(usePBuffer);
-            } else if(relaxed) {
+            if(EGLConfigDrawableTypeVerify(val[0], onscreen, usePBuffer) || relaxed) {
                 caps.setDoubleBuffered( 0 != (val[0] & EGL.EGL_WINDOW_BIT) );
                 caps.setOnscreen( 0 != (val[0] & EGL.EGL_WINDOW_BIT) );
                 caps.setPBuffer ( 0 != (val[0] & EGL.EGL_PBUFFER_BIT) );

src/jogl/classes/com/jogamp/opengl/impl/windows/wgl/WindowsWGLGraphicsConfiguration.java
@@ -428,10 +428,7 @@ public class WindowsWGLGraphicsConfiguration extends DefaultGraphicsConfiguratio
                                                          boolean relaxed, boolean onscreen, boolean usePBuffer) {
         GLCapabilities res = new GLCapabilities(glp);
         int drawableTypeBits = WGLConfig2DrawableTypeBits(iattribs, niattribs, iresults);
-        if(WGLConfigDrawableTypeVerify(drawableTypeBits, onscreen, usePBuffer)) {
-            res.setOnscreen(onscreen);
-            res.setPBuffer(usePBuffer);
-        } else if(relaxed) {
+        if(WGLConfigDrawableTypeVerify(drawableTypeBits, onscreen, usePBuffer) || relaxed) {
             res.setOnscreen( 0 != (drawableTypeBits & WINDOW_BIT) );
             res.setPBuffer ( 0 != (drawableTypeBits & PBUFFER_BIT) );
         } else {

src/jogl/classes/com/jogamp/opengl/impl/x11/glx/X11GLXGraphicsConfiguration.java
@@ -250,10 +250,7 @@ public class X11GLXGraphicsConfiguration extends X11GraphicsConfiguration implem
     GLCapabilities res = new GLCapabilities(glp);
 
     val = glXGetFBConfig(display, fbcfg, GLX.GLX_DRAWABLE_TYPE, tmp, 0);
-    if(GLXFBConfigDrawableTypeVerify(val, onscreen, usePBuffer)) {
-        res.setOnscreen(onscreen);
-        res.setPBuffer(usePBuffer);
-    } else if(relaxed) {
+    if(GLXFBConfigDrawableTypeVerify(val, onscreen, usePBuffer) || relaxed) {
         res.setOnscreen( 0 != (val & GLX.GLX_WINDOW_BIT) );
         res.setPBuffer ( 0 != (val & GLX.GLX_PBUFFER_BIT) );
     } else {

make/java-run-newt.sh
@@ -1,0 +1,23 @@
+#! /bin/sh
+
+function print_usage() {
+    echo "Usage: $0 jogl-build-dir ..."
+}
+
+if [ -z "$1" ] ; then
+    echo JOGL BUILD DIR missing
+    print_usage
+    return
+fi
+
+. ./setenv-jogl.sh $1
+shift
+
+MOSX=0
+uname -a | grep -i Darwin && MOSX=1
+
+if [ $MOSX -eq 1 ] ; then
+    X_ARGS="-XstartOnFirstThread"
+fi
+
+java $X_ARGS -Djava.awt.headless=true com.jogamp.newt.util.MainThread $* 2>&1 | tee java-run-newt.log

make/java-run.sh
@@ -1,0 +1,19 @@
+#! /bin/sh
+
+function print_usage() {
+    echo "Usage: $0 jogl-build-dir ..."
+}
+
+if [ -z "$1" ] ; then
+    echo JOGL BUILD DIR missing
+    print_usage
+    return
+fi
+
+. ./setenv-jogl.sh $1
+shift
+
+MOSX=0
+uname -a | grep -i Darwin && MOSX=1
+
+java $X_ARGS $* 2>&1 | tee java-run.log

make/java-win32-dbg.bat
@@ -1,0 +1,16 @@
+
+set BLD_SUB=build-win32
+set J2RE_HOME=c:\jre1.6.0_19
+set JAVA_HOME=c:\jdk1.6.0_19
+set ANT_PATH=C:\apache-ant-1.8.0
+
+set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw\bin;%PATH%
+
+set BLD_DIR=..\%BLD_SUB%
+set LIB_DIR=%BLD_DIR%\lib;..\..\gluegen\%BLD_SUB%\obj
+
+set CP_ALL=.;%BLD_DIR%\jogl\jogl.all.jar;%BLD_DIR%\nativewindow\nativewindow.all.jar;%BLD_DIR%\newt\newt.all.jar;%BLD_DIR%\jogl\jogl.test.jar;..\..\gluegen\%BLD_SUB%\gluegen-rt.jar;..\..\gluegen\make\lib\junit-4.5.jar;%ANT_PATH%\lib\ant.jar;%ANT_PATH%\lib\ant-junit.jar
+
+echo CP_ALL %CP_ALL%
+
+%J2RE_HOME%\bin\java -classpath %CP_ALL% "-Djava.library.path=%LIB_DIR%" "-Dnativewindow.debug=all" "-Djogl.debug=all" "-Dnewt.debug=all" "-Dsun.java2d.noddraw=true" "-Dsun.awt.noerasebackground=true" %1 %2 %3 %4 %5 %6 %7 %8 %9 > java-win32-dbg.log 2>&1

make/java-win32.bat
@@ -1,0 +1,16 @@
+
+set BLD_SUB=build-win32
+set J2RE_HOME=c:\jre1.6.0_19
+set JAVA_HOME=c:\jdk1.6.0_19
+set ANT_PATH=C:\apache-ant-1.8.0
+
+set PATH=%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw\bin;%PATH%
+
+set BLD_DIR=..\%BLD_SUB%
+set LIB_DIR=%BLD_DIR%\lib;..\..\gluegen\%BLD_SUB%\obj
+
+set CP_ALL=.;%BLD_DIR%\jogl\jogl.all.jar;%BLD_DIR%\nativewindow\nativewindow.all.jar;%BLD_DIR%\newt\newt.all.jar;%BLD_DIR%\jogl\jogl.test.jar;..\..\gluegen\%BLD_SUB%\gluegen-rt.jar;..\..\gluegen\make\lib\junit-4.5.jar;%ANT_PATH%\lib\ant.jar;%ANT_PATH%\lib\ant-junit.jar
+
+echo CP_ALL %CP_ALL%
+
+%J2RE_HOME%\bin\java -classpath %CP_ALL% "-Djava.library.path=%LIB_DIR%" "-Dsun.java2d.noddraw=true" "-Dsun.awt.noerasebackground=true" %1 %2 %3 %4 %5 %6 %7 %8 %9 > java-win32.log 2>&1

make/setenv-jogl.sh
@@ -1,0 +1,92 @@
+#! /bin/sh
+
+function print_usage() {
+    echo "Usage: $0 jogl-build-dir"
+}
+
+if [ -z "$1" ] ; then
+    echo JOGL BUILD DIR missing
+    print_usage
+    return
+fi
+
+if [ -e /devtools/etc/profile.ant ] ; then
+    . /devtools/etc/profile.ant
+fi
+
+JOGL_PROFILE=JOGL_ALL
+
+JOGL_BUILDDIR=$1
+shift
+
+THISDIR=`pwd`
+
+if [ -e "$JOGL_BUILDDIR" ] ; then
+    JOGL_DIR=$JOGL_BUILDDIR/..
+    JOGL_BUILDDIR_BASE=`basename $JOGL_BUILDDIR`
+else
+    echo JOGL_BUILDDIR $JOGL_BUILDDIR not exist or not given
+    print_usage
+    return
+fi
+
+gpf=`find ../../gluegen/make -name dynlink-unix.cfg`
+if [ -z "$gpf" ] ; then
+    gpf=`find .. -name dynlink-unix.cfg`
+fi
+if [ -z "$gpf" ] ; then
+    echo GLUEGEN_BUILDDIR not found
+    print_usage
+    return
+fi
+
+GLUEGEN_DIR=`dirname $gpf`/..
+GLUEGEN_BUILDDIR=$GLUEGEN_DIR/$JOGL_BUILDDIR_BASE
+if [ ! -e "$GLUEGEN_BUILDDIR" ] ; then
+    echo GLUEGEN_BUILDDIR $GLUEGEN_BUILDDIR does not exist
+    print_usage
+    return
+fi
+GLUEGEN_JAR=$GLUEGEN_BUILDDIR/gluegen-rt.jar
+GLUEGEN_OS=$GLUEGEN_BUILDDIR/obj
+JUNIT_JAR=$GLUEGEN_DIR/make/lib/junit-4.5.jar
+
+if [ -z "$ANT_PATH" ] ; then
+    echo ANT_PATH does not exist, set it
+    print_usage
+    return
+else
+    ANT_JARS=$ANT_PATH/lib/ant.jar:$ANT_PATH/lib/ant-junit.jar
+fi
+
+echo GLUEGEN BUILDDIR: $GLUEGEN_BUILDDIR
+echo JOGL DIR: $JOGL_DIR
+echo JOGL BUILDDIR: $JOGL_BUILDDIR
+echo JOGL BUILDDIR BASE: $JOGL_BUILDDIR_BASE
+echo JOGL PROFILE: $JOGL_PROFILE
+
+J2RE_HOME=$(which java)
+JAVA_HOME=$(which javac)
+CP_SEP=:
+
+. $JOGL_DIR/etc/profile.jogl $JOGL_PROFILE $JOGL_BUILDDIR 
+
+SWT_CLASSPATH=$HOME/.java/swt.jar
+LIB=$THISDIR/lib
+
+CLASSPATH=.:$GLUEGEN_JAR:$JOGL_CLASSPATH:$SWT_CLASSPATH:$JUNIT_JAR:$ANT_JARS
+for i in $LIB/*jar ; do
+    CLASSPATH=$CLASSPATH:$i
+done
+export CLASSPATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GLUEGEN_OS:$JOGL_LIB_DIR
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$GLUEGEN_OS:$JOGL_LIB_DIR
+
+echo CLASSPATH: $CLASSPATH
+echo
+echo MacOSX REMEMBER to add the JVM arguments "-XstartOnFirstThread -Djava.awt.headless=true" for running demos without AWT, e.g. NEWT
+echo MacOSX REMEMBER to add the JVM arguments "-XstartOnFirstThread -Djava.awt.headless=true com.jogamp.newt.util.MainThread" for running demos with NEWT
+
+PATH=$J2RE_HOME/bin:$JAVA_HOME/bin:$PATH
+export PATH
+

commit 60da84a5ca8fa5e74e995ad0343c8967ba9463a5

Fix broken Offscreen/Pbuffer query introduced in bd4904fb04ab2168aeaf76e74385b3991429289a     - Have to set the requested values in GLCapabilities if not relaxed and valid,       otherwise the result is always onscreen, since the onscreen/pbuffer bits       can be set for the same config.
    - Let GLContext implementations throw an Exception
      in case of no surface handle.

JUnit Tests:
    - MiscUtils.setField -> MiscUtils.setFieldIfExists
      To allow _not_ throwing an exception :)

src/jogl/classes/com/jogamp/opengl/impl/egl/EGLContext.java
@@ -90,8 +90,7 @@ public abstract class EGLContext extends GLContextImpl {
 
     protected int makeCurrentImpl() throws GLException {
         if(EGL.EGL_NO_DISPLAY==((EGLDrawable)drawable).getDisplay() ) {
-            System.err.println("drawable not properly initialized");
-            return CONTEXT_NOT_CURRENT;
+            throw new GLException("drawable not properly initialized: "+drawable);
         }
         boolean created = false;
         if (eglContext == 0) {

src/jogl/classes/com/jogamp/opengl/impl/egl/EGLGraphicsConfiguration.java
@@ -179,7 +179,11 @@ public class EGLGraphicsConfiguration extends DefaultGraphicsConfiguration imple
             } */
         }
         if(EGL.eglGetConfigAttrib(display, config, EGL.EGL_SURFACE_TYPE, val, 0)) {
-            if(EGLConfigDrawableTypeVerify(val[0], onscreen, usePBuffer) || relaxed) {
+            if(EGLConfigDrawableTypeVerify(val[0], onscreen, usePBuffer)) {
+                caps.setDoubleBuffered(onscreen);
+                caps.setOnscreen(onscreen);
+                caps.setPBuffer(usePBuffer);
+            } else if(relaxed) {
                 caps.setDoubleBuffered( 0 != (val[0] & EGL.EGL_WINDOW_BIT) );
                 caps.setOnscreen( 0 != (val[0] & EGL.EGL_WINDOW_BIT) );
                 caps.setPBuffer ( 0 != (val[0] & EGL.EGL_PBUFFER_BIT) );

src/jogl/classes/com/jogamp/opengl/impl/macosx/cgl/MacOSXCGLContext.java
@@ -161,10 +161,7 @@ public abstract class MacOSXCGLContext extends GLContextImpl
 	
   protected int makeCurrentImpl() throws GLException {
     if (0 == cglContext && drawable.getNativeWindow().getSurfaceHandle() == 0) {
-        if (DEBUG) {
-          System.err.println("drawable not properly initialized");
-        }
-        return CONTEXT_NOT_CURRENT;
+        throw new GLException("drawable not properly initialized: "+drawable);
     }
     boolean created = false;
     if ( 0 == cglContext && 0 == nsContext) {

src/jogl/classes/com/jogamp/opengl/impl/windows/wgl/WindowsWGLContext.java
@@ -282,10 +282,7 @@ public class WindowsWGLContext extends GLContextImpl {
   
   protected int makeCurrentImpl() throws GLException {
     if (drawable.getNativeWindow().getSurfaceHandle() == 0) {
-        if (DEBUG) {
-          System.err.println("drawable not properly initialized");
-        }
-        return CONTEXT_NOT_CURRENT;
+        throw new GLException("drawable not properly initialized: "+drawable);
     }
     boolean created = false;
     if (hglrc == 0) {

src/jogl/classes/com/jogamp/opengl/impl/windows/wgl/WindowsWGLGraphicsConfiguration.java
@@ -428,7 +428,10 @@ public class WindowsWGLGraphicsConfiguration extends DefaultGraphicsConfiguratio
                                                          boolean relaxed, boolean onscreen, boolean usePBuffer) {
         GLCapabilities res = new GLCapabilities(glp);
         int drawableTypeBits = WGLConfig2DrawableTypeBits(iattribs, niattribs, iresults);
-        if(WGLConfigDrawableTypeVerify(drawableTypeBits, onscreen, usePBuffer) || relaxed) {
+        if(WGLConfigDrawableTypeVerify(drawableTypeBits, onscreen, usePBuffer)) {
+            res.setOnscreen(onscreen);
+            res.setPBuffer(usePBuffer);
+        } else if(relaxed) {
             res.setOnscreen( 0 != (drawableTypeBits & WINDOW_BIT) );
             res.setPBuffer ( 0 != (drawableTypeBits & PBUFFER_BIT) );
         } else {

src/jogl/classes/com/jogamp/opengl/impl/x11/glx/X11GLXContext.java
@@ -335,10 +335,7 @@ public abstract class X11GLXContext extends GLContextImpl {
     getDrawableImpl().getFactoryImpl().lockToolkit();
     try {
         if (drawable.getNativeWindow().getSurfaceHandle() == 0) {
-            if (DEBUG) {
-              System.err.println("drawable not properly initialized");
-            }
-            return CONTEXT_NOT_CURRENT;
+            throw new GLException("drawable not properly initialized: "+drawable);
         }
         boolean created = false;
         if (context == 0) {

src/jogl/classes/com/jogamp/opengl/impl/x11/glx/X11GLXGraphicsConfiguration.java
@@ -250,7 +250,10 @@ public class X11GLXGraphicsConfiguration extends X11GraphicsConfiguration implem
     GLCapabilities res = new GLCapabilities(glp);
 
     val = glXGetFBConfig(display, fbcfg, GLX.GLX_DRAWABLE_TYPE, tmp, 0);
-    if(GLXFBConfigDrawableTypeVerify(val, onscreen, usePBuffer) || relaxed) {
+    if(GLXFBConfigDrawableTypeVerify(val, onscreen, usePBuffer)) {
+        res.setOnscreen(onscreen);
+        res.setPBuffer(usePBuffer);
+    } else if(relaxed) {
         res.setOnscreen( 0 != (val & GLX.GLX_WINDOW_BIT) );
         res.setPBuffer ( 0 != (val & GLX.GLX_PBUFFER_BIT) );
     } else {

src/junit/com/jogamp/test/junit/jogl/offscreen/WindowUtilNEWT.java
@@ -55,11 +55,11 @@ public class WindowUtilNEWT {
         Assert.assertNotNull(demo);
         Assert.assertNotNull(window);
         if(debug) {
-            MiscUtils.setField(demo, "glDebug", true);
-            MiscUtils.setField(demo, "glTrace", true);
+            MiscUtils.setFieldIfExists(demo, "glDebug", true);
+            MiscUtils.setFieldIfExists(demo, "glTrace", true);
         }
-        if(!MiscUtils.setField(demo, "window", window)) {
-            MiscUtils.setField(demo, "glWindow", glWindow);
+        if(!MiscUtils.setFieldIfExists(demo, "window", window)) {
+            MiscUtils.setFieldIfExists(demo, "glWindow", glWindow);
         }
     }
 

src/junit/com/jogamp/test/junit/util/MiscUtils.java
@@ -40,13 +40,12 @@ public class MiscUtils {
         try {
             return Integer.parseInt(str);
         } catch (Exception ex) {
-            // FIXME
             ex.printStackTrace();
         }
         return def;
     }
 
-    public static boolean setField(Object instance, String fieldName, Object value) {
+    public static boolean setFieldIfExists(Object instance, String fieldName, Object value) {
         try {
             Field f = instance.getClass().getField(fieldName);
             if(value instanceof Boolean || f.getType().isInstance(value)) {
@@ -58,7 +57,7 @@ public class MiscUtils {
         } catch (IllegalAccessException ex) {
             throw new RuntimeException(ex);
         } catch (NoSuchFieldException nsfe) {
-            throw new RuntimeException(instance.getClass()+" has no '"+fieldName+"' field", nsfe);
+            // OK - throw new RuntimeException(instance.getClass()+" has no '"+fieldName+"' field", nsfe);
         }
         return false;
     }