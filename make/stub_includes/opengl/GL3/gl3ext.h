52d59d3f54f930eb7f45803b6f9ae605a719adb5
Sven Gothel <sgothel@jausoft.com>
2010-08-24 19:48:52 +0200

    Add license header to gl3ext.h

--- make/stub_includes/opengl/GL3/gl3ext.h
+++ make/stub_includes/opengl/GL3/gl3ext.h
@@ -2,6 +2,34 @@
 #define __gl3ext_h_
 
 /*
+** Copyright (c) 2010 JogAmp Developer Team
+** 
+** Permission is hereby granted, free of charge, to any person obtaining a
+** copy of this software and/or associated documentation files (the
+** "Materials"), to deal in the Materials without restriction, including
+** without limitation the rights to use, copy, modify, merge, publish,
+** distribute, sublicense, and/or sell copies of the Materials, and to
+** permit persons to whom the Materials are furnished to do so, subject to
+** the following conditions:
+** 
+** The above copyright notice and this permission notice shall be included
+** in all copies or substantial portions of the Materials.
+** 
+** THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+** MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
+*/
+
+/**
+ * This header files contains additional extensions not covered by the 
+ * 'official' khronos gl3.h
+ */
+
+/*
  * ------------------------------------------------
  * Everything here and below was added manually
  * to the version of glext.h obtained from:
