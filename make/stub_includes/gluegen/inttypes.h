510d3eddce9e26e9443bbfbedde0ee2dfa834daa
Sven Gothel <sgothel@jausoft.com>
2010-11-07 17:07:11 +0100

    Add gluegen stdarg.h and inttypes.h

---
+++ make/stub_includes/gluegen/inttypes.h
@@ -0,0 +1,7 @@
+#ifndef __inttypes_h
+#define __inttypes_h
+
+#include <gluegen_types.h>
+
+#endif /* __stdint_h */
+
---
+++ make/stub_includes/gluegen/stdarg.h
@@ -0,0 +1,7 @@
+#ifndef __stdarg_h
+#define __stdarg_h
+
+#include <gluegen_types.h>
+
+#endif /* __stdarg_h */
+
