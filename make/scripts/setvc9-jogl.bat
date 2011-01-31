02d5240ccac8875144e5f37c2a4d09375338adc2
Sven Gothel <sgothel@jausoft.com>
2011-01-31 07:10:50 +0100

    version bump: j2se 1.6.0_23, ant 1.8.2

--- make/scripts/setvc9-jogl.bat
+++ make/scripts/setvc9-jogl.bat
@@ -1,7 +1,7 @@
 
 
-set PATH=C:\cygwin\devtools\share\apache-ant-1.8.0\bin;%PATH%
-set ANT_PATH=C:\cygwin\devtools\share\apache-ant-1.8.0
+set PATH=C:\cygwin\devtools\share\apache-ant-1.8.2\bin;%PATH%
+set ANT_PATH=C:\cygwin\devtools\share\apache-ant-1.8.2
 
 c:
 cd C:\SUN\JOGL2\jogl\make
