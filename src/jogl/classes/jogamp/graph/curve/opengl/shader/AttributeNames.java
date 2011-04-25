61647a8da2279e1bf04a7bbe09a8912d70e90162
Sven Gothel <sgothel@jausoft.com>
2011-04-25 15:39:55 +0200

    graph/glsl: AMD need vertice attrib location 0 ..

--- src/jogl/classes/jogamp/graph/curve/opengl/shader/AttributeNames.java
+++ src/jogl/classes/jogamp/graph/curve/opengl/shader/AttributeNames.java
@@ -3,16 +3,16 @@ package jogamp.graph.curve.opengl.shader;
 public class AttributeNames {
     /** The vertices index in an OGL object
      */
-    public static final int VERTEX_ATTR_IDX = 1; // 0 is a generic special 0 ..
+    public static final int VERTEX_ATTR_IDX = 0; // FIXME: AMD needs this to be location 0 ? hu ?
     public static final String VERTEX_ATTR_NAME = "gca_Vertices";
 
     /** The Texture Coord index in an OGL object
      */
-    public static final int TEXCOORD_ATTR_IDX = 2;
+    public static final int TEXCOORD_ATTR_IDX = 1;
     public static final String TEXCOORD_ATTR_NAME = "gca_TexCoords";
     
     /** The color index in an OGL object
      */
-    public static final int COLOR_ATTR_IDX = 3;
+    public static final int COLOR_ATTR_IDX = 2;
     public static final String COLOR_ATTR_NAME = "gca_Colors";    
 }
