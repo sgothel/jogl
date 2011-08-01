3ef3ff252e6d73e6d263035eb2deb8ef4d597134
Sven Gothel <sgothel@jausoft.com>
2011-08-01 15:29:05 +0200

    Cross Test linux/armv7 script: Use es1 and es2 classes

--- make/scripts/crosstest-java-linux-armv7-rel.sh
+++ make/scripts/crosstest-java-linux-armv7-rel.sh
@@ -5,6 +5,8 @@ export HOST_RSYNC_ROOT=PROJECTS/JOGL
 export TARGET_UID=jogamp
 export TARGET_IP=beagle01
 export TARGET_ROOT=projects-cross
+#export TARGET_JAVA=/usr/bin/java
+export TARGET_JAVA=/opt-linux-armv7-eabi/jre6/bin/java
 
 export BUILD_DIR=../build-linux-armv7
 export ANT_PATH=/usr/share/ant
@@ -18,8 +20,9 @@ export ANT_PATH=/usr/share/ant
 #TSTCLASS=com.jogamp.opengl.test.junit.jogl.glsl.TestGLSLShaderState01NEWT
 #TSTCLASS=com.jogamp.opengl.test.junit.jogl.glsl.TestGLSLShaderState02NEWT
 #TSTCLASS=com.jogamp.opengl.test.junit.jogl.glsl.TestRulerNEWT01
-TSTCLASS=com.jogamp.opengl.test.junit.graph.demos.GPUTextNewtDemo01
+#TSTCLASS=com.jogamp.opengl.test.junit.graph.demos.GPUTextNewtDemo01
 #TSTCLASS=com.jogamp.opengl.test.junit.graph.demos.GPUTextNewtDemo02
+TSTCLASS=com.jogamp.opengl.test.junit.jogl.demos.gl2es1.gears.newt.TestGearsGL2ES1NEWT
 
 LOGFILE=`basename $0 .sh`.log
 
@@ -28,22 +31,24 @@ LOGFILE=`basename $0 .sh`.log
 
 CP_BASE=../../gluegen/make/lib/junit.jar:$ANT_PATH/lib/ant.jar:$ANT_PATH/lib/ant-junit.jar:../../gluegen/make/$BUILD_DIR/gluegen.jar
 CP_JOGL_ALL=$BUILD_DIR/jar/nativewindow.all.jar:$BUILD_DIR/jar/jogl.all.jar:$BUILD_DIR/jar/newt.all.jar:$BUILD_DIR/jar/jogl.test.jar
-#CP_JOGL_EGLES=$BUILD_DIR/jar/nativewindow.all-noawt.jar:$BUILD_DIR/jar/jogl.core.jar:$BUILD_DIR/jar/jogl.util.jar:$BUILD_DIR/jar/jogl.gles1.jar:$BUILD_DIR/jar/jogl.gles1.dbg.jar:$BUILD_DIR/jar/jogl.gles2.jar:$BUILD_DIR/jar/jogl.gles2.dbg.jar:$BUILD_DIR/jar/jogl.egl.jar:$BUILD_DIR/jar/jogl.util.fixedfuncemu.jar:$BUILD_DIR/jar/jogl.glu.tess.jar:$BUILD_DIR/jar/jogl.glu.mipmap.jar:$BUILD_DIR/jar/newt.all-noawt.jar:$BUILD_DIR/jar/jogl.test.jar
-CP_JOGL_EGLES=$BUILD_DIR/jar/nativewindow.all-noawt.jar:$BUILD_DIR/jar/jogl.core.jar:$BUILD_DIR/jar/jogl.util.jar:$BUILD_DIR/jar/jogl.gles2.jar:$BUILD_DIR/jar/jogl.gles2.dbg.jar:$BUILD_DIR/jar/jogl.egl.jar:$BUILD_DIR/jar/jogl.util.fixedfuncemu.jar:$BUILD_DIR/jar/jogl.glu.tess.jar:$BUILD_DIR/jar/jogl.glu.mipmap.jar:$BUILD_DIR/jar/newt.all-noawt.jar:$BUILD_DIR/jar/jogl.test.jar
+CP_JOGL_EGLES12=$BUILD_DIR/jar/nativewindow.all-noawt.jar:$BUILD_DIR/jar/jogl.core.jar:$BUILD_DIR/jar/jogl.util.jar:$BUILD_DIR/jar/jogl.gles1.jar:$BUILD_DIR/jar/jogl.gles1.dbg.jar:$BUILD_DIR/jar/jogl.gles2.jar:$BUILD_DIR/jar/jogl.gles2.dbg.jar:$BUILD_DIR/jar/jogl.egl.jar:$BUILD_DIR/jar/jogl.util.fixedfuncemu.jar:$BUILD_DIR/jar/jogl.glu.tess.jar:$BUILD_DIR/jar/jogl.glu.mipmap.jar:$BUILD_DIR/jar/newt.all-noawt.jar:$BUILD_DIR/jar/jogl.test.jar
+CP_JOGL_EGLES1=$BUILD_DIR/jar/nativewindow.all-noawt.jar:$BUILD_DIR/jar/jogl.core.jar:$BUILD_DIR/jar/jogl.util.jar:$BUILD_DIR/jar/jogl.gles1.jar:$BUILD_DIR/jar/jogl.gles1.dbg.jar:$BUILD_DIR/jar/jogl.egl.jar:$BUILD_DIR/jar/jogl.util.fixedfuncemu.jar:$BUILD_DIR/jar/jogl.glu.tess.jar:$BUILD_DIR/jar/jogl.glu.mipmap.jar:$BUILD_DIR/jar/newt.all-noawt.jar:$BUILD_DIR/jar/jogl.test.jar
+CP_JOGL_EGLES2=$BUILD_DIR/jar/nativewindow.all-noawt.jar:$BUILD_DIR/jar/jogl.core.jar:$BUILD_DIR/jar/jogl.util.jar:$BUILD_DIR/jar/jogl.gles2.jar:$BUILD_DIR/jar/jogl.gles2.dbg.jar:$BUILD_DIR/jar/jogl.egl.jar:$BUILD_DIR/jar/jogl.util.fixedfuncemu.jar:$BUILD_DIR/jar/jogl.glu.tess.jar:$BUILD_DIR/jar/jogl.glu.mipmap.jar:$BUILD_DIR/jar/newt.all-noawt.jar:$BUILD_DIR/jar/jogl.test.jar
+
+#  -Djogl.debug=all \
+#  -Dnewt.debug=all \
+#  -Djogl.debug.DebugGL \
 
 ssh $TARGET_UID@$TARGET_IP "\
 rsync -aAv --delete --delete-after --exclude 'build-x86*/' $HOST_UID@$HOST_IP::$HOST_RSYNC_ROOT/gluegen $HOST_UID@$HOST_IP::$HOST_RSYNC_ROOT/jogl $TARGET_ROOT ; \
 cd $TARGET_ROOT/jogl/make ;
 export DISPLAY=:0.0 ;
 LD_LIBRARY_PATH=../../gluegen/make/$BUILD_DIR/obj:$BUILD_DIR/lib \
-java \
+$TARGET_JAVA \
+  -server \
+  -Xmx256m \
   -Djava.library.path=../../gluegen/make/$BUILD_DIR/obj:$BUILD_DIR/lib \
-  -Djava.class.path=$CP_BASE:$CP_JOGL_EGLES \
-  -Djogl.debug.GLProfile=true \
-  -Djogl.debug.EGL=true \
-  -Djogamp.debug.JNILibLoader=true \
-  -Djogamp.debug.NativeLibrary=true \
-  -Dnewt.debug.Screen=true \
+  -Djava.class.path=$CP_BASE:$CP_JOGL_EGLES12 \
   -Dnativewindow.x11.mt-bug=true \
   $TSTCLASS $* \
  2>&1 | tee $LOGFILE \
