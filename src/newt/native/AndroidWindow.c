d870cb763cc0c6c995037b7db2285e1d365f09a7
Sven Gothel <sgothel@jausoft.com>
2011-08-11 06:19:25 +0200

    NEWT/Android: Add missing native code

---
+++ src/newt/native/AndroidWindow.c
@@ -0,0 +1,47 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include <gluegen_stdint.h>
+
+#include <unistd.h>
+#include <errno.h>
+#include <EGL/egl.h>
+
+#include "jogamp_newt_driver_android_AndroidWindow.h"
+
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+
+#define VERBOSE_ON 1
+
+#ifdef VERBOSE_ON
+    #define DBG_PRINT(...) fprintf(stdout, __VA_ARGS__)
+#else
+    #define DBG_PRINT(...)
+#endif
+
+
+JNIEXPORT jlong JNICALL Java_jogamp_newt_driver_android_AndroidWindow_getSurfaceHandle
+    (JNIEnv *env, jclass clazz, jobject surface)
+{
+    ANativeWindow * anw = ANativeWindow_fromSurface(env, surface);
+    return (jlong) (intptr_t) anw;
+}
+
+JNIEXPORT void JNICALL Java_jogamp_newt_driver_android_AndroidWindow_setSurfaceVisualID
+    (JNIEnv *env, jclass clazz, jlong surfaceHandle, jint nativeVisualID)
+{
+    ANativeWindow * anw = (ANativeWindow *) (intptr_t) surfaceHandle;
+    ANativeWindow_setBuffersGeometry(anw, 0, 0, nativeVisualID);
+}
+
+JNIEXPORT jboolean JNICALL Java_jogamp_newt_driver_android_AndroidWindow_initIDs
+  (JNIEnv *env, jclass clazz)
+{
+    DBG_PRINT( "initIDs ok\n" );
+    return JNI_TRUE;
+}
+
