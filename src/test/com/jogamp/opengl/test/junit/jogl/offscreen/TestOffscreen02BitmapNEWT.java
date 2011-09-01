59b18566d4fd19153dcd667bed44a1660762a5f9
Sven Gothel <sgothel@jausoft.com>
2011-09-01 16:16:11 +0200

    Test PIXMAP/BITMAP: Can't expect GL2/ES2 - might be SW OpenGL

--- src/test/com/jogamp/opengl/test/junit/jogl/offscreen/TestOffscreen02BitmapNEWT.java
+++ src/test/com/jogamp/opengl/test/junit/jogl/offscreen/TestOffscreen02BitmapNEWT.java
@@ -46,9 +46,13 @@ import javax.media.opengl.*;
 import javax.media.nativewindow.*;
 
 import com.jogamp.opengl.test.junit.util.UITestCase;
-import com.jogamp.opengl.test.junit.jogl.demos.es2.RedSquareES2;
+import com.jogamp.opengl.test.junit.jogl.demos.es1.RedSquareES1;
 import java.io.IOException;
 
+/**
+ * Using ES1 GL demo, since pixmap might not be hw accelerated, 
+ * hence it is possible to not have GLSL. 
+ */
 public class TestOffscreen02BitmapNEWT extends UITestCase {
     static GLProfile glpDefault;
     static GLDrawableFactory glDrawableFactory;
@@ -97,7 +101,7 @@ public class TestOffscreen02BitmapNEWT extends UITestCase {
         Assert.assertNotNull(glWindow);
         glWindow.setVisible(true);
 
-        GLEventListener demo = new RedSquareES2();
+        GLEventListener demo = new RedSquareES1();
         WindowUtilNEWT.setDemoFields(demo, window, glWindow, false);
         glWindow.addGLEventListener(demo);
 
@@ -144,7 +148,7 @@ public class TestOffscreen02BitmapNEWT extends UITestCase {
         MouseListener ml=null;
         SurfaceUpdatedListener ul=null;
 
-        GLEventListener demo = new RedSquareES2();
+        GLEventListener demo = new RedSquareES1();
         Assert.assertNotNull(demo);
 
         WindowUtilNEWT.run(glWindow, demo, null, wl, ml, ul, 2, true /*snapshot*/, false /*debug*/);
