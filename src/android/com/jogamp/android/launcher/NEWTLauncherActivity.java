a76dad8da0d85b45771e4b550ff0b1c4b60306fe
Sven Gothel <sgothel@jausoft.com>
2011-10-01 06:04:06 +0200

    Android Launcher: disable trace

--- src/android/com/jogamp/android/launcher/NEWTLauncherActivity.java
+++ src/android/com/jogamp/android/launcher/NEWTLauncherActivity.java
@@ -71,7 +71,7 @@ public abstract class NEWTLauncherActivity extends Activity {
        
        // System.setProperty("jogl.debug.GLSLState", "true");
        // System.setProperty("jogl.debug.DebugGL", "true");
-       System.setProperty("jogl.debug.TraceGL", "true");
+       // System.setProperty("jogl.debug.TraceGL", "true");
 
        // System.setProperty("newt.debug", "all");
        System.setProperty("newt.debug.Window", "true");
