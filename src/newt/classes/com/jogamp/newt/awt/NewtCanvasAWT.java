sgothel/jogl

commit 46542168d64b37f544f61802693f15b59b224e4e

OS X Layered View: Part5 NEWT/AWT Interaction ; Fix NSOpenGLLayer pos ; Cleanup
Better JAWT* name for offscreen layer surface:
  - MacOSXCGLDrawableFactory, JAWTUtil, JAWTWindow, MacOSXJAWTWindow

  - FIXME: Need to get rid of the cached JAWT instance,
    in case we like to have dual usage of offscreenLayerSurface and onscreen.
    This would be done implicit by using NEWT .. hence low prio.

AWTGraphicsConfiguration:
  - Fix create(): Use capsRequested for AWT aligned caps if capsChosen is null.
    (was capsChosen .. which is null)

  - Add updateGraphicsConfiguration() which allows to update the AWTGraphicsConfiguration.

NewtFactoryAWT:
  - Add updateGraphicsConfiguration() .. entrypoint for AWTGraphicsConfiguration.updateGraphicsConfiguration()

NSOpenGLLayer Impl:
  - For 'some reason' the layer's position is initially negative, fix it @ 1st 'draw'
  - Re-add CVDisplayLink OpenGL setting .. for what it's worth .. I don't know

JAWTWindow:
  - Remove test setting: Only enable offscreenLayerSurface for applets if avail.
    (New unit test enables it seperatly)

NewtCanvasAWT:
  - If NEWT child is offscreen, attach AWTMouseAdapter and AWTKeyAdapter
    to route these AWT input events to NEWT.

  - Don't loose-focus if NEWT child is offscreen.

  - Impl. NativeSurfaceHolder, NativeWindowHolder

  - NativeWindow is created at construction
    and it's GraphicsConfiguration updated at addNotify(..).

  - At addNotify/reparent: try harder to determine proper NEWT child size:
    - use preferred size if set
    - use minimum size if set
    - subtract insets from container size

OffscreenWindow/WindowImpl:
  - Allow setSize() .. currently NOP for offscreen.
  - WindowImpl: Commented out recreate case for offscreen-setSize ..

TEST: com.jogamp.opengl.test.junit.newt.parenting.TestParentingOffscreenLayer01AWT
  Passed tests:
    - GLCanvas
    - NewtCanvasAWT/OffscreenWindow

src/newt/classes/com/jogamp/newt/awt/NewtCanvasAWT.java
@@ -29,42 +29,52 @@
 
 package com.jogamp.newt.awt;
 
-import com.jogamp.newt.Display;
-import java.lang.reflect.*;
-import java.security.*;
-
 import java.awt.Canvas;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.GraphicsConfiguration;
 import java.awt.KeyboardFocusManager;
+import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
+import javax.media.nativewindow.Capabilities;
+import javax.media.nativewindow.CapabilitiesImmutable;
+import javax.media.nativewindow.NativeSurface;
+import javax.media.nativewindow.NativeSurfaceHolder;
 import javax.media.nativewindow.NativeWindow;
 import javax.media.nativewindow.NativeWindowException;
+import javax.media.nativewindow.NativeWindowHolder;
 import javax.media.nativewindow.WindowClosingProtocol;
 import javax.media.nativewindow.awt.AWTWindowClosingProtocol;
-import jogamp.nativewindow.awt.AWTMisc;
+import javax.swing.MenuSelectionManager;
 
-import com.jogamp.newt.event.awt.AWTAdapter;
-import com.jogamp.newt.event.WindowEvent;
-import com.jogamp.newt.Window;
-import com.jogamp.newt.event.WindowAdapter;
-import com.jogamp.newt.event.WindowListener;
+import jogamp.nativewindow.awt.AWTMisc;
 import jogamp.newt.Debug;
 import jogamp.newt.awt.event.AWTParentWindowAdapter;
 import jogamp.newt.awt.event.NewtFactoryAWT;
 
-import javax.swing.MenuSelectionManager;
+import com.jogamp.newt.Display;
+import com.jogamp.newt.Window;
+import com.jogamp.newt.event.WindowAdapter;
+import com.jogamp.newt.event.WindowEvent;
+import com.jogamp.newt.event.WindowListener;
+import com.jogamp.newt.event.awt.AWTAdapter;
+import com.jogamp.newt.event.awt.AWTKeyAdapter;
+import com.jogamp.newt.event.awt.AWTMouseAdapter;
 
 @SuppressWarnings("serial")
-public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProtocol {
+public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProtocol, NativeSurfaceHolder, NativeWindowHolder {
     public static final boolean DEBUG = Debug.debug("Window");
 
     NativeWindow nativeWindow = null;
     Window newtChild = null;
+    boolean isNewtChildOnscreen = true;
     int newtChildCloseOp;
     AWTAdapter awtAdapter = null;
-
+    AWTAdapter awtMouseAdapter = null;
+    AWTAdapter awtKeyAdapter = null;
+    
     private AWTWindowClosingProtocol awtWindowClosingProtocol =
           new AWTWindowClosingProtocol(this, new Runnable() {
                 public void run() {
@@ -77,6 +87,7 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
      */
     public NewtCanvasAWT() {
         super();
+        createNativeWindow(new Capabilities());
     }
 
     /**
@@ -84,6 +95,23 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
      */
     public NewtCanvasAWT(GraphicsConfiguration gc) {
         super(gc);
+        createNativeWindow(new Capabilities());
+    }
+
+    /**
+     * Instantiates a NewtCanvas without a NEWT child.<br>
+     */
+    public NewtCanvasAWT(CapabilitiesImmutable caps) {
+        super();
+        createNativeWindow(caps);
+    }
+
+    /**
+     * Instantiates a NewtCanvas without a NEWT child.<br>
+     */
+    public NewtCanvasAWT(GraphicsConfiguration gc, CapabilitiesImmutable caps) {
+        super(gc);
+        createNativeWindow(caps);
     }
 
     /**
@@ -91,6 +119,7 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
      */
     public NewtCanvasAWT(Window child) {
         super();
+        createNativeWindow(child.getRequestedCapabilities());
         setNEWTChild(child);
     }
 
@@ -99,7 +128,12 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
      */
     public NewtCanvasAWT(GraphicsConfiguration gc, Window child) {
         super(gc);
+        createNativeWindow(child.getRequestedCapabilities());
         setNEWTChild(child);
+    }
+    
+    private final void createNativeWindow(CapabilitiesImmutable caps) {
+        nativeWindow = NewtFactoryAWT.getNativeWindow(this, caps);
     }
     
     class FocusAction implements Window.FocusRunnable {
@@ -128,7 +162,9 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
                     System.err.println("FocusActionImpl.run() "+Display.getThreadName());
                 }
                 NewtCanvasAWT.this.requestFocusAWTParent();
-                KeyboardFocusManager.getCurrentKeyboardFocusManager().clearGlobalFocusOwner();
+                if(isNewtChildOnscreen) {                    
+                    KeyboardFocusManager.getCurrentKeyboardFocusManager().clearGlobalFocusOwner();
+                }
             }
         }
         FocusActionImpl focusActionImpl = new FocusActionImpl();
@@ -142,13 +178,13 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
           }
     };
 
-    /** sets a new NEWT child, provoking reparenting on the NEWT level. */
-    /*package */ NewtCanvasAWT setNEWTChild(Window child) {
+    /** sets a new NEWT child, provoking reparenting. */
+    private NewtCanvasAWT setNEWTChild(Window child) {
         if(newtChild!=child) {
             newtChild = child;
-            if(null!=nativeWindow) {
-                java.awt.Container cont = AWTMisc.getContainer(this);
+            if(isDisplayable()) {
                 // reparent right away, addNotify has been called already
+                final java.awt.Container cont = AWTMisc.getContainer(this);
                 reparentWindow( (null!=child) ? true : false, cont );
             }
         }
@@ -163,6 +199,9 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
     /** @return this AWT Canvas NativeWindow representation, may be null in case {@link #removeNotify()} has been called,
      * or {@link #addNotify()} hasn't been called yet.*/
     public NativeWindow getNativeWindow() { return nativeWindow; }
+    
+    /** See {@link #getNativeWindow()} */    
+    public NativeSurface getNativeSurface() { return nativeWindow; }
 
     public int getDefaultCloseOperation() {
         return awtWindowClosingProtocol.getDefaultCloseOperation();
@@ -172,11 +211,31 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
         return awtWindowClosingProtocol.setDefaultCloseOperation(op);
     }
 
+    private final void configureNewtChildInputEventHandler() {
+        if(null==awtMouseAdapter && null != newtChild.getGraphicsConfiguration()) {
+            isNewtChildOnscreen = newtChild.getGraphicsConfiguration().getChosenCapabilities().isOnscreen();
+            if(!isNewtChildOnscreen) {
+                // offscreen childs require AWT event fwd for key/mouse
+                awtMouseAdapter = new AWTMouseAdapter(newtChild).addTo(this);
+                awtKeyAdapter = new AWTKeyAdapter(newtChild).addTo(this);
+            }
+        }
+    }
+    
     /* package */ void configureNewtChild(boolean attach) {
         if(null!=awtAdapter) {
           awtAdapter.removeFrom(this);
           awtAdapter=null;
         }
+        if(null!=awtMouseAdapter) {
+            awtMouseAdapter.removeFrom(this);
+            awtMouseAdapter = null;
+        }
+        if(null!=awtKeyAdapter) {
+            awtKeyAdapter.removeFrom(this);
+            awtKeyAdapter = null;
+        }
+        
         if( null != newtChild ) {
             if(attach) {
                 awtAdapter = new AWTParentWindowAdapter(newtChild).addTo(this);
@@ -232,21 +291,34 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
 
       newtChild.setFocusAction(null); // no AWT focus traversal ..
       if(add) {
-          nativeWindow = NewtFactoryAWT.getNativeWindow(this, newtChild.getRequestedCapabilities());
-          if(null!=nativeWindow) {
-              if(DEBUG) {
-                System.err.println("NewtCanvasAWT.reparentWindow: "+newtChild);
-              }
-              final int w = cont.getWidth();
-              final int h = cont.getHeight();
-              setSize(w, h);
-              newtChild.setSize(w, h);
-              newtChild.reparentWindow(nativeWindow);
-              newtChild.setVisible(true);
-              configureNewtChild(true);
-              newtChild.sendWindowEvent(WindowEvent.EVENT_WINDOW_RESIZED); // trigger a resize/relayout to listener
-              newtChild.windowRepaint(0, 0, w, h);
+          NewtFactoryAWT.updateGraphicsConfiguration(nativeWindow, this);
+          if(DEBUG) {
+            System.err.println("NewtCanvasAWT.reparentWindow: "+newtChild);
           }
+          final int w;
+          final int h;
+          if(isPreferredSizeSet()) {
+             java.awt.Dimension d = getPreferredSize();
+             w = d.width;
+             h = d.height;
+          } else {
+             final java.awt.Dimension min;
+             if(this.isMinimumSizeSet()) {
+                 min = getMinimumSize();
+             } else {
+                 min = new java.awt.Dimension(0, 0);
+             }
+             java.awt.Insets ins = cont.getInsets();
+             w = Math.max(min.width, cont.getWidth() - ins.left - ins.right);
+             h = Math.max(min.height, cont.getHeight() - ins.top - ins.bottom);
+          }
+          setSize(w, h);
+          newtChild.setSize(w, h);
+          newtChild.reparentWindow(nativeWindow);
+          newtChild.setVisible(true);
+          configureNewtChild(true);
+          newtChild.sendWindowEvent(WindowEvent.EVENT_WINDOW_RESIZED); // trigger a resize/relayout to listener
+          newtChild.windowRepaint(0, 0, w, h);
       } else {
           configureNewtChild(false);
           nativeWindow = null;
@@ -273,7 +345,8 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
                 System.err.println("NewtCanvasAWT.destroy(): "+newtChild+", from "+cont);
             }
             configureNewtChild(false);
-            nativeWindow = null;
+            nativeWindow.destroy();
+            nativeWindow=null;
             newtChild.setVisible(false);
             newtChild.reparentWindow(null);
             newtChild.destroy();
@@ -288,6 +361,7 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
     public void paint(Graphics g) {
         awtWindowClosingProtocol.addClosingListenerOneShot();
         if(null!=newtChild) {
+            configureNewtChildInputEventHandler();
             newtChild.windowRepaint(0, 0, getWidth(), getHeight());
         }
     }
@@ -295,18 +369,21 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
     public void update(Graphics g) {
         awtWindowClosingProtocol.addClosingListenerOneShot();
         if(null!=newtChild) {
+            configureNewtChildInputEventHandler();
             newtChild.windowRepaint(0, 0, getWidth(), getHeight());
         }
     }
 
-    final void requestFocusAWTParent() {
+    private final void requestFocusAWTParent() {
         super.requestFocusInWindow();
     }
 
-    final void requestFocusNEWTChild() {
+    private final void requestFocusNEWTChild() {
         if(null!=newtChild) {
             newtChild.setFocusAction(null);
-            KeyboardFocusManager.getCurrentKeyboardFocusManager().clearGlobalFocusOwner();            
+            if(isNewtChildOnscreen) {                    
+                KeyboardFocusManager.getCurrentKeyboardFocusManager().clearGlobalFocusOwner();
+            }
             newtChild.requestFocus();
             newtChild.setFocusAction(focusAction);
         }

make/scripts/tests.sh
@@ -49,7 +49,7 @@ function jrun() {
     shift
 
     #D_ARGS="-Djogl.debug.ExtensionAvailabilityCache -Djogl.debug=all -Dnativewindow.debug=all -Djogamp.debug.ProcAddressHelper=true -Djogamp.debug.NativeLibrary=true -Djogamp.debug.NativeLibrary.Lookup=true"
-    D_ARGS="-Dnativewindow.debug=all -Djogl.debug.GLContext -Djogl.debug.GLDrawable"
+    D_ARGS="-Dnativewindow.debug=all -Djogl.debug.GLContext -Djogl.debug.GLDrawable -Dnewt.debug.Window -Dnewt.test.Window.reparent.incompatible"
     #D_ARGS="-Djogl.debug=all -Dnativewindow.debug=all"
     #D_ARGS="-Djogl.debug.GLContext -Djogl.debug.ExtensionAvailabilityCache"
     #D_ARGS="-Djogl.debug.GLContext -Djogl.debug.GLProfile -Djogl.debug.GLDrawable"
@@ -202,7 +202,7 @@ function testswt() {
 #testawt com.jogamp.opengl.test.junit.jogl.awt.TestAWT03GLCanvasRecreate01 $*
 #testawt com.jogamp.opengl.test.junit.jogl.awt.TestAWT02WindowClosing
 #testawt com.jogamp.opengl.test.junit.jogl.awt.text.TestAWTTextRendererUseVertexArrayBug464
-testawt com.jogamp.opengl.test.junit.jogl.demos.gl2.awt.TestGearsAWT $*
+#testawt com.jogamp.opengl.test.junit.jogl.demos.gl2.awt.TestGearsAWT $*
 #testawt com.jogamp.opengl.test.junit.jogl.demos.gl2.awt.TestGearsGLJPanelAWT $*
 #testawt com.jogamp.opengl.test.junit.jogl.texture.TestTexture01AWT
 #testawt com.jogamp.opengl.test.junit.jogl.awt.TestBug461OffscreenSupersamplingSwingAWT
@@ -230,6 +230,7 @@ testawt com.jogamp.opengl.test.junit.jogl.demos.gl2.awt.TestGearsAWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting01cAWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting01cSwingAWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting03AWT $*
+testawt com.jogamp.opengl.test.junit.newt.parenting.TestParentingOffscreenLayer01AWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestTranslucentParentingAWT $*
 #testawt com.jogamp.opengl.test.junit.newt.TestCloseNewtAWT
 #testawt com.jogamp.opengl.test.junit.jogl.caps.TestMultisampleAWT $*

src/jogl/classes/jogamp/opengl/macosx/cgl/MacOSXCGLContext.java
@@ -422,7 +422,7 @@ public abstract class MacOSXCGLContext extends GLContextImpl
             System.err.println("NS create pixelFormat: "+toHexString(pixelFormat));
             System.err.println("NS create drawable native-handle: "+toHexString(drawable.getHandle()));
             System.err.println("NS create drawable NSView-handle: "+toHexString(drawable.getNSViewHandle()));
-            Thread.dumpStack();
+            // Thread.dumpStack();
         }
         try {
           int[] viewNotReady = new int[1];

src/jogl/classes/jogamp/opengl/macosx/cgl/MacOSXCGLDrawableFactory.java
@@ -266,7 +266,7 @@ public class MacOSXCGLDrawableFactory extends GLDrawableFactoryImpl {
           if( nwThis instanceof MacOSXJAWTWindow) {
               // direct surface host, eg. via AWT GLCanvas
               final MacOSXJAWTWindow r = (MacOSXJAWTWindow) nwThis;
-              return r.isLayeredSurface() ? r : null;
+              return r.isOffscreenLayerSurface() ? r : null;
           } else {
               // parent surface host, eg. via native parenting w/ NewtCanvasAWT
               NativeWindow nwParent = nwThis.getParent();
@@ -276,7 +276,7 @@ public class MacOSXCGLDrawableFactory extends GLDrawableFactoryImpl {
               }
               if(null != nwParent && nwParent instanceof MacOSXJAWTWindow) {
                   final MacOSXJAWTWindow r = (MacOSXJAWTWindow) nwParent;
-                  return r.isLayeredSurface() ? r : null;
+                  return r.isOffscreenLayerSurface() ? r : null;
               }
           }
       }

src/jogl/classes/jogamp/opengl/macosx/cgl/MacOSXPbufferCGLDrawable.java
@@ -51,14 +51,10 @@ import javax.media.opengl.GLDrawableFactory;
 import javax.media.opengl.GLException;
 import javax.media.opengl.GLProfile;
 
-import jogamp.opengl.Debug;
-
 import com.jogamp.common.nio.PointerBuffer;
 import com.jogamp.opengl.util.GLBuffers;
 
-public class MacOSXPbufferCGLDrawable extends MacOSXCGLDrawable {
-  private static final boolean DEBUG = Debug.debug("MacOSXPbufferCGLDrawable");
-  
+public class MacOSXPbufferCGLDrawable extends MacOSXCGLDrawable {  
   // Abstract interface for implementation of this drawable (either
   // NSOpenGL-based or CGL-based)
   interface GLBackendImpl {
@@ -171,9 +167,10 @@ public class MacOSXPbufferCGLDrawable extends MacOSXCGLDrawable {
     pBuffer = impl.create(pBufferTexTarget, internalFormat, getWidth(), getHeight());
     if(DEBUG) {
         System.err.println("MacOSXPbufferCGLDrawable tex: target "+toHexString(pBufferTexTarget)+
-                            ", size "+toHexString(pBufferTexWidth)+"x"+toHexString(pBufferTexHeight)+
+                            ", size "+pBufferTexWidth+"x"+pBufferTexHeight+
                             ", internal-fmt "+toHexString(internalFormat));
         System.err.println("MacOSXPbufferCGLDrawable pBuffer: "+toHexString(pBuffer));
+        // Thread.dumpStack();
     }
     if (pBuffer == 0) {
       throw new GLException("pbuffer creation error: CGL.createPBuffer() failed");

src/jogl/native/macosx/MacOSXWindowSystemInterface-pbuffer.m
@@ -122,14 +122,13 @@ static CVReturn renderMyNSOpenGLLayer(CVDisplayLinkRef displayLink,
             displayLink = NULL;
         }
     }
-    /**
     if(NULL != displayLink) {
         cvres = CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, [_ctx CGLContextObj], [_fmt CGLPixelFormatObj]);
         if(kCVReturnSuccess != cvres) {
             DBG_PRINT("MyNSOpenGLLayer::init %p, CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext failed: %d\n", self, cvres);
             displayLink = NULL;
         }
-    } */
+    }
     if(NULL != displayLink) {
         cvres = CVDisplayLinkSetOutputCallback(displayLink, renderMyNSOpenGLLayer, self);
         if(kCVReturnSuccess != cvres) {
@@ -150,8 +149,12 @@ static CVReturn renderMyNSOpenGLLayer(CVDisplayLinkRef displayLink,
     textureID = 0;
     swapInterval = -1;
     shallDraw = NO;
-    DBG_PRINT("MyNSOpenGLLayer::init %p, ctx %p, pfmt %p, pbuffer %p, opaque %d, pbuffer %dx%d -> tex %dx%d\n", 
-        self, _ctx, _fmt, pbuffer, opaque, [pbuffer pixelsWide], [pbuffer pixelsHigh], texWidth, texHeight);
+
+    CGRect lRect = [self frame];
+
+    DBG_PRINT("MyNSOpenGLLayer::init %p, ctx %p, pfmt %p, pbuffer %p, opaque %d, pbuffer %dx%d -> tex %dx%d, frame: %lf/%lf %lfx%lf\n", 
+        self, _ctx, _fmt, pbuffer, opaque, [pbuffer pixelsWide], [pbuffer pixelsHigh], texWidth, texHeight,
+        lRect.origin.x, lRect.origin.y, lRect.size.width, lRect.size.height);
     return self;
 }
 
@@ -193,6 +196,7 @@ static CVReturn renderMyNSOpenGLLayer(CVDisplayLinkRef displayLink,
         forLayerTime:(CFTimeInterval)timeInterval displayTime:(const CVTimeStamp *)timeStamp
 {
     [context makeCurrentContext];
+    // FIXME ?? [context update];
    
     /**
      * v-sync doesn't works w/ NSOpenGLLayer's context .. well :(
@@ -220,6 +224,17 @@ static CVReturn renderMyNSOpenGLLayer(CVDisplayLinkRef displayLink,
         glGenTextures(1, &textureID);
         DBG_PRINT("MyNSOpenGLLayer::drawInOpenGLContext %p, ctx %p, pfmt %p tex %dx%d -> %fx%f 0x%X: creating texID 0x%X\n", 
             self, context, pixelFormat, texWidth, texHeight, tWidth, tHeight, textureTarget, textureID);
+
+        CGRect lRect = [self frame];
+        DBG_PRINT("MyNSOpenGLLayer::drawInOpenGLContext %p frame0: %lf/%lf %lfx%lf\n", 
+            self, lRect.origin.x, lRect.origin.y, lRect.size.width, lRect.size.height);
+        if(lRect.origin.x<0 || lRect.origin.y<0) {
+            lRect.origin.x = 0;
+            lRect.origin.y = 0;
+            [self setFrame: lRect];
+            DBG_PRINT("MyNSOpenGLLayer::drawInOpenGLContext %p frame*: %lf/%lf %lfx%lf\n", 
+                self, lRect.origin.x, lRect.origin.y, lRect.size.width, lRect.size.height);
+        }
     }
 
     glBindTexture(textureTarget, textureID);

src/nativewindow/classes/javax/media/nativewindow/awt/AWTGraphicsConfiguration.java
@@ -41,6 +41,7 @@
 package javax.media.nativewindow.awt;
 
 import javax.media.nativewindow.*;
+
 import java.awt.Component;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
@@ -98,12 +99,42 @@ public class AWTGraphicsConfiguration extends DefaultGraphicsConfiguration imple
 
       if(null==capsChosen) {
           GraphicsConfiguration gc = awtGraphicsDevice.getDefaultConfiguration();
-          capsChosen = setupCapabilitiesRGBABits(capsChosen, gc);
+          capsChosen = setupCapabilitiesRGBABits(capsRequested, gc);
       }
       return new AWTGraphicsConfiguration(awtScreen, capsChosen, capsRequested, awtGfxConfig);
   }
 
-    @Override
+  public void updateGraphicsConfiguration(Component awtComp)
+  {
+      AWTGraphicsScreen awtScreen = null;
+      AWTGraphicsDevice awtDevice = null;
+      GraphicsDevice awtGraphicsDevice = null;
+      GraphicsConfiguration awtGfxConfig = awtComp.getGraphicsConfiguration();
+      if(null!=awtGfxConfig) {
+          awtGraphicsDevice = awtGfxConfig.getDevice();
+          if(null!=awtGraphicsDevice) {
+              // Create Device/Screen
+              awtDevice = new AWTGraphicsDevice(awtGraphicsDevice, AbstractGraphicsDevice.DEFAULT_UNIT);
+              awtScreen = new AWTGraphicsScreen(awtDevice);
+          }
+      }
+      if(null==awtScreen) {
+          throw new NativeWindowException("native peer n/a: "+awtComp);
+      }
+      config = awtGfxConfig;
+      setScreen(awtScreen);
+
+      CapabilitiesImmutable caps = ( null != getChosenCapabilities() ) ? getChosenCapabilities() : getRequestedCapabilities();
+      GraphicsConfiguration gc = awtGraphicsDevice.getDefaultConfiguration();
+      setChosenCapabilities(setupCapabilitiesRGBABits(caps, gc));
+  }
+  
+  // open access to superclass method
+  public void setChosenCapabilities(CapabilitiesImmutable capsChosen) {
+      super.setChosenCapabilities(capsChosen);
+  }
+  
+  @Override
   public Object clone() {
       return super.clone();
   }

src/nativewindow/classes/jogamp/nativewindow/jawt/JAWTUtil.java
@@ -77,20 +77,36 @@ public class JAWTUtil {
     boolean ok;
   }
   
-  public static boolean setJAWTVersionFlags(boolean useOffScreenLayerIfAvailable) {
+  /**
+   * Returns true if this platform's JAWT implementation supports 
+   * or uses offscreen layer.
+   */
+  public static boolean isOffscreenLayerSupported() {
+       return Platform.OS_TYPE == Platform.OSType.MACOS &&
+              Platform.OS_VERSION_NUMBER.compareTo(JAWT.JAWT_MacOSXCALayerMinVersion) >= 0;      
+  }
+  
+  /**
+   * 
+   * FIXME: Need to get rid of the cached JAWT instance, 
+   *        in case we like to have dual usage of offscreenLayeredSurface and onscreen.
+   *        This would be done implicit by using NEWT .. hence low prio.
+   *        
+   * @param useOffscreenLayerIfAvailable
+   * @return
+   */
+  public static boolean setCachedJAWTVersionFlags(boolean useOffscreenLayerIfAvailable) {
     if(JAWT.isJAWTInstantiated()) { return false; } // already instantiated
     
-    if(useOffScreenLayerIfAvailable &&
-       Platform.OS_TYPE == Platform.OSType.MACOS &&
-       Platform.OS_VERSION_NUMBER.compareTo(JAWT.JAWT_MacOSXCALayerMinVersion) >= 0) {
+    if(useOffscreenLayerIfAvailable && isOffscreenLayerSupported()) {
         JAWT.setJAWTVersionFlags(JAWTFactory.JAWT_VERSION_1_4 | JAWT.JAWT_MACOSX_USE_CALAYER);
        return true;
     }
     JAWT.setJAWTVersionFlags(JAWTFactory.JAWT_VERSION_1_4);
-    return !useOffScreenLayerIfAvailable; // n/a
+    return !useOffscreenLayerIfAvailable; // n/a
   }
   
-  public static boolean isJAWTVersionUsingOffscreenLayer() {
+  public static boolean isCachedJAWTUsingOffscreenLayer() {
       return 0 != ( JAWT.getJAWT().getVersionCached() & JAWT.JAWT_MACOSX_USE_CALAYER );
   }
   

commit a968068d835a28610bd63e6eec5043cb4592cf08

NewtCanvasAWT: Fix NPE (regression from commit 46542168d64b37f544f61802693f15b59b224e4e)

src/newt/classes/com/jogamp/newt/awt/NewtCanvasAWT.java
@@ -345,8 +345,10 @@ public class NewtCanvasAWT extends java.awt.Canvas implements WindowClosingProto
                 System.err.println("NewtCanvasAWT.destroy(): "+newtChild+", from "+cont);
             }
             configureNewtChild(false);
-            nativeWindow.destroy();
-            nativeWindow=null;
+            if(null!=nativeWindow) {
+                nativeWindow.destroy();
+                nativeWindow=null;
+            }
             newtChild.setVisible(false);
             newtChild.reparentWindow(null);
             newtChild.destroy();