7ce7febbbc81ebadb502f3a0f572a1623e7ae3a7
Sven Gothel <sgothel@jausoft.com>
2011-12-18 23:35:29 +0100

    refine 32/64 bit unit tests run (OSX)

--- make/scripts/check-junit.sh
+++ make/scripts/check-junit.sh
@@ -3,25 +3,36 @@
 builddir=$1
 shift
 
-echo number of junit classes
-grep failures $builddir/test/results/* | wc
-echo
-echo number of passed junit classes - failures
-grep failures $builddir/test/results/* | grep "failures=\"0\"" | wc
-echo
-echo number of passed junit classes - errors
-grep failures $builddir/test/results/* | grep "errors=\"0\"" | wc
-echo
-echo number of failed junit classes - failures
-grep failures $builddir/test/results/* | grep -v "failures=\"0\"" | wc
-echo
-echo number of failed junit classes - errors
-grep failures $builddir/test/results/* | grep -v "errors=\"0\"" | wc
-echo
-echo failed junit classes - failures
-grep failures $builddir/test/results/* | grep -v "failures=\"0\""
-echo
-echo failed junit classes - errors
-grep failures $builddir/test/results/* | grep -v "errors=\"0\""
-echo
+function checkresult() {
+    resdir=$1
+    shift
+    if [ -e $builddir/test/$resdir ] ; then
+        echo
+        echo Results of $builddir/test/$resdir
+        echo
+        echo number of junit classes
+        grep failures $builddir/test/$resdir/* | wc
+        echo
+        echo number of passed junit classes - failures
+        grep failures $builddir/test/$resdir/* | grep "failures=\"0\"" | wc
+        echo
+        echo number of passed junit classes - errors
+        grep failures $builddir/test/$resdir/* | grep "errors=\"0\"" | wc
+        echo
+        echo number of failed junit classes - failures
+        grep failures $builddir/test/$resdir/* | grep -v "failures=\"0\"" | wc
+        echo
+        echo number of failed junit classes - errors
+        grep failures $builddir/test/$resdir/* | grep -v "errors=\"0\"" | wc
+        echo
+        echo failed junit classes - failures
+        grep failures $builddir/test/$resdir/* | grep -v "failures=\"0\""
+        echo
+        echo failed junit classes - errors
+        grep failures $builddir/test/$resdir/* | grep -v "errors=\"0\""
+        echo
+    fi
+}
 
+checkresult results
+checkresult results-x32
