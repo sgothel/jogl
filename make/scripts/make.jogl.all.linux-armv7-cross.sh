bd1eebacdb0bf426ac122867ed7e468fffbead1a
Sven Gothel <sgothel@jausoft.com>
2012-02-24 04:49:00 +0100

    Fix GLUEGEN_CPPTASKS_FILE="../../gluegen/make/lib/gluegen-cpptasks-linux-armv7.xml"

--- make/scripts/make.jogl.all.linux-armv7-cross.sh
+++ make/scripts/make.jogl.all.linux-armv7-cross.sh
@@ -29,7 +29,7 @@ export TARGET_ANT_HOME=/usr/share/ant
 export TARGET_PLATFORM_LIBS=/opt-linux-armv7-eabi/lib
 export TARGET_JAVA_LIBS=/opt-linux-armv7-eabi/jre/lib/arm
 
-export GLUEGEN_CPPTASKS_FILE="lib/gluegen-cpptasks-linux-armv7.xml"
+export GLUEGEN_CPPTASKS_FILE="../../gluegen/make/lib/gluegen-cpptasks-linux-armv7.xml"
 
 export JUNIT_RUN_ARG0="-Dnewt.test.Screen.disableScreenMode"
 
