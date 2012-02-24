e018064a70c53cd1c7b61861c43a869661e77ed2
Sven Gothel <sgothel@jausoft.com>
2012-02-24 04:46:03 +0100

    fix targetcommand.sh name on target ; Use GLUEGEN_CPPTASKS_FILE="lib/gluegen-cpptasks-linux-armv7.xml
    make.jogl.all.linux-armv7-cross.sh:
      Use GLUEGEN_CPPTASKS_FILE="lib/gluegen-cpptasks-linux-armv7.xml

--- make/scripts/make.jogl.all.linux-armv7-cross.sh
+++ make/scripts/make.jogl.all.linux-armv7-cross.sh
@@ -29,16 +29,12 @@ export TARGET_ANT_HOME=/usr/share/ant
 export TARGET_PLATFORM_LIBS=/opt-linux-armv7-eabi/lib
 export TARGET_JAVA_LIBS=/opt-linux-armv7-eabi/jre/lib/arm
 
+export GLUEGEN_CPPTASKS_FILE="lib/gluegen-cpptasks-linux-armv7.xml"
+
+export JUNIT_RUN_ARG0="-Dnewt.test.Screen.disableScreenMode"
+
 ant \
     -Drootrel.build=build-linux-armv7 \
-    -Dgluegen.cpptasks.detected.os=true \
-    -DisUnix=true \
-    -DisLinux=true \
-    -DisLinuxARMv7=true \
-    -DisX11=true \
-    -DjvmDataModel.arg="-Djnlp.no.jvm.data.model.set=true" \
-    -Djunit.run.arg0="-Dnewt.test.Screen.disableScreenMode" \
-    -DisCrosscompilation=true \
     \
     -Dsetup.addNativeKD=true \
     -Dsetup.addNativeOpenMAX=true \
