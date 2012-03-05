9ea78af201414e0eb5daa62ab8b72d7204590c40
Sven Gothel <sgothel@jausoft.com>
2012-03-05 17:40:46 +0100

    linux-armv7-cross script: Introduce TARGET_PLATFORM_ROOT

--- make/scripts/make.jogl.all.linux-armv7-cross.sh
+++ make/scripts/make.jogl.all.linux-armv7-cross.sh
@@ -29,7 +29,8 @@ export TARGET_IP=panda01
 export TARGET_ROOT=/home/jogamp/projects-cross
 export TARGET_ANT_HOME=/usr/share/ant
 
-export TARGET_PLATFORM_LIBS=/opt-linux-armv7-armel/usr/lib
+export TARGET_PLATFORM_ROOT=/opt-linux-armv7-armel
+export TARGET_PLATFORM_LIBS=$TARGET_PLATFORM_ROOT/usr/lib
 export TARGET_JAVA_LIBS=/opt-linux-armv7-armel/jre/lib/arm
 
 export GLUEGEN_CPPTASKS_FILE="../../gluegen/make/lib/gluegen-cpptasks-linux-armv7.xml"
