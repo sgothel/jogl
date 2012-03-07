8385d98098f7804e5ee369d77963f5a7464a3e1f
Sven Gothel <sgothel@jausoft.com>
2012-03-07 05:19:40 +0100

    Update android armv7 cross build/test script

--- make/scripts/make.jogl.all.android-armv7-cross.sh
+++ make/scripts/make.jogl.all.android-armv7-cross.sh
@@ -1,26 +1,43 @@
 #! /bin/sh
 
-export HOST_UID=sven
-export HOST_IP=192.168.0.52
+export NODE_LABEL=.
+
+export HOST_UID=jogamp
+# jogamp02 - 10.1.0.122
+export HOST_IP=10.1.0.122
 export HOST_RSYNC_ROOT=PROJECTS/JOGL
 
 export TARGET_UID=jogamp
-export TARGET_IP=beagle01
-export TARGET_ROOT=/projects
+#export TARGET_IP=panda02
+#export TARGET_IP=jautab03
+export TARGET_IP=jauphone04
+export TARGET_ADB_PORT=5555
+# needs executable bit (probably su)
+export TARGET_ROOT=/data/projects
 export TARGET_ANT_HOME=/usr/share/ant
-
-export ANDROID_VERSION=9
 
 echo ANDROID_SDK_HOME $ANDROID_SDK_HOME
 echo NDK_ROOT $NDK_ROOT
 
 if [ -z "$NDK_ROOT" ] ; then
-    if [ -e /usr/local/android-ndk-r7 ] ; then
-        NDK_ROOT=/usr/local/android-ndk-r7
-    elif [ -e /opt-linux-x86/android-ndk-r7 ] ; then
-        NDK_ROOT=/opt-linux-x86/android-ndk-r7
-    elif [ -e /opt/android-ndk-r7 ] ; then
-        NDK_ROOT=/opt/android-ndk-r7
+    #
+    # Generic android-ndk
+    #
+    if [ -e /usr/local/android-ndk ] ; then
+        NDK_ROOT=/usr/local/android-ndk
+    elif [ -e /opt-linux-x86/android-ndk ] ; then
+        NDK_ROOT=/opt-linux-x86/android-ndk
+    elif [ -e /opt/android-ndk ] ; then
+        NDK_ROOT=/opt/android-ndk
+    #
+    # Specific android-ndk-r7b
+    #
+    elif [ -e /usr/local/android-ndk-r7b ] ; then
+        NDK_ROOT=/usr/local/android-ndk-r7b
+    elif [ -e /opt-linux-x86/android-ndk-r7b ] ; then
+        NDK_ROOT=/opt-linux-x86/android-ndk-r7b
+    elif [ -e /opt/android-ndk-r7b ] ; then
+        NDK_ROOT=/opt/android-ndk-r7b
     else 
         echo NDK_ROOT is not specified and does not exist in default locations
         exit 1
@@ -48,30 +65,27 @@ elif [ ! -e $ANDROID_SDK_HOME ] ; then
 fi
 export ANDROID_SDK_HOME
 
+export ANDROID_VERSION=9
+export SOURCE_LEVEL=1.6
+export TARGET_LEVEL=1.6
+export TARGET_RT_JAR=/opt-share/jre1.6.0_30/lib/rt.jar
+
 export GCC_VERSION=4.4.3
 HOST_ARCH=linux-x86
-export TARGET_ARCH=arm-linux-androideabi
 export TARGET_TRIPLE=arm-linux-androideabi
 
-export NDK_TOOLCHAIN_ROOT=$NDK_ROOT/toolchains/${TARGET_ARCH}-${GCC_VERSION}/prebuilt/${HOST_ARCH}
+export NDK_TOOLCHAIN_ROOT=$NDK_ROOT/toolchains/${TARGET_TRIPLE}-${GCC_VERSION}/prebuilt/${HOST_ARCH}
 export TARGET_PLATFORM_ROOT=${NDK_ROOT}/platforms/android-${ANDROID_VERSION}/arch-arm
 
 # Need to add toolchain bins to the PATH. 
-export PATH="$NDK_TOOLCHAIN_ROOT/$TARGET_ARCH/bin:$ANDROID_SDK_HOME/platform-tools:$PATH"
+export PATH="$NDK_TOOLCHAIN_ROOT/$TARGET_TRIPLE/bin:$ANDROID_SDK_HOME/platform-tools:$PATH"
 
-which gcc 2>&1 | tee make.jogl.all.android-armv7-cross.log
+export GLUEGEN_CPPTASKS_FILE=`pwd`/../../gluegen/make/lib/gluegen-cpptasks-android-armv7.xml
+
+#export JUNIT_DISABLED="true"
+#export JUNIT_RUN_ARG0="-Dnewt.test.Screen.disableScreenMode"
 
 ant \
-    -Dgluegen-cpptasks.file=`pwd`/../../gluegen/make/lib/gluegen-cpptasks-android-armv7.xml \
     -Drootrel.build=build-android-armv7 \
-    -Dgluegen.cpptasks.detected.os=true \
-    -DisUnix=true \
-    -DisAndroid=true \
-    -DisAndroidARMv7=true \
-    -DjvmDataModel.arg="-Djnlp.no.jvm.data.model.set=true" \
-    -DisCrosscompilation=true \
-    -Dandroid.abi=armeabi-v7a \
-    \
     $* 2>&1 | tee -a make.jogl.all.android-armv7-cross.log
-
 
