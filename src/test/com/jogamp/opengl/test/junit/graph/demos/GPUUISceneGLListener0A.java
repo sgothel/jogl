802404e08476387f656115b8542366706d7eeb3f
Sven Gothel <sgothel@jausoft.com>
2012-11-11 08:55:28 +0100

    Fix Graph SceneUIController: Remove Listener by itself in dispose()

--- src/test/com/jogamp/opengl/test/junit/graph/demos/GPUUISceneGLListener0A.java
+++ src/test/com/jogamp/opengl/test/junit/graph/demos/GPUUISceneGLListener0A.java
@@ -267,9 +267,8 @@ public class GPUUISceneGLListener0A implements GLEventListener {
         } else {
             System.err.println("GPUUISceneGLListener0A: dispose (0)");            
         }
-        
-        drawable.removeGLEventListener(sceneUIController);
-        sceneUIController.dispose(drawable);
+
+        // sceneUIController will remove itself from the drawable!
         
         GL2ES2 gl = drawable.getGL().getGL2ES2();
         regionRenderer.destroy(gl);
