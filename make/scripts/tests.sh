sgothel/jogl

commit dec4b02fe4b93028c85de6a56b6af79601042d6e

NEWT Display.create: If reusing an existing instance, ensure EDT is running!

make/scripts/tests.sh
@@ -150,8 +150,9 @@ function jrun() {
     #D_ARGS="-Djogl.debug.GLContext -Djogl.debug.GLProfile -Djogl.debug.GLDrawable"
     #D_ARGS="-Djogl.debug.GLContext -Djogl.debug.GLProfile"
     #D_ARGS="-Djogl.debug.GLProfile"
-    #D_ARGS="-Dnativewindow.debug.NativeWindow"
+    #D_ARGS="-Dnativewindow.debug.NativeWindow -Dnewt.debug.Window -Dnewt.debug.Screen -Dnewt.debug.Display"
     #D_ARGS="-Djogl.debug.GLCanvas -Dnewt.debug.Window -Dnewt.debug.Display -Dnewt.debug.EDT -Djogl.debug.Animator"
+    #D_ARGS="-Dnewt.debug.Display -Dnewt.debug.EDT"
     #D_ARGS="-Dnewt.debug.EDT -Dnewt.debug.Window -Djogl.debug.GLContext"
     #D_ARGS="-Dnativewindow.debug.X11Util.XErrorStackDump -Dnativewindow.debug.X11Util.TraceDisplayLifecycle -Dnativewindow.debug.X11Util"
     #D_ARGS="-Dnativewindow.debug.X11Util -Djogl.debug.GLContext -Djogl.debug.GLDrawable -Dnewt.debug=all"
@@ -401,7 +402,7 @@ function testawtswt() {
 #testnoawt com.jogamp.opengl.test.junit.newt.TestGLWindows02NEWTAnimated $*
 #testnoawt com.jogamp.opengl.test.junit.newt.TestGLWindowInvisiblePointer01NEWT $*
 #testnoawt com.jogamp.opengl.test.junit.newt.TestDisplayLifecycle01NEWT
-testnoawt com.jogamp.opengl.test.junit.newt.TestDisplayLifecycle02NEWT
+#testnoawt com.jogamp.opengl.test.junit.newt.TestDisplayLifecycle02NEWT
 #testnoawt com.jogamp.opengl.test.junit.newt.mm.TestScreenMode00aNEWT $*
 #testnoawt com.jogamp.opengl.test.junit.newt.mm.TestScreenMode00bNEWT $*
 #testnoawt com.jogamp.opengl.test.junit.newt.mm.TestScreenMode01aNEWT $*
@@ -514,7 +515,7 @@ testnoawt com.jogamp.opengl.test.junit.newt.TestDisplayLifecycle02NEWT
 #
 # NEWT Parenting (w/ NEWT, AWT or SWT)
 #
-#testnoawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting01NEWT $*
+testnoawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting01NEWT $*
 #testnoawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting02NEWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestParentingFocusTraversal01AWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting01cSwingAWT $*

src/newt/classes/jogamp/newt/DisplayImpl.java
@@ -77,13 +77,14 @@ public abstract class DisplayImpl extends Display {
     /** Make sure to reuse a Display with the same name */
     public static Display create(String type, String name, final long handle, boolean reuse) {
         try {
-            Class<?> displayClass = getDisplayClass(type);
-            DisplayImpl display = (DisplayImpl) displayClass.newInstance();
+            final Class<?> displayClass = getDisplayClass(type);
+            final DisplayImpl display = (DisplayImpl) displayClass.newInstance();
             name = display.validateDisplayName(name, handle);
             synchronized(displayList) {
                 if(reuse) {
-                    Display display0 = Display.getLastDisplayOf(type, name, -1, true /* shared only */);
+                    final Display display0 = Display.getLastDisplayOf(type, name, -1, true /* shared only */);
                     if(null != display0) {
+                        display0.setEDTUtil(display0.getEDTUtil()); // ensures EDT is running
                         if(DEBUG) {
                             System.err.println("Display.create() REUSE: "+display0+" "+getThreadName());
                         }
@@ -99,7 +100,7 @@ public abstract class DisplayImpl extends Display {
                 display.hashCode = display.fqname.hashCode();
                 Display.addDisplay2List(display);
             }
-            display.setEDTUtil(display.edtUtil); // device's default if EDT is used, or null
+            display.setEDTUtil(display.edtUtil); // device's default if EDT is used, or null - ensures EDT is running
             
             if(DEBUG) {
                 System.err.println("Display.create() NEW: "+display+" "+getThreadName());

src/test/com/jogamp/opengl/test/junit/newt/parenting/TestParenting01NEWT.java
@@ -673,14 +673,28 @@ public class TestParenting01NEWT extends UITestCase {
     }
 
     public static void main(String args[]) throws IOException {
+        boolean asMain = false;
         for(int i=0; i<args.length; i++) {
             if(args[i].equals("-time")) {
                 durationPerTest = atoi(args[++i]);
+            } else if(args[i].equals("-asMain")) {
+                asMain = true;
             }
         }
         System.err.println("durationPerTest: "+durationPerTest);
-        String tstname = TestParenting01NEWT.class.getName();
-        org.junit.runner.JUnitCore.main(tstname);
+        if( asMain ) {
+            try {
+                TestParenting01NEWT.initClass();
+                TestParenting01NEWT m = new TestParenting01NEWT();
+                m.testWindowParenting02ReparentTop2WinReparentRecreate();
+                m.testWindowParenting01CreateVisibleDestroy();
+            } catch (Throwable t ) {
+                t.printStackTrace();
+            }
+        } else {
+            String tstname = TestParenting01NEWT.class.getName();
+            org.junit.runner.JUnitCore.main(tstname);
+        }
     }
 
 }

commit 433e3914324b90c910b018bb7d9d80e814c67123

Fix SWTEDTUtil regression caused by dec4b02fe4b93028c85de6a56b6af79601042d6e, ensuring EDT is running for reused Display instances.
Refine EDTUtil semantics of:
  - reset()
  - waitUntilStopped()

AWTEDTUtil/SWTEDTUtil: Properly signal !running when shutdown

SWTEDTUtil: Take SWT isDisposed() into account.

make/scripts/tests.sh
@@ -152,7 +152,7 @@ function jrun() {
     #D_ARGS="-Djogl.debug.GLProfile"
     #D_ARGS="-Dnativewindow.debug.NativeWindow -Dnewt.debug.Window -Dnewt.debug.Screen -Dnewt.debug.Display"
     #D_ARGS="-Djogl.debug.GLCanvas -Dnewt.debug.Window -Dnewt.debug.Display -Dnewt.debug.EDT -Djogl.debug.Animator"
-    #D_ARGS="-Dnewt.debug.Display -Dnewt.debug.EDT"
+    D_ARGS="-Dnewt.debug.Display -Dnewt.debug.EDT"
     #D_ARGS="-Dnewt.debug.EDT -Dnewt.debug.Window -Djogl.debug.GLContext"
     #D_ARGS="-Dnativewindow.debug.X11Util.XErrorStackDump -Dnativewindow.debug.X11Util.TraceDisplayLifecycle -Dnativewindow.debug.X11Util"
     #D_ARGS="-Dnativewindow.debug.X11Util -Djogl.debug.GLContext -Djogl.debug.GLDrawable -Dnewt.debug=all"
@@ -461,7 +461,7 @@ function testawtswt() {
 #
 #testswt com.jogamp.opengl.test.junit.jogl.swt.TestSWTEclipseGLCanvas01GLn $*
 #testswt com.jogamp.opengl.test.junit.jogl.swt.TestSWTJOGLGLCanvas01GLn $*
-#testswt com.jogamp.opengl.test.junit.jogl.swt.TestNewtCanvasSWTGLn $*
+testswt com.jogamp.opengl.test.junit.jogl.swt.TestNewtCanvasSWTGLn $*
 
 #
 # awtswt (testawtswt) 
@@ -515,7 +515,7 @@ function testawtswt() {
 #
 # NEWT Parenting (w/ NEWT, AWT or SWT)
 #
-testnoawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting01NEWT $*
+#testnoawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting01NEWT $*
 #testnoawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting02NEWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestParentingFocusTraversal01AWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestParenting01cSwingAWT $*

src/newt/classes/com/jogamp/newt/util/EDTUtil.java
@@ -65,14 +65,18 @@ public interface EDTUtil {
     public void setPollPeriod(long ms);
     
     /**
-     * Create a new EDT. One should invoke <code>reset()</code><br>
-     * after <code>invokeStop(..)</code> in case another start via <code>invoke(..)</code>
-     * is expected.
+     * Resets the stopped EDT, i.e. prepares it for a restart via
+     * the next <code>invoke(..)</code>  call.
+     * <p>
+     * One must stop the EDT first via {@link #invokeStop(boolean, Runnable)}
+     * and wait until it's stopped via {@link #waitUntilStopped()}.
+     * </p>
      *
      * @see #invoke(boolean, java.lang.Runnable)
      * @see #invokeStop(boolean, java.lang.Runnable)
+     * @throws IllegalStateException if stop has not been issued, or EDT is still running (caller thread not being EDT or NEDT). 
      */
-    public void reset();
+    public void reset() throws IllegalStateException;
 
     /**
      * Returns true if the current thread is the event dispatch thread (EDT).
@@ -150,6 +154,9 @@ public interface EDTUtil {
     /**
      * Wait until EDT task is stopped.<br>
      * No <code>stop</code> action is performed, {@link #invokeStop(boolean, java.lang.Runnable)} should be used before.
+     * <p>
+     * If caller thread is EDT or NEDT, this call will not block.
+     * </p>
      */
     public void waitUntilStopped();
 }

src/newt/classes/jogamp/newt/DefaultEDTUtil.java
@@ -76,9 +76,14 @@ public class DefaultEDTUtil implements EDTUtil {
     }
     
     @Override
-    public final void reset() {
-        synchronized(edtLock) { 
-            waitUntilStopped();
+    public final void reset() throws IllegalStateException {
+        synchronized(edtLock) {
+            if( isRunning() ) {
+                if( !edt.shouldStop ) {
+                    throw new IllegalStateException("EDT stop not issued.");
+                }
+                throw new IllegalStateException("EDT still running");
+            }
             if(DEBUG) {
                 if(edt.tasks.size()>0) {
                     System.err.println(Thread.currentThread()+": Default-EDT reset, remaining tasks: "+edt.tasks.size()+" - "+edt);

src/newt/classes/jogamp/newt/DisplayImpl.java
@@ -208,7 +208,7 @@ public abstract class DisplayImpl extends Display {
     }
 
     private void removeEDT(final Runnable task) {
-        if(null!=edtUtil) {            
+        if(null!=edtUtil) {
             edtUtil.invokeStop(true, task);
             // ready for restart ..
             edtUtil.waitUntilStopped();

src/newt/classes/jogamp/newt/driver/awt/AWTEDTUtil.java
@@ -68,9 +68,18 @@ public class AWTEDTUtil implements EDTUtil {
     }
     
     @Override
-    final public void reset() {
-        synchronized(edtLock) { 
-            waitUntilStopped();
+    public final void reset() throws IllegalStateException {
+        synchronized(edtLock) {
+            final Thread curT = Thread.currentThread();
+            final boolean onAWTEDT = EventQueue.isDispatchThread();
+            if( nedt.isRunning() ) {
+                if( !nedt.shouldStop ) {
+                    throw new IllegalStateException("EDT stop not issued.");
+                }
+                if( nedt != curT && !onAWTEDT ) {
+                    throw new IllegalStateException("EDT still running: Curr "+curT.getName()+", NEDT "+nedt.getName()+", on AWT-EDT "+onAWTEDT);
+                }
+            }
             if(DEBUG) {
                 System.err.println(Thread.currentThread()+": AWT-EDT reset - edt: "+nedt);
             }
@@ -110,7 +119,7 @@ public class AWTEDTUtil implements EDTUtil {
     
     @Override
     final public boolean isRunning() {
-        return nedt.isRunning() ; // AWT is always running
+        return nedt.isRunning() ;
     }
 
     @Override
@@ -203,7 +212,7 @@ public class AWTEDTUtil implements EDTUtil {
     @Override
     final public void waitUntilStopped() {
         synchronized(edtLock) {
-            if(nedt.isRunning() && nedt != Thread.currentThread() && !EventQueue.isDispatchThread()) {
+            if( nedt.isRunning() && nedt != Thread.currentThread() && !EventQueue.isDispatchThread() ) {
                 while(nedt.isRunning()) {
                     try {
                         edtLock.wait();
@@ -278,10 +287,8 @@ public class AWTEDTUtil implements EDTUtil {
                     System.err.println(getName()+": AWT-EDT run() END "+ getName()+", "+error); 
                 }
                 synchronized(edtLock) {
-                    isRunning = !shouldStop;
-                    if(!isRunning) {
-                        edtLock.notifyAll();
-                    }
+                    isRunning = false;
+                    edtLock.notifyAll();
                 }
                 if(DEBUG) {
                     System.err.println(getName()+": AWT-EDT run() EXIT "+ getName()+", exception: "+error);

src/newt/classes/jogamp/newt/swt/SWTEDTUtil.java
@@ -77,9 +77,19 @@ public class SWTEDTUtil implements EDTUtil {
     }
 
     @Override
-    public void reset() {
-        synchronized(edtLock) { 
-            waitUntilStopped();
+    public final void reset() throws IllegalStateException {
+        synchronized(edtLock) {
+            final Thread curT = Thread.currentThread();
+            final Thread swtT = !swtDisplay.isDisposed() ? swtDisplay.getThread() : null;
+            final boolean onSWTEDT = swtT == curT;
+            if( nedt.isRunning() ) {
+                if( !nedt.shouldStop ) {
+                    throw new IllegalStateException("EDT stop not issued.");
+                }
+                if( nedt != curT && !onSWTEDT ) {
+                    throw new IllegalStateException("EDT still running: Curr "+curT.getName()+", NEDT "+nedt.getName()+", SWT-EDT "+swtT.getName());
+                }
+            }
             if(DEBUG) {
                 System.err.println(Thread.currentThread()+": SWT-EDT reset - edt: "+nedt);
             }
@@ -104,7 +114,7 @@ public class SWTEDTUtil implements EDTUtil {
     
     @Override
     public boolean isCurrentThreadEDT() {
-        return swtDisplay.getThread() == Thread.currentThread();
+        return !swtDisplay.isDisposed() && swtDisplay.getThread() == Thread.currentThread();
     }
 
     @Override
@@ -115,12 +125,12 @@ public class SWTEDTUtil implements EDTUtil {
     @Override
     public final boolean isCurrentThreadEDTorNEDT() {
         final Thread ct = Thread.currentThread();
-        return ct == swtDisplay.getThread() || ct == nedt ;
+        return ( !swtDisplay.isDisposed() && ct == swtDisplay.getThread() ) || ct == nedt ;
     }
     
     @Override
     public boolean isRunning() {
-        return nedt.isRunning() ; // SWT is always running
+        return nedt.isRunning() ;
     }
     
     @Override
@@ -142,33 +152,32 @@ public class SWTEDTUtil implements EDTUtil {
                 if( nedt.shouldStop ) {
                     // drop task ..
                     if(DEBUG) {
-                        System.err.println(Thread.currentThread()+": Warning: SWT-EDT about (1) to stop, won't enqueue new task: "+nedt);
-                        Thread.dumpStack();
+                        System.err.println(Thread.currentThread()+": Warning: SWT-EDT about (1) to stop, won't enqueue new task: "+nedt+", isRunning "+nedt.isRunning());
+                        // Thread.dumpStack();
                     }
                     return; 
                 }
                 // System.err.println(Thread.currentThread()+" XXX stop: "+stop+", tasks: "+edt.tasks.size()+", task: "+task);
                 // Thread.dumpStack();
                 if(stop) {
+                    if(DEBUG) {
+                        System.err.println(Thread.currentThread()+": SWT-EDT signal STOP (on edt: "+isCurrentThreadEDT()+") - "+nedt+", isRunning "+nedt.isRunning());
+                    }
                     synchronized(nedt.sync) {
                         nedt.shouldStop = true;
                         nedt.sync.notifyAll(); // stop immediate if waiting (poll freq)
                     }
-                    if(DEBUG) {
-                        System.err.println(Thread.currentThread()+": SWT-EDT signal STOP (on edt: "+isCurrentThreadEDT()+") - "+nedt);
-                        // Thread.dumpStack();
-                    }
-                } else if( !nedt.isRunning() ) {
+                } else if( !nedt.isRunning() && !swtDisplay.isDisposed() ) {
                     // start if should not stop && not started yet
                     startImpl();
                 }
                 if( null == task ) {
                     wait = false;
+                } else if( swtDisplay.isDisposed() ) {
+                    wait = false; // drop task, SWT disposed 
                 } else if( isCurrentThreadEDT() ) {
                     task.run();
                     wait = false; // running in same thread (EDT) -> no wait
-                } else if( swtDisplay.isDisposed() ) {
-                    wait = false; // drop task, SWT disposed 
                 } else {
                     rTask = new RunnableTask(task,
                                              wait ? rTaskLock : null,
@@ -203,7 +212,7 @@ public class SWTEDTUtil implements EDTUtil {
             _nedt = nedt;
         }
         final Thread ct = Thread.currentThread();
-        if(!_nedt.isRunning() || _nedt == ct || swtDisplay.getThread() == ct) {
+        if( !_nedt.isRunning() || _nedt == ct || swtDisplay.isDisposed() || swtDisplay.getThread() == ct ) {
             return;
         }
         try {
@@ -216,8 +225,10 @@ public class SWTEDTUtil implements EDTUtil {
     @Override
     final public void waitUntilStopped() {
         synchronized(edtLock) {
-            final Thread ct = Thread.currentThread();
-            if(nedt.isRunning() && nedt != ct && swtDisplay.getThread() != ct) {
+            final Thread curT = Thread.currentThread();
+            final Thread swtT = !swtDisplay.isDisposed() ? swtDisplay.getThread() : null;
+            final boolean onSWTEDT = swtT == curT;
+            if( nedt.isRunning() && nedt != curT && !onSWTEDT ) {
                 while(nedt.isRunning()) {
                     try {
                         edtLock.wait();
@@ -296,10 +307,8 @@ public class SWTEDTUtil implements EDTUtil {
                     System.err.println(getName()+": SWT-EDT run() END "+ getName()+", "+error); 
                 }
                 synchronized(edtLock) {
-                    isRunning = !shouldStop;
-                    if(!isRunning) {
-                        edtLock.notifyAll();
-                    }
+                    isRunning = false;
+                    edtLock.notifyAll();
                 }
                 if(DEBUG) {
                     System.err.println(getName()+": SWT-EDT run() EXIT "+ getName()+", exception: "+error);