3647f6ad5ee3bc490a5647aee2de8d02119dd46c
Sven Gothel <sgothel@jausoft.com>
2013-10-24 04:56:08 +0200

    GLContext CTX_IMPL_* bits: Use 10 cached bits (+1), and 6 uncached (-1) ; CTX_IMPL_FP32_COMPAT_API: "FP32 compat-api" -> "FP32 compat"

--- src/jogl/classes/javax/media/opengl/GLContext.java
+++ src/jogl/classes/javax/media/opengl/GLContext.java
@@ -149,11 +149,11 @@ public abstract class GLContext {
   // Cached keys, bits [0..15]
   //
 
-  /** Context option bits, full bit mask covering bits [0..15], i.e. <code>0x0000FFFF</code>, {@value}. */
+  /** Context option bits, full bit mask covering 16 bits [0..15], i.e. <code>0x0000FFFF</code>, {@value}. */
   protected static final int CTX_IMPL_FULL_MASK = 0x0000FFFF;
 
-  /** Context option bits, cached bit mask covering 9 bits [0..8], i.e. <code>0x000001FF</code>, {@value}. Leaving 7 bits for non cached options, i.e. 9:7. */
-  protected static final int CTX_IMPL_CACHE_MASK = 0x000001FF;
+  /** Context option bits, cached bit mask covering 10 bits [0..9], i.e. <code>0x000003FF</code>, {@value}. Leaving 6 bits for non cached options, i.e. 10:6. */
+  protected static final int CTX_IMPL_CACHE_MASK = 0x000003FF;
 
   /** <code>ARB_create_context</code> related: created via ARB_create_context. Cache key value. See {@link #getAvailableContextProperties(AbstractGraphicsDevice, GLProfile)}. */
   protected static final int CTX_IS_ARB_CREATED  = 1 <<  0;
@@ -171,14 +171,14 @@ public abstract class GLContext {
   protected static final int CTX_IMPL_ACCEL_SOFT = 1 <<  6;
 
   //
-  // Non cached keys, bits [9..15]
+  // Non cached keys, 6 bits [10..15]
   //
 
   /** <code>GL_ARB_ES2_compatibility</code> implementation related: Context is compatible w/ ES2. Not a cache key. See {@link #isGLES2Compatible()}, {@link #getAvailableContextProperties(AbstractGraphicsDevice, GLProfile)}. */
-  protected static final int CTX_IMPL_ES2_COMPAT = 1 <<  9;
+  protected static final int CTX_IMPL_ES2_COMPAT = 1 << 10;
 
   /** <code>GL_ARB_ES3_compatibility</code> implementation related: Context is compatible w/ ES3. Not a cache key. See {@link #isGLES3Compatible()}, {@link #getAvailableContextProperties(AbstractGraphicsDevice, GLProfile)}. */
-  protected static final int CTX_IMPL_ES3_COMPAT = 1 << 10;
+  protected static final int CTX_IMPL_ES3_COMPAT = 1 << 11;
 
   /**
    * Context supports basic FBO, details see {@link #hasBasicFBOSupport()}.
@@ -186,7 +186,7 @@ public abstract class GLContext {
    * @see #hasBasicFBOSupport()
    * @see #getAvailableContextProperties(AbstractGraphicsDevice, GLProfile)
    */
-  protected static final int CTX_IMPL_FBO        = 1 << 11;
+  protected static final int CTX_IMPL_FBO        = 1 << 12;
 
   /**
    * Context supports <code>OES_single_precision</code>, fp32, fixed function point (FFP) compatibility entry points,
@@ -195,7 +195,7 @@ public abstract class GLContext {
    * @see #hasFP32CompatAPI()
    * @see #getAvailableContextProperties(AbstractGraphicsDevice, GLProfile)
    */
-  protected static final int CTX_IMPL_FP32_COMPAT_API = 1 << 12;
+  protected static final int CTX_IMPL_FP32_COMPAT_API = 1 << 13;
 
   private static final ThreadLocal<GLContext> currentContext = new ThreadLocal<GLContext>();
 
@@ -1870,7 +1870,7 @@ public abstract class GLContext {
     needColon = appendString(sb, "ES2 compat",            needColon, 0 != ( CTX_IMPL_ES2_COMPAT & ctp ));
     needColon = appendString(sb, "ES3 compat",            needColon, 0 != ( CTX_IMPL_ES3_COMPAT & ctp ));
     needColon = appendString(sb, "FBO",                   needColon, 0 != ( CTX_IMPL_FBO & ctp ));
-    needColon = appendString(sb, "FP32 compat-api",       needColon, 0 != ( CTX_IMPL_FP32_COMPAT_API & ctp ));
+    needColon = appendString(sb, "FP32 compat",           needColon, 0 != ( CTX_IMPL_FP32_COMPAT_API & ctp ));
     if( 0 != ( CTX_IMPL_ACCEL_SOFT & ctp ) ) {
         needColon = appendString(sb, "software",          needColon, true);
     } else {
