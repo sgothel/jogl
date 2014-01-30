ac55c5bfa469537c755730d3be9617d97f82950d
Sven Gothel <sgothel@jausoft.com>
2014-01-30 11:43:40 +0100

    NEWT: Add Support for AWT-Less Applet3
    - Adding 'plugin3-public' jar and sources for Applet3 support, copied from icedtea-web3
    
    - Added com.jogamp.newt.util.applet.JOGLNewtApplet3Run capable to run Applet3

--- src/newt/classes/com/jogamp/newt/util/applet/JOGLNewtApplet3Run.java
+++ src/newt/classes/com/jogamp/newt/util/applet/JOGLNewtApplet3Run.java
@@ -25,30 +25,28 @@
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of JogAmp Community.
  */
-package com.jogamp.newt.awt.applet;
+package com.jogamp.newt.util.applet;
 
-import java.applet.Applet;
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.EventQueue;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.util.Locale;
 
+import com.jogamp.plugin.applet.Applet3;
+import com.jogamp.plugin.applet.Applet3Context;
+import com.jogamp.plugin.ui.NativeWindowDownstream;
+import com.jogamp.plugin.ui.NativeWindowUpstream;
+
+import javax.media.nativewindow.AbstractGraphicsDevice;
+import javax.media.nativewindow.AbstractGraphicsScreen;
+import javax.media.nativewindow.NativeWindow;
+import javax.media.nativewindow.NativeWindowFactory;
 import javax.media.nativewindow.WindowClosingProtocol.WindowClosingMode;
 import javax.media.opengl.FPSCounter;
 import javax.media.opengl.GLCapabilities;
-import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GLProfile;
 
-import jogamp.nativewindow.jawt.JAWTUtil;
-
-import com.jogamp.common.util.awt.AWTEDTExecutor;
-import com.jogamp.newt.awt.NewtCanvasAWT;
+import com.jogamp.nativewindow.UpstreamSurfaceHookMutableSize;
+import com.jogamp.newt.NewtFactory;
+import com.jogamp.newt.Window;
 import com.jogamp.newt.opengl.GLWindow;
-import com.jogamp.newt.util.applet.JOGLNewtAppletBase;
 
 /**
  * Simple GLEventListener deployment as an applet using JOGL. This demo must be
@@ -95,48 +93,32 @@ import com.jogamp.newt.util.applet.JOGLNewtAppletBase;
  *  </pre>
  *  </p>
  */
-@SuppressWarnings("serial")
-public class JOGLNewtApplet1Run extends Applet {
+public class JOGLNewtApplet3Run implements Applet3 {
     public static final boolean DEBUG = JOGLNewtAppletBase.DEBUG;
 
     GLWindow glWindow = null;
-    NewtCanvasAWT newtCanvasAWT = null;
     JOGLNewtAppletBase base = null;
     /** if valid glStandalone:=true (own window) ! */
     int glXd=Integer.MAX_VALUE, glYd=Integer.MAX_VALUE, glWidth=Integer.MAX_VALUE, glHeight=Integer.MAX_VALUE;
+    Applet3Context ctx;
+    boolean glStandalone = false;
+
+    final String getParameter(String name) {
+        return ctx.getParameter(name);
+    }
 
     @Override
-    public void init() {
-        if(DEBUG) {
-            System.err.println("JOGLNewtApplet1Run.init() START - "+currentThreadName());
-        }
-        if(!(this instanceof Container)) {
-            throw new RuntimeException("This Applet is not a AWT Container");
-        }
-        final Container container = this;
+    public NativeWindowDownstream createNativeWindow(final Applet3Context ctx, final NativeWindowUpstream upstreamWin) {
+        this.ctx = ctx;
 
-        String glEventListenerClazzName=null;
         String glProfileName=null;
-        int glSwapInterval=0;
-        boolean glDebug=false;
-        boolean glTrace=false;
-        boolean glUndecorated=false;
-        boolean glAlwaysOnTop=false;
-        boolean glCloseable=false;
         boolean glOpaque=true;
         int glAlphaBits=0;
         int glNumMultisampleBuffer=0;
-        boolean glNoDefaultKeyListener = false;
-        boolean appletDebugTestBorder = false;
+        boolean glUndecorated=false;
+        boolean glAlwaysOnTop=false;
         try {
-            glEventListenerClazzName = getParameter("gl_event_listener_class");
             glProfileName = getParameter("gl_profile");
-            glSwapInterval = JOGLNewtAppletBase.str2Int(getParameter("gl_swap_interval"), glSwapInterval);
-            glDebug = JOGLNewtAppletBase.str2Bool(getParameter("gl_debug"), glDebug);
-            glTrace = JOGLNewtAppletBase.str2Bool(getParameter("gl_trace"), glTrace);
-            glUndecorated = JOGLNewtAppletBase.str2Bool(getParameter("gl_undecorated"), glUndecorated);
-            glAlwaysOnTop = JOGLNewtAppletBase.str2Bool(getParameter("gl_alwaysontop"), glAlwaysOnTop);
-            glCloseable = JOGLNewtAppletBase.str2Bool(getParameter("gl_closeable"), glCloseable);
             glOpaque = JOGLNewtAppletBase.str2Bool(getParameter("gl_opaque"), glOpaque);
             glAlphaBits = JOGLNewtAppletBase.str2Int(getParameter("gl_alpha"), glAlphaBits);
             glNumMultisampleBuffer = JOGLNewtAppletBase.str2Int(getParameter("gl_multisamplebuffer"), glNumMultisampleBuffer);
@@ -144,33 +126,133 @@ public class JOGLNewtApplet1Run extends Applet {
             glYd = JOGLNewtAppletBase.str2Int(getParameter("gl_dy"), glYd);
             glWidth = JOGLNewtAppletBase.str2Int(getParameter("gl_width"), glWidth);
             glHeight = JOGLNewtAppletBase.str2Int(getParameter("gl_height"), glHeight);
-            glNoDefaultKeyListener = JOGLNewtAppletBase.str2Bool(getParameter("gl_nodefaultkeyListener"), glNoDefaultKeyListener);
-            appletDebugTestBorder = JOGLNewtAppletBase.str2Bool(getParameter("appletDebugTestBorder"), appletDebugTestBorder);
+            glUndecorated = JOGLNewtAppletBase.str2Bool(getParameter("gl_undecorated"), glUndecorated);
+            glAlwaysOnTop = JOGLNewtAppletBase.str2Bool(getParameter("gl_alwaysontop"), glAlwaysOnTop);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if(null==glEventListenerClazzName) {
-            throw new RuntimeException("No applet parameter 'gl_event_listener_class'");
-        }
-        final boolean glStandalone = Integer.MAX_VALUE>glXd && Integer.MAX_VALUE>glYd && Integer.MAX_VALUE>glWidth && Integer.MAX_VALUE>glHeight;
+        glStandalone = Integer.MAX_VALUE>glXd && Integer.MAX_VALUE>glYd && Integer.MAX_VALUE>glWidth && Integer.MAX_VALUE>glHeight;
         if(DEBUG) {
             System.err.println("JOGLNewtApplet1Run Configuration:");
             System.err.println("glStandalone: "+glStandalone);
             if(glStandalone) {
                 System.err.println("pos-size: "+glXd+"/"+glYd+" "+glWidth+"x"+glHeight);
             }
-            System.err.println("glEventListenerClazzName: "+glEventListenerClazzName);
             System.err.println("glProfileName: "+glProfileName);
-            System.err.println("glSwapInterval: "+glSwapInterval);
-            System.err.println("glDebug: "+glDebug);
-            System.err.println("glTrace: "+glTrace);
-            System.err.println("glUndecorated: "+glUndecorated);
-            System.err.println("glAlwaysOnTop: "+glAlwaysOnTop);
-            System.err.println("glCloseable: "+glCloseable);
             System.err.println("glOpaque: "+glOpaque);
             System.err.println("glAlphaBits: "+glAlphaBits);
             System.err.println("glNumMultisampleBuffer: "+glNumMultisampleBuffer);
+            System.err.println("glUndecorated: "+glUndecorated);
+            System.err.println("glAlwaysOnTop: "+glAlwaysOnTop);
+        }
+        final GLCapabilities caps = new GLCapabilities(GLProfile.get(glProfileName));
+        caps.setAlphaBits(glAlphaBits);
+        if(0<glNumMultisampleBuffer) {
+            caps.setSampleBuffers(true);
+            caps.setNumSamples(glNumMultisampleBuffer);
+        }
+        caps.setBackgroundOpaque(glOpaque);
+
+        final AbstractGraphicsDevice aDevice = NativeWindowFactory.createDevice(upstreamWin.getDisplayConnection(), false /* own */); // don't open - dummy
+        final AbstractGraphicsScreen aScreen = NativeWindowFactory.createScreen(aDevice, upstreamWin.getScreenIndex());
+        final UpstreamSurfaceHookMutableSize upstreamSizeHook = new UpstreamSurfaceHookMutableSize(upstreamWin.getWidth(), upstreamWin.getHeight());
+        final NativeWindow parentWin = NativeWindowFactory.createWrappedWindow(aScreen,
+                                                                               0 /* surfaceHandle */, upstreamWin.getWindowHandle(),
+                                                                               upstreamSizeHook);
+        final Window w = NewtFactory.createWindow(parentWin, caps);
+        glWindow = GLWindow.create(w);
+        glWindow.setUndecorated(glUndecorated);
+        glWindow.setAlwaysOnTop(glAlwaysOnTop);
+        glWindow.setSize(parentWin.getWidth(), parentWin.getHeight());
+
+        return new NativeWindowDownstream() {
+            @Override
+            public void setVisible(boolean v) {
+                if( null != glWindow ) {
+                    glWindow.setVisible(v);
+                }
+            }
+
+            @Override
+            public void setSize(int width, int height) {
+                if( null != glWindow ) {
+                    upstreamSizeHook.setSize(width, height);
+                    glWindow.setSize(width, height);
+                }
+            }
+
+            @Override
+            public void requestFocus() {
+                if( null != glWindow ) {
+                    glWindow.requestFocus();
+                }
+            }
+
+            @Override
+            public void destroy() {
+                if( null != glWindow ) {
+                    glWindow.destroy();
+                }
+            }
+
+            @Override
+            public NativeWindowUpstream getParent() {
+                return upstreamWin;
+            }
+
+            @Override
+            public long getWindowHandle() {
+                if( null != glWindow ) {
+                    return glWindow.getWindowHandle();
+                } else {
+                    return 0;
+                }
+            }
+
+            @Override
+            public void display() {
+                if( null != glWindow ) {
+                    glWindow.display();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void init(Applet3Context ctx) {
+        if(DEBUG) {
+            System.err.println("JOGLNewtApplet1Run.init() START - "+currentThreadName());
+        }
+        this.ctx = ctx;
+        String glEventListenerClazzName=null;
+        int glSwapInterval=0;
+        boolean glDebug=false;
+        boolean glTrace=false;
+        boolean glNoDefaultKeyListener = false;
+        boolean glCloseable=false;
+
+        try {
+            glEventListenerClazzName = getParameter("gl_event_listener_class");
+            glSwapInterval = JOGLNewtAppletBase.str2Int(getParameter("gl_swap_interval"), glSwapInterval);
+            glDebug = JOGLNewtAppletBase.str2Bool(getParameter("gl_debug"), glDebug);
+            glTrace = JOGLNewtAppletBase.str2Bool(getParameter("gl_trace"), glTrace);
+            glNoDefaultKeyListener = JOGLNewtAppletBase.str2Bool(getParameter("gl_nodefaultkeyListener"), glNoDefaultKeyListener);
+            glCloseable = JOGLNewtAppletBase.str2Bool(getParameter("gl_closeable"), glCloseable);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(null==glEventListenerClazzName) {
+            throw new RuntimeException("No applet parameter 'gl_event_listener_class'");
+        }
+        glStandalone = Integer.MAX_VALUE>glXd && Integer.MAX_VALUE>glYd && Integer.MAX_VALUE>glWidth && Integer.MAX_VALUE>glHeight;
+        if(DEBUG) {
+            System.err.println("JOGLNewtApplet1Run Configuration:");
+            System.err.println("glEventListenerClazzName: "+glEventListenerClazzName);
+            System.err.println("glSwapInterval: "+glSwapInterval);
+            System.err.println("glDebug: "+glDebug);
+            System.err.println("glTrace: "+glTrace);
             System.err.println("glNoDefaultKeyListener: "+glNoDefaultKeyListener);
+            System.err.println("glCloseable: "+glCloseable);
         }
 
         base = new JOGLNewtAppletBase(glEventListenerClazzName,
@@ -181,51 +263,9 @@ public class JOGLNewtApplet1Run extends Applet {
                                       glTrace);
 
         try {
-            GLCapabilities caps = new GLCapabilities(GLProfile.get(glProfileName));
-            caps.setAlphaBits(glAlphaBits);
-            if(0<glNumMultisampleBuffer) {
-                caps.setSampleBuffers(true);
-                caps.setNumSamples(glNumMultisampleBuffer);
-            }
-            caps.setBackgroundOpaque(glOpaque);
-            glWindow = GLWindow.create(caps);
             glWindow.setUpdateFPSFrames(FPSCounter.DEFAULT_FRAMES_PER_INTERVAL, System.err);
-            glWindow.setUndecorated(glUndecorated);
-            glWindow.setAlwaysOnTop(glAlwaysOnTop);
             glWindow.setDefaultCloseOperation(glCloseable ? WindowClosingMode.DISPOSE_ON_CLOSE : WindowClosingMode.DO_NOTHING_ON_CLOSE);
-            container.setLayout(new BorderLayout());
-            if(appletDebugTestBorder) {
-                AWTEDTExecutor.singleton.invoke(true, new Runnable() {
-                    public void run() {
-                        container.add(new Button("North"), BorderLayout.NORTH);
-                        container.add(new Button("South"), BorderLayout.SOUTH);
-                        container.add(new Button("East"), BorderLayout.EAST);
-                        container.add(new Button("West"), BorderLayout.WEST);
-                    } } );
-            }
             base.init(glWindow);
-            if(base.isValid()) {
-                GLEventListener glEventListener = base.getGLEventListener();
-
-                if(glEventListener instanceof MouseListener) {
-                    addMouseListener((MouseListener)glEventListener);
-                }
-                if(glEventListener instanceof MouseMotionListener) {
-                    addMouseMotionListener((MouseMotionListener)glEventListener);
-                }
-                if(glEventListener instanceof KeyListener) {
-                    addKeyListener((KeyListener)glEventListener);
-                }
-            }
-            if( !glStandalone ) {
-                AWTEDTExecutor.singleton.invoke(true, new Runnable() {
-                    public void run() {
-                        newtCanvasAWT = new NewtCanvasAWT(glWindow);
-                        newtCanvasAWT.setSkipJAWTDestroy(true); // Bug 910
-                        container.add(newtCanvasAWT, BorderLayout.CENTER);
-                        container.validate();
-                    } } );
-            }
         } catch (Throwable t) {
             throw new RuntimeException(t);
         }
@@ -234,56 +274,25 @@ public class JOGLNewtApplet1Run extends Applet {
         }
     }
 
-    private static String currentThreadName() { return "["+Thread.currentThread().getName()+", isAWT-EDT "+EventQueue.isDispatchThread()+"]"; }
+    private static String currentThreadName() { return "["+Thread.currentThread().getName()+"]"; }
 
     @Override
     public void start() {
         if(DEBUG) {
-            System.err.println("JOGLNewtApplet1Run.start() START (isVisible "+isVisible()+", isDisplayable "+isDisplayable()+") - "+currentThreadName());
+            System.err.println("JOGLNewtApplet1Run.start() START (isVisible "+glWindow.isVisible()+") - "+currentThreadName());
         }
-        final java.awt.Point[] p0 = { null };
-        AWTEDTExecutor.singleton.invoke(true, new Runnable() {
-            public void run() {
-                setVisible(true);
-                p0[0] = getLocationOnScreen();
-                if( null != newtCanvasAWT ) {
-                    newtCanvasAWT.setFocusable(true);
-                    newtCanvasAWT.requestFocus();
-                }
-            }
-        });
-        if( null == newtCanvasAWT ) {
-            glWindow.requestFocus();
+        if( glStandalone ) {
             glWindow.setSize(glWidth, glHeight);
-            glWindow.setPosition(p0[0].x+glXd, p0[0].y+glYd);
+            glWindow.setPosition(glXd, glYd);
+            glWindow.setVisible(true);
+            glWindow.requestFocus();
         }
         if(DEBUG) {
-            Component topC = this;
-            while (null != topC.getParent()) {
-                topC = topC.getParent();
-            }
             System.err.println("JOGLNewtApplet1Run start:");
-            System.err.println("TopComponent: "+topC.getLocation()+" rel, "+topC.getLocationOnScreen()+" screen, visible "+topC.isVisible()+", "+topC);
-            System.err.println("Applet Pos: "+this.getLocation()+" rel, "+p0+" screen, visible "+this.isVisible()+", "+this);
-            if(null != newtCanvasAWT) {
-                System.err.println("NewtCanvasAWT Pos: "+newtCanvasAWT.getLocation()+" rel, "+newtCanvasAWT.getLocationOnScreen()+" screen, visible "+newtCanvasAWT.isVisible()+", "+newtCanvasAWT);
-            }
             System.err.println("GLWindow Pos: "+glWindow.getX()+"/"+glWindow.getY()+" rel, "+glWindow.getLocationOnScreen(null)+" screen");
             System.err.println("GLWindow: "+glWindow);
         }
         base.start();
-        if( null != newtCanvasAWT &&
-            newtCanvasAWT.isOffscreenLayerSurfaceEnabled() &&
-            0 != ( JAWTUtil.JAWT_OSX_CALAYER_QUIRK_POSITION & JAWTUtil.getOSXCALayerQuirks() ) ) {
-            // force relayout
-            AWTEDTExecutor.singleton.invoke(true, new Runnable() {
-                public void run() {
-                    final int cW = newtCanvasAWT.getWidth();
-                    final int cH = newtCanvasAWT.getHeight();
-                    newtCanvasAWT.setSize(cW+1, cH+1);
-                    newtCanvasAWT.setSize(cW, cH);
-                } } );
-        }
         if(DEBUG) {
             System.err.println("JOGLNewtApplet1Run.start() END - "+currentThreadName());
         }
@@ -305,22 +314,29 @@ public class JOGLNewtApplet1Run extends Applet {
         if(DEBUG) {
             System.err.println("JOGLNewtApplet1Run.destroy() START - "+currentThreadName());
         }
-        AWTEDTExecutor.singleton.invoke(true, new Runnable() {
-            public void run() {
-                glWindow.setVisible(false); // hide 1st
-                if( null != newtCanvasAWT ) {
-                    newtCanvasAWT.setSkipJAWTDestroy(false); // Bug 910
-                    remove(newtCanvasAWT); // remove newtCanvasAWT incl. glWindow.reparentWindow(null) if not done yet!
-                    newtCanvasAWT.destroy();
-                }
-            } } );
+        glWindow.setVisible(false); // hide 1st
         base.destroy(); // destroy glWindow unrecoverable
         base=null;
         glWindow=null;
-        newtCanvasAWT=null;
         if(DEBUG) {
             System.err.println("JOGLNewtApplet1Run.destroy() END - "+currentThreadName());
         }
     }
+
+    @Override
+    public String getAppletInfo() {
+        return null;
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+    @Override
+    public String[][] getParameterInfo() {
+        return null;
+    }
+
 }
 
---
+++ src/newt/classes/com/jogamp/newt/util/applet/VersionApplet3.java
@@ -0,0 +1,212 @@
+package com.jogamp.newt.util.applet;
+
+import com.jogamp.plugin.applet.Applet3;
+import com.jogamp.plugin.applet.Applet3Context;
+import com.jogamp.plugin.ui.NativeWindowDownstream;
+import com.jogamp.plugin.ui.NativeWindowUpstream;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLCapabilitiesImmutable;
+import javax.media.opengl.GLDrawableFactory;
+import javax.media.opengl.GLEventListener;
+
+import com.jogamp.common.GlueGenVersion;
+import com.jogamp.common.util.VersionUtil;
+import com.jogamp.newt.NewtFactory;
+import com.jogamp.newt.Window;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.JoglVersion;
+
+public class VersionApplet3 implements Applet3 {
+
+    public static void main(String[] args) {
+        VersionApplet3 va = new VersionApplet3();
+
+        final NativeWindowDownstream nwc = va.createNativeWindow(null, new NativeWindowUpstream() {
+            @Override
+            public long getWindowHandle() {
+                return 0;
+            }
+            @Override
+            public int getWidth() {
+                return 64;
+            }
+            @Override
+            public int getHeight() {
+                return 64;
+            }
+            @Override
+            public String getDisplayConnection() {
+                return null; // default
+            }
+            @Override
+            public int getScreenIndex() {
+                return 0; // default
+            }
+            @Override
+            public void notifySurfaceUpdated(NativeWindowDownstream swappedWin) {
+                // NOP
+            }
+
+        });
+        va.init(null);
+        va.start();
+        va.stop();
+        va.destroy();
+        nwc.destroy();
+    }
+
+    GLWindow canvas;
+
+    @Override
+    public NativeWindowDownstream createNativeWindow(final Applet3Context ctx, final NativeWindowUpstream parentWin) {
+        final GLCapabilities caps = new GLCapabilities(GLProfile.getDefault());
+        final Window w = NewtFactory.createWindow(parentWin.getDisplayConnection(), parentWin.getScreenIndex(), parentWin.getWindowHandle(), caps);
+        canvas = GLWindow.create(w);
+        canvas.setSize(parentWin.getWidth(), parentWin.getHeight());
+
+        return new NativeWindowDownstream() {
+            @Override
+            public void setVisible(boolean v) {
+                if( null != canvas ) {
+                    canvas.setVisible(v);
+                }
+            }
+
+            @Override
+            public void setSize(int width, int height) {
+                if( null != canvas ) {
+                    canvas.setSize(width, height);
+                }
+            }
+
+            @Override
+            public void requestFocus() {
+                if( null != canvas ) {
+                    canvas.requestFocus();
+                }
+            }
+
+            @Override
+            public void destroy() {
+                if( null != canvas ) {
+                    canvas.destroy();
+                }
+            }
+
+            @Override
+            public NativeWindowUpstream getParent() {
+                return parentWin;
+            }
+
+            @Override
+            public long getWindowHandle() {
+                if( null != canvas ) {
+                    return canvas.getWindowHandle();
+                } else {
+                    return 0;
+                }
+            }
+
+            @Override
+            public void display() {
+                if( null != canvas ) {
+                    canvas.display();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void init(Applet3Context ctx) {
+        System.err.println("VersionApplet: init() - begin");
+        canvas.addGLEventListener(new GLInfo());
+        System.err.println("VersionApplet: init() - end");
+    }
+
+    @Override
+    public void start() {
+        System.err.println("VersionApplet: start() - begin");
+
+        String s;
+
+        s = VersionUtil.getPlatformInfo().toString();
+        System.err.println(s);
+
+        s = GlueGenVersion.getInstance().toString();
+        System.err.println(s);
+
+        /*
+            s = NativeWindowVersion.getInstance().toString();
+            System.err.println(s);
+        */
+
+        s = JoglVersion.getInstance().toString();
+        System.err.println(s);
+
+        GLDrawableFactory factory = GLDrawableFactory.getFactory(canvas.getGLProfile());
+        List<GLCapabilitiesImmutable> availCaps = factory.getAvailableCapabilities(null);
+        for(int i=0; i<availCaps.size(); i++) {
+            s = availCaps.get(i).toString();
+            System.err.println(s);
+        }
+        canvas.display();
+        System.err.println("VersionApplet: start() - end");
+    }
+
+    @Override
+    public void stop() {
+        System.err.println("VersionApplet: stop() - begin");
+        canvas.setVisible(false);
+        System.err.println("VersionApplet: stop() - end");
+    }
+
+    @Override
+    public void destroy() {
+        System.err.println("VersionApplet: destroy() - start");
+        if(null!=canvas) {
+            canvas.destroy();
+            canvas = null;
+        }
+        System.err.println("VersionApplet: destroy() - end");
+    }
+
+    @Override
+    public String getAppletInfo() {
+        return null;
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+
+    @Override
+    public String[][] getParameterInfo() {
+        return null;
+    }
+
+    class GLInfo implements GLEventListener {
+        @Override
+        public void init(GLAutoDrawable drawable) {
+            GL gl = drawable.getGL();
+            String s = JoglVersion.getGLInfo(gl, null).toString();
+            System.err.println(s);
+        }
+        @Override
+        public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        }
+        @Override
+        public void display(GLAutoDrawable drawable) {
+        }
+        @Override
+        public void dispose(GLAutoDrawable drawable) {
+        }
+    }
+}
