08649aec02bae161423871c9bb3ec9a011e91088
Sven Gothel <sgothel@jausoft.com>
2014-06-25 12:58:13 +0200

    Bug 1021: OVR GlueGen Mapping: Handle non-existent native oculusvr lib gracefully
    - query isAvailable() in ovr_Initialize(), ovrHmd_Create(..) and ovrHmd_CreateDebug(..)
      and return appropriate values.

--- make/config/oculusvr/oculusvr-ovr-CustomJavaCode.java
+++ make/config/oculusvr/oculusvr-ovr-CustomJavaCode.java
@@ -1,22 +1,33 @@
 
-static {
-    AccessController.doPrivileged(new PrivilegedAction<DynamicLibraryBundle>() {
-                public DynamicLibraryBundle run() {
-                    final DynamicLibraryBundle bundle =  new DynamicLibraryBundle(new OVRDynamicLibraryBundleInfo());
-                    if(null==bundle) {
-                      throw new RuntimeException("Null DynamicLibraryBundle");
-                    }
-                    /** No native tool library to load
-                    if(!bundle.isToolLibLoaded()) {
-                      throw new RuntimeException("Couln't load native OVR library");
-                    } */
-                    if(!bundle.isLibComplete()) {
-                      throw new RuntimeException("Couln't load native OVR/JNI glue library");
-                    }
-                    if( !initializeImpl() ) {
-                        throw new RuntimeException("Initialization failure");
-                    }
-                    return bundle;
-                } } );
-}
+    static final DynamicLibraryBundle dynamicLookupHelper;
+
+    static {
+        dynamicLookupHelper = AccessController.doPrivileged(new PrivilegedAction<DynamicLibraryBundle>() {
+                    public DynamicLibraryBundle run() {
+                        final DynamicLibraryBundle bundle =  new DynamicLibraryBundle(new OVRDynamicLibraryBundleInfo());
+                        if(null==bundle) {
+                          throw new RuntimeException("Null DynamicLibraryBundle");
+                        }
+                        /** No native tool library to load
+                        if(!bundle.isToolLibLoaded()) {
+                          System.err.println("Couln't load native OVR/JNI glue library");
+                          return null;
+                        } */
+                        if(!bundle.isLibComplete()) {
+                          System.err.println("Couln't load native OVR/JNI glue library");
+                          return null;
+                        }
+                        if( !initializeImpl() ) {
+                          System.err.println("Native initialization failure of OVR/JNI glue library");
+                          return null;
+                        }
+                        return bundle;
+                    } } );
+    }
+
+    /**
+     * Accessor.
+     * @returns true if OVR library is available on this machine.
+     */
+    public static boolean isAvailable() { return dynamicLookupHelper != null; }
 
