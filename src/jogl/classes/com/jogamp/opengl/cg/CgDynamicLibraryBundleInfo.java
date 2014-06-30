5951b33b303df3a12888fe0fbc5ccc88112a3984
Mark Raynsford <code@io7m.com>
2014-06-30 12:45:53 +0000

    Attempt to remove aliasing from native libraries.
    Renamed:
    
      jogl-core.jar         → jogl.jar
      nativewindow-core.jar → nativewindow.jar
    
    The build scripts have been edited to produce sets of natives for
    each "module" (as opposed to producing one set of natives and then
    have each module point to them via aliasing).
    
    Bug: 1023
    Bug: 1024
    
    Depends on 46faa59d439ef235d7691fc64d56eedc600ffa1a from gluegen.

--- src/jogl/classes/com/jogamp/opengl/cg/CgDynamicLibraryBundleInfo.java
+++ src/jogl/classes/com/jogamp/opengl/cg/CgDynamicLibraryBundleInfo.java
@@ -49,7 +49,7 @@ public final class CgDynamicLibraryBundleInfo implements DynamicLibraryBundleInf
 
                 if(TempJarCache.isInitialized()) {
                    // only: jogl-cg.jar -> jogl-cg-natives-<os.and.arch>.jar [atomic JAR files only]
-                   JNILibLoaderBase.addNativeJarLibs(new Class<?>[] { CgDynamicLibraryBundleInfo.class }, null, null );
+                   JNILibLoaderBase.addNativeJarLibs(new Class<?>[] { CgDynamicLibraryBundleInfo.class }, null);
                 }
                 return null;
             }
