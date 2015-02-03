sgothel/jogl

commit 2120be14c7525ef051d105f9bb02294f78d17d28

Bug 1068 - Unify Surfaceless probing; Fix Surfaceless for OSX and Windows (probing, etc)
- Unify surfaceless probing
  GLDrawableFactoryImpl.probeSurfacelessCtx(..) implements surfaceless probing for all platforms

- Fix Surfaceless for OSX (probing, etc)
  - Handle 'surfaceless' mode in MacOSXCGLContext impl
  - MacOSXCGLDrawableFactory.getOrCreateSharedResourceImpl adds surfaceless probing

- Fix Surfaceless for Windows (probing, etc)
  - WindowsWGLContext.wglMakeContextCurrent(..)
    - Split release code into WindowsWGLContext.wglReleaseContext(..)
      allowing to handle zero HDC.
  - WindowsWGLGraphicsConfigurationFactory.updateGraphicsConfiguration(..)
    - Skip HDC -> PFD handling for
  - WindowsWGLDrawableFactory.createSharedResource adds surfaceless probing

src/jogl/classes/jogamp/opengl/GLDrawableFactoryImpl.java
@@ -88,6 +88,73 @@ public abstract class GLDrawableFactoryImpl extends GLDrawableFactory {
   }
 
   /**
+   * Returns {@code true} if context is capable of operating without a surface,
+   * otherwise returns {@code false} and sets the {@link GLRendererQuirks#NoSurfacelessCtx}.
+   * <p>
+   * Method will skip probing in case {@link GLRendererQuirks#NoSurfacelessCtx} has been already set.
+   * </p>
+   * <p>
+   * Caller shall skip probing in case surfaceless support is not possible,
+   * e.g. OpenGL ES without {@code EGL_KHR_surfaceless_context} or EGL &lt; 1.5,
+   * or desktop OpenGL context &lt; 3.0.
+   * </p>
+   *
+   * @param context the context to probe, must be current
+   * @param restoreDrawable If {@code true}, the initial drawable will be restored after probing
+   *                        and the temporary {@code zeroDrawable} will be released.
+   *                        If {@code false}, the temporary {@code zeroDrawable} will be kept (bound) to the context.
+   *                        Restoration may be skipped, if the drawable and context will be destroyed anyways.
+   *
+   * @see GLRendererQuirks#NoSurfacelessCtx
+   */
+  protected final boolean probeSurfacelessCtx(final GLContext context, final boolean restoreDrawable) {
+      final GLDrawable origDrawable = context.getGLDrawable();
+      final AbstractGraphicsDevice device = origDrawable.getNativeSurface().getGraphicsConfiguration().getScreen().getDevice();
+
+      final boolean noSurfacelessCtxQuirk = context.hasRendererQuirk(GLRendererQuirks.NoSurfacelessCtx);
+      boolean allowsSurfacelessCtx = false;
+
+      if( !noSurfacelessCtxQuirk ) {
+          GLDrawable zeroDrawable = null;
+          try {
+              final GLCapabilitiesImmutable caps = origDrawable.getRequestedGLCapabilities();
+              final ProxySurface zeroSurface = createSurfacelessImpl(device, true, caps, caps, null, 64, 64);
+              zeroDrawable = createOnscreenDrawableImpl(zeroSurface);
+              zeroDrawable.setRealized(true);
+
+              // Since context is still current,
+              // will keep context current w/ zeroDrawable or throws GLException
+              context.setGLDrawable(zeroDrawable, false);
+              allowsSurfacelessCtx = true; // if setGLDrawable is successful, i.e. no GLException
+
+              if( restoreDrawable ) {
+                  context.setGLDrawable(origDrawable, false);
+              }
+          } catch (final Throwable t) {
+              if( DEBUG  || GLContext.DEBUG ) {
+                  ExceptionUtils.dumpThrowable("", t);
+              }
+          } finally {
+              if( null != zeroDrawable && restoreDrawable ) {
+                  zeroDrawable.setRealized(false);
+              }
+          }
+      }
+      if( !noSurfacelessCtxQuirk && !allowsSurfacelessCtx ) {
+          final int quirk = GLRendererQuirks.NoSurfacelessCtx;
+          if(DEBUG || GLContext.DEBUG) {
+              System.err.println("Quirk: "+GLRendererQuirks.toString(quirk)+" -> "+device+": cause: probe");
+          }
+          final GLRendererQuirks glrq = context.getRendererQuirks();
+          if( null != glrq ) {
+              glrq.addQuirk(quirk);
+          }
+          GLRendererQuirks.addStickyDeviceQuirk(device, quirk);
+      }
+      return allowsSurfacelessCtx;
+  }
+
+  /**
    * Returns the shared resource mapped to the <code>device</code> {@link AbstractGraphicsDevice#getConnection()},
    * either a pre-existing or newly created, or <code>null</code> if creation failed or not supported.<br>
    * Creation of the shared resource is tried only once.

src/jogl/classes/jogamp/opengl/egl/EGLDrawableFactory.java
@@ -632,7 +632,6 @@ public class EGLDrawableFactory extends GLDrawableFactoryImpl {
             NativeSurface surface = null;
             ProxySurface upstreamSurface = null; // X11, GLX, ..
             ProxySurface downstreamSurface = null; // EGL
-            boolean allowsSurfacelessCtx = false;
             boolean success = false;
             try {
                 final GLCapabilities reqCapsAny = new GLCapabilities(glp);
@@ -718,80 +717,70 @@ public class EGLDrawableFactory extends GLDrawableFactoryImpl {
                 }
 
                 if(null != surface) {
-                    GLDrawableImpl zeroDrawable = null;
-                    final EGLDrawable drawable = (EGLDrawable) createOnscreenDrawableImpl ( surface );
-                    drawable.setRealized(true);
-                    final EGLContext context = (EGLContext) drawable.createContext(null);
-                    if (null != context) {
-                        try {
-                            if( GLContext.CONTEXT_NOT_CURRENT != context.makeCurrent() ) { // could cause exception
-                                // context.isCurrent() !
-                                final String glVersionString = context.getGL().glGetString(GL.GL_VERSION);
-                                if(null != glVersionString) {
-                                    context.mapCurrentAvailableGLESVersion(eglDevice);
-                                    if(eglDevice != adevice) {
-                                        context.mapCurrentAvailableGLESVersion(adevice);
-                                    }
+                    EGLDrawable drawable = null;
+                    GLDrawable zeroDrawable = null;
+                    EGLContext context = null;
+                    try {
+                        drawable = (EGLDrawable) createOnscreenDrawableImpl ( surface );
+                        drawable.setRealized(true);
 
-                                    if( eglFeatures.hasKHRSurfaceless &&
-                                        !context.hasRendererQuirk(GLRendererQuirks.NoSurfacelessCtx) )
-                                    {
-                                        try {
-                                            final ProxySurface zeroSurface = createSurfacelessImpl(eglDevice, true, reqCapsAny, reqCapsAny, null, 64, 64);
-                                            zeroDrawable = createOnscreenDrawableImpl(zeroSurface);
-                                            zeroDrawable.setRealized(true);
+                        context = (EGLContext) drawable.createContext(null);
+                        if (null == context) {
+                            throw new GLException("Couldn't create shared context for drawable: "+drawable);
+                        }
 
-                                            // Since sharedContext is still current,
-                                            // will keep sharedContext current w/ zeroDrawable or throws GLException
-                                            context.setGLDrawable(zeroDrawable, false);
-                                            allowsSurfacelessCtx = true; // if setGLDrawable is successful, i.e. no GLException
+                        if( GLContext.CONTEXT_NOT_CURRENT != context.makeCurrent() ) { // could cause exception
+                            // context.isCurrent() !
+                            final String glVersionString = context.getGL().glGetString(GL.GL_VERSION);
+                            if(null != glVersionString) {
+                                context.mapCurrentAvailableGLESVersion(eglDevice);
+                                if(eglDevice != adevice) {
+                                    context.mapCurrentAvailableGLESVersion(adevice);
+                                }
 
-                                            // no switch back, will be destroyed anyways
-                                            // context.setGLDrawable(drawable, false);
-                                        } catch (final Throwable t) {
-                                            if( DEBUG ) {
-                                                ExceptionUtils.dumpThrowable("", t);
-                                            }
-                                        }
-                                    }
-                                    if( !allowsSurfacelessCtx ) {
-                                        final int quirk = GLRendererQuirks.NoSurfacelessCtx;
-                                        if(DEBUG) {
-                                            System.err.println("Quirk: "+GLRendererQuirks.toString(quirk)+" -> "+eglDevice+": cause: probe");
-                                        }
-                                        final GLRendererQuirks glrq = context.getRendererQuirks();
-                                        if( null != glrq ) {
-                                            glrq.addQuirk(quirk);
-                                        }
-                                        GLRendererQuirks.addStickyDeviceQuirk(eglDevice, quirk);
-                                    }
-                                    rendererQuirks[0] = context.getRendererQuirks();
-                                    ctxProfile[0] = context.getContextOptions();
-                                    majorVersion[0] = context.getGLVersionNumber().getMajor();
-                                    minorVersion[0] = context.getGLVersionNumber().getMinor();
-                                    success = true;
-                                } else {
-                                    // Oops .. something is wrong
-                                    if(DEBUG) {
-                                        System.err.println("EGLDrawableFactory.mapAvailableEGLESConfig: "+eglDevice+", "+context.getGLVersion()+" - VERSION is null, dropping availability!");
+                                if( eglFeatures.hasKHRSurfaceless &&
+                                    ( context.isGLES() || context.getGLVersionNumber().compareTo(GLContext.Version3_0) >= 0 )
+                                  )
+                                {
+                                    if( probeSurfacelessCtx(context, false /* restoreDrawable */) ) {
+                                        zeroDrawable = context.getGLDrawable();
                                     }
                                 }
-                            }
-                        } catch (final Throwable t) {
-                            if (DEBUG) {
-                                System.err.println("EGLDrawableFactory.mapAvailableEGLESConfig: INFO: context create/makeCurrent failed");
-                                t.printStackTrace();
-                            }
-                        } finally {
-                            if( context.isCreated() ) {
-                                context.destroy();
+                                rendererQuirks[0] = context.getRendererQuirks();
+                                ctxProfile[0] = context.getContextOptions();
+                                majorVersion[0] = context.getGLVersionNumber().getMajor();
+                                minorVersion[0] = context.getGLVersionNumber().getMinor();
+                                success = true;
+                            } else {
+                                // Oops .. something is wrong
+                                if ( DEBUG_SHAREDCTX ) {
+                                    System.err.println("EGLDrawableFactory.mapAvailableEGLESConfig: "+eglDevice+", "+context.getGLVersion()+" - VERSION is null, dropping availability!");
+                                }
                             }
                         }
+                    } catch (final Throwable t) {
+                        if ( DEBUG_SHAREDCTX ) {
+                            System.err.println("EGLDrawableFactory.mapAvailableEGLESConfig: INFO: context create/makeCurrent failed");
+                            t.printStackTrace();
+                        }
+                    } finally {
+                        if( null != context ) {
+                            try {
+                                context.destroy();
+                            } catch (final GLException gle) {
+                                if ( DEBUG_SHAREDCTX ) {
+                                    System.err.println("EGLDrawableFactory.mapAvailableEGLESConfig: INFO: destroy caught exception:");
+                                    gle.printStackTrace();
+                                }
+                            }
+                        }
+                        if( null != zeroDrawable ) {
+                            zeroDrawable.setRealized(false);
+                        }
+                        if( null != drawable ) {
+                            drawable.setRealized(false);
+                        }
                     }
-                    if( null != zeroDrawable ) {
-                        zeroDrawable.setRealized(false);
-                    }
-                    drawable.setRealized(false);
                 }
             } catch (final Throwable t) {
                 if ( DEBUG_SHAREDCTX ) {

src/jogl/classes/jogamp/opengl/windows/wgl/WindowsWGLDrawableFactory.java
@@ -326,53 +326,70 @@ public class WindowsWGLDrawableFactory extends GLDrawableFactoryImpl {
         }
 
         @Override
-        public SharedResourceRunner.Resource createSharedResource(final AbstractGraphicsDevice device) {
-            final WindowsGraphicsDevice sharedDevice = new WindowsGraphicsDevice(device.getConnection(), device.getUnitID());
-            sharedDevice.lock();
+        public SharedResourceRunner.Resource createSharedResource(final AbstractGraphicsDevice adevice) {
+            final WindowsGraphicsDevice device = new WindowsGraphicsDevice(adevice.getConnection(), adevice.getUnitID());
+            GLContextImpl context = null;
+            boolean contextIsCurrent = false;
+            device.lock();
             try {
-                final AbstractGraphicsScreen absScreen = new DefaultGraphicsScreen(sharedDevice, 0);
-                final GLProfile glp = GLProfile.get(sharedDevice, GLProfile.GL_PROFILE_LIST_MIN_DESKTOP, false);
+                final AbstractGraphicsScreen absScreen = new DefaultGraphicsScreen(device, 0);
+                final GLProfile glp = GLProfile.get(device, GLProfile.GL_PROFILE_LIST_MIN_DESKTOP, false);
                 if (null == glp) {
-                    throw new GLException("Couldn't get default GLProfile for device: "+sharedDevice);
+                    throw new GLException("Couldn't get default GLProfile for device: "+device);
                 }
                 final GLCapabilitiesImmutable caps = new GLCapabilities(glp);
-                final GLDrawableImpl sharedDrawable = createOnscreenDrawableImpl(createDummySurfaceImpl(sharedDevice, false, caps, caps, null, 64, 64));
-                sharedDrawable.setRealized(true);
+                final GLDrawableImpl drawable = createOnscreenDrawableImpl(createDummySurfaceImpl(device, false, caps, caps, null, 64, 64));
+                drawable.setRealized(true);
 
-                final GLContextImpl sharedContext  = (GLContextImpl) sharedDrawable.createContext(null);
-                if (null == sharedContext) {
-                    throw new GLException("Couldn't create shared context for drawable: "+sharedDrawable);
+                context  = (GLContextImpl) drawable.createContext(null);
+                if (null == context) {
+                    throw new GLException("Couldn't create shared context for drawable: "+drawable);
                 }
-                boolean hasARBPixelFormat;
-                boolean hasARBMultisample;
-                boolean hasARBPBuffer;
-                boolean hasARBReadDrawableAvailable;
-                sharedContext.makeCurrent();
-                try {
-                    hasARBPixelFormat = sharedContext.isExtensionAvailable(WGL_ARB_pixel_format);
-                    hasARBMultisample = sharedContext.isExtensionAvailable(WGL_ARB_multisample);
-                    hasARBPBuffer = sharedContext.isExtensionAvailable(GLExtensions.ARB_pbuffer);
-                    hasARBReadDrawableAvailable = sharedContext.isExtensionAvailable(WGL_ARB_make_current_read) &&
-                                            sharedContext.isFunctionAvailable(wglMakeContextCurrent);
-                } finally {
-                    sharedContext.release();
+                contextIsCurrent = GLContext.CONTEXT_NOT_CURRENT != context.makeCurrent();
+
+                final boolean allowsSurfacelessCtx;
+                final boolean hasARBPixelFormat;
+                final boolean hasARBMultisample;
+                final boolean hasARBPBuffer;
+                final boolean hasARBReadDrawableAvailable;
+                if( contextIsCurrent ) {
+                    if( context.getGLVersionNumber().compareTo(GLContext.Version3_0) >= 0 ) {
+                        allowsSurfacelessCtx = probeSurfacelessCtx(context, true /* restoreDrawable */);
+                    } else {
+                        allowsSurfacelessCtx = false;
+                    }
+                    hasARBPixelFormat = context.isExtensionAvailable(WGL_ARB_pixel_format);
+                    hasARBMultisample = context.isExtensionAvailable(WGL_ARB_multisample);
+                    hasARBPBuffer = context.isExtensionAvailable(GLExtensions.ARB_pbuffer);
+                    hasARBReadDrawableAvailable = context.isExtensionAvailable(WGL_ARB_make_current_read) &&
+                                                  context.isFunctionAvailable(wglMakeContextCurrent);
+                } else {
+                    allowsSurfacelessCtx = false;
+                    hasARBPixelFormat = false;
+                    hasARBMultisample = false;
+                    hasARBPBuffer = false;
+                    hasARBReadDrawableAvailable = false;
                 }
-                if (DEBUG) {
-                    System.err.println("SharedDevice:  " + sharedDevice);
+                if ( DEBUG_SHAREDCTX ) {
+                    System.err.println("SharedDevice:  " + device);
                     System.err.println("SharedScreen:  " + absScreen);
-                    System.err.println("SharedContext: " + sharedContext);
+                    System.err.println("SharedContext: " + context + ", madeCurrent " + contextIsCurrent);
+                    System.err.println("  allowsSurfacelessCtx "+allowsSurfacelessCtx);
                     System.err.println("pixelformat:   " + hasARBPixelFormat);
                     System.err.println("multisample:   " + hasARBMultisample);
                     System.err.println("pbuffer:       " + hasARBPBuffer);
                     System.err.println("readDrawable:  " + hasARBReadDrawableAvailable);
                 }
-                return new SharedResource(sharedDevice, absScreen, sharedDrawable, sharedContext,
+                return new SharedResource(device, absScreen, drawable, context,
                                           hasARBPixelFormat, hasARBMultisample,
                                           hasARBPBuffer, hasARBReadDrawableAvailable);
             } catch (final Throwable t) {
-                throw new GLException("WindowsWGLDrawableFactory - Could not initialize shared resources for "+device, t);
+                throw new GLException("WindowsWGLDrawableFactory - Could not initialize shared resources for "+adevice, t);
             } finally {
-                sharedDevice.unlock();
+                if ( contextIsCurrent ) {
+                    context.release();
+                }
+                device.unlock();
             }
         }
 

commit 3317e7a427fbb81dd3d7daa8116c7d33166ed003

Bug 1068 - Fix issue w/ unifying Surfaceless probing (commit 2120be14c7525ef051d105f9bb02294f78d17d28)
Commit 2120be14c7525ef051d105f9bb02294f78d17d28
caused GLDrawableFactoryImpl.probeSurfacelessCtx(..)
to be only called if appropriate, hence GLRendererQuirks.NoSurfacelessCtx
was not set otherwise.

This patch adds the required 'else' branch for the case
GLDrawableFactoryImpl.probeSurfacelessCtx(..) is not called
and issues GLDrawableFactoryImpl.setNoSurfacelessCtxQuirk(..).

+++

Add Note to EGLDrawableFactory's shutdown:
  "sr.device.close() Issues eglTerminate(), which may cause SIGSEGV w/ NVIDIA 343.36 w/ TestGLProfile01NEWT"

However keep this code enabled, since this seems to be a driver issue.

+++

make/scripts/tests.sh
@@ -101,7 +101,7 @@ function jrun() {
     #D_ARGS="-Dnativewindow.debug=all"
     #D_ARGS="-Djogl.debug=all"
     #D_ARGS="-Djogl.debug=all -Dnewt.debug=all -Djogl.debug.DebugGL"
-    #D_ARGS="-Dnewt.debug=all"
+    D_ARGS="-Dnewt.debug=all"
     #D_ARGS="-Djogl.debug=all -Dnewt.debug=all"
     #D_ARGS="-Djogl.debug=all -Dnativewindow.debug=all"
     #D_ARGS="-Djogamp.debug=all -Dnativewindow.debug=all -Djogl.debug=all -Dnewt.debug=all"
@@ -231,7 +231,7 @@ function jrun() {
     #D_ARGS="-Dnewt.debug.EDT"
     #D_ARGS="-Dnewt.debug.Window -Dnewt.debug.Display -Dnewt.debug.EDT -Djogl.debug.GLContext"
     #D_ARGS="-Dnewt.debug.Window -Djogl.debug.Animator -Dnewt.debug.Screen"
-    D_ARGS="-Dnativewindow.debug.JAWT -Dnewt.debug.Window -Djogl.debug.GLJPanel -Djogl.debug.GLCanvas -Djogamp.debug.TempJarCache"
+    #D_ARGS="-Dnativewindow.debug.JAWT -Dnewt.debug.Window -Djogl.debug.GLJPanel -Djogl.debug.GLCanvas -Djogamp.debug.TempJarCache"
     #D_ARGS="-Dnewt.debug.Window.KeyEvent"
     #D_ARGS="-Dnewt.debug.Window.MouseEvent"
     #D_ARGS="-Dnewt.debug.Window.MouseEvent -Dnewt.debug.Window.KeyEvent"
@@ -400,7 +400,7 @@ function testawtswt() {
 #testnoawt com.jogamp.opengl.test.junit.jogl.acore.TestMainVersionGLWindowNEWT $*
 #testawt com.jogamp.opengl.test.junit.jogl.acore.TestMainVersionGLCanvasAWT $*
 #testnoawt com.jogamp.opengl.test.junit.jogl.acore.TestGLProfile00NEWT $*
-#testnoawt com.jogamp.opengl.test.junit.jogl.acore.TestGLProfile01NEWT $*
+testnoawt com.jogamp.opengl.test.junit.jogl.acore.TestGLProfile01NEWT $*
 #testawt com.jogamp.opengl.test.junit.jogl.acore.TestVersionSemanticsNOUI $*
 
 #
@@ -840,7 +840,7 @@ function testawtswt() {
 #testnoawt com.jogamp.opengl.test.junit.graph.demos.ui.UINewtDemo01 $*
 #testnoawt com.jogamp.opengl.test.junit.graph.demos.GPUTextNewtDemo $*
 #testnoawt com.jogamp.opengl.test.junit.graph.demos.GPURegionNewtDemo $*
-testnoawt com.jogamp.opengl.test.junit.graph.demos.GPUUISceneNewtDemo $*
+#testnoawt com.jogamp.opengl.test.junit.graph.demos.GPUUISceneNewtDemo $*
 #testawt com.jogamp.opengl.test.junit.graph.demos.GPUUISceneNewtCanvasAWTDemo $*
 
 #testnoawt com.jogamp.opengl.test.junit.jogl.demos.es2.av.MovieCube $*

src/jogl/classes/jogamp/opengl/GLDrawableFactoryImpl.java
@@ -96,7 +96,7 @@ public abstract class GLDrawableFactoryImpl extends GLDrawableFactory {
    * <p>
    * Caller shall skip probing in case surfaceless support is not possible,
    * e.g. OpenGL ES without {@code EGL_KHR_surfaceless_context} or EGL &lt; 1.5,
-   * or desktop OpenGL context &lt; 3.0.
+   * or desktop OpenGL context &lt; 3.0 and instead call {@link #setNoSurfacelessCtxQuirk(GLContext)}!
    * </p>
    *
    * @param context the context to probe, must be current
@@ -141,17 +141,36 @@ public abstract class GLDrawableFactoryImpl extends GLDrawableFactory {
           }
       }
       if( !noSurfacelessCtxQuirk && !allowsSurfacelessCtx ) {
-          final int quirk = GLRendererQuirks.NoSurfacelessCtx;
-          if(DEBUG || GLContext.DEBUG) {
-              System.err.println("Quirk: "+GLRendererQuirks.toString(quirk)+" -> "+device+": cause: probe");
-          }
-          final GLRendererQuirks glrq = context.getRendererQuirks();
-          if( null != glrq ) {
-              glrq.addQuirk(quirk);
-          }
-          GLRendererQuirks.addStickyDeviceQuirk(device, quirk);
+          setNoSurfacelessCtxQuirkImpl(device, context);
       }
       return allowsSurfacelessCtx;
+  }
+
+  /**
+   * Method will set the {@link GLRendererQuirks#NoSurfacelessCtx}
+   * if it has not been set already.
+   *
+   * @param context the context to probe, must be current
+   * @see GLRendererQuirks#NoSurfacelessCtx
+   */
+  protected final void setNoSurfacelessCtxQuirk(final GLContext context) {
+      final boolean noSurfacelessCtxQuirk = context.hasRendererQuirk(GLRendererQuirks.NoSurfacelessCtx);
+      if( !noSurfacelessCtxQuirk ) {
+          final GLDrawable origDrawable = context.getGLDrawable();
+          final AbstractGraphicsDevice device = origDrawable.getNativeSurface().getGraphicsConfiguration().getScreen().getDevice();
+          setNoSurfacelessCtxQuirkImpl(device, context);
+      }
+  }
+  private final void setNoSurfacelessCtxQuirkImpl(final AbstractGraphicsDevice device, final GLContext context) {
+      final int quirk = GLRendererQuirks.NoSurfacelessCtx;
+      if(DEBUG || GLContext.DEBUG) {
+          System.err.println("Quirk: "+GLRendererQuirks.toString(quirk)+" -> "+device+": cause: probe");
+      }
+      final GLRendererQuirks glrq = context.getRendererQuirks();
+      if( null != glrq ) {
+          glrq.addQuirk(quirk);
+      }
+      GLRendererQuirks.addStickyDeviceQuirk(device, quirk);
   }
 
   /**

src/jogl/classes/jogamp/opengl/egl/EGLDrawableFactory.java
@@ -745,6 +745,8 @@ public class EGLDrawableFactory extends GLDrawableFactoryImpl {
                                     if( probeSurfacelessCtx(context, false /* restoreDrawable */) ) {
                                         zeroDrawable = context.getGLDrawable();
                                     }
+                                } else {
+                                    setNoSurfacelessCtxQuirk(context);
                                 }
                                 rendererQuirks[0] = context.getRendererQuirks();
                                 ctxProfile[0] = context.getContextOptions();
@@ -814,7 +816,8 @@ public class EGLDrawableFactory extends GLDrawableFactoryImpl {
             }
 
             if (null != sr.device) {
-                // may cause JVM SIGSEGV:
+                // Issues eglTerminate(), which may cause SIGSEGV w/ NVIDIA 343.36 w/ TestGLProfile01NEWT
+                // May cause JVM SIGSEGV:
                 sr.device.close();
                 sr.device = null;
             }

src/jogl/classes/jogamp/opengl/windows/wgl/WindowsWGLDrawableFactory.java
@@ -356,6 +356,7 @@ public class WindowsWGLDrawableFactory extends GLDrawableFactoryImpl {
                     if( context.getGLVersionNumber().compareTo(GLContext.Version3_0) >= 0 ) {
                         allowsSurfacelessCtx = probeSurfacelessCtx(context, true /* restoreDrawable */);
                     } else {
+                        setNoSurfacelessCtxQuirk(context);
                         allowsSurfacelessCtx = false;
                     }
                     hasARBPixelFormat = context.isExtensionAvailable(WGL_ARB_pixel_format);

src/jogl/classes/jogamp/opengl/x11/glx/X11GLXDrawableFactory.java
@@ -300,6 +300,7 @@ public class X11GLXDrawableFactory extends GLDrawableFactoryImpl {
                 if( contextIsCurrent && context.getGLVersionNumber().compareTo(GLContext.Version3_0) >= 0 ) {
                     allowsSurfacelessCtx = probeSurfacelessCtx(context, true /* restoreDrawable */);
                 } else {
+                    setNoSurfacelessCtxQuirk(context);
                     allowsSurfacelessCtx = false;
                 }
 