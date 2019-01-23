cc157b6a7dfbdefbbfc31085b4013e395cfecd13
Sven Gothel <sgothel@jausoft.com>
2019-01-23 02:13:44 +0100

    osx test scripts using latest Java

--- make/scripts/tests-osx-x64.sh
+++ make/scripts/tests-osx-x64.sh
@@ -4,7 +4,8 @@
 #export DYLD_LIBRARY_PATH=$HOME/ffmpeg-2.2.3/lib:$DYLD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=/usr/local/Cellar/ffmpeg/2.8/lib:$DYLD_LIBRARY_PATH
 
-JAVA_HOME=`/usr/libexec/java_home -version 1.8`
+JAVA_HOME=`/usr/libexec/java_home -version`
+#JAVA_HOME=`/usr/libexec/java_home -version 1.8`
 #JAVA_HOME=`/usr/libexec/java_home -version 1.7`
 #JAVA_HOME=`/usr/libexec/java_home -version 1.7.0_25`
 #JAVA_HOME=`/usr/libexec/java_home -version 1.6.0`
