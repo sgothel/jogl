a307ab505ed3fb5fe64f01478ce6d2933cd26c7d
Sven Gothel <sgothel@jausoft.com>
2019-07-08 04:38:42 +0200

    Copyright: Add missing Copyright tag
    Adding the missing JogAmp copyright tag on certain files.
    
    Currently in debate whether 'JogAmp Community' is a legal Copyright tag in the first place,
    we might need to add (or replace it with) my authorship.
    However, as authorship is well documented via the git repository,
    this should be no real world issue.

--- src/newt/native/KDWindow.c
+++ src/newt/native/KDWindow.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2008 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright (c) 2010 JogAmp Community. All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
