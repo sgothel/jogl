c5fabde01babfdd07c72347ff17ac0705b8c6c5a
Sven Gothel <sgothel@jausoft.com>
2019-12-09 07:40:14 +0100

    Bug 1411 DRM/GBM JOGL: Fix 'junit.run.console' unit test target, use it in respective (renamed) scripts.
    Added junit.run.console target in build.xml
    
    build-test.xml
    - Moves all results.test artifact moving from current directory into test-zip-archive target,
      reducing duplication.
    
    - junit.run.console: Added proper dependencies etc
    
    Renamed scripts to match junit.run.console:
      junit-linux-x86_64-headless.sh -> junit-linux-x86_64-console.sh
      junit-linux-armv6hf-headless.sh -> junit-linux-armv6hf-console.sh

--- make/scripts/junit-linux-armv6hf-console.sh
+++ make/scripts/junit-linux-armv6hf-console.sh
@@ -6,8 +6,10 @@ if [ -e $SDIR/../../../gluegen/make/scripts/setenv-build-jogl-x86_64.sh ] ; then
     . $SDIR/../../../gluegen/make/scripts/setenv-build-jogl-x86_64.sh
 fi
 
-LOGF=junit.jogl.all.linux-x86_64-headless.log
+LOGF=junit.jogl.all.linux-armv6hf-console.log
 rm -f $LOGF
+
+export GLUEGEN_CPPTASKS_FILE="../../gluegen/make/lib/gluegen-cpptasks-linux-armv6hf.xml"
 
 export SOURCE_LEVEL=1.8
 export TARGET_LEVEL=1.8
@@ -16,11 +18,17 @@ export TARGET_RT_JAR=/opt-share/jre1.8.0_212/lib/rt.jar
 #export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
 export JOGAMP_JAR_CODEBASE="Codebase: *.goethel.localnet"
 
+#TARGET=junit.run
+TARGET=junit.run.console
+
 # BUILD_ARCHIVE=true \
 ant  \
     -Dsetup.noAWT=true \
     -Dsetup.noSWT=true \
-    -Drootrel.build=build-x86_64 \
+    -Drootrel.build=build-linux-armv6hf \
     -Djunit.run.arg0="--illegal-access=warn" \
-    junit.run 2>&1 | tee -a $LOGF
+    -Dsetup.addNativeKD=true \
+    -Dsetup.addNativeOpenMAX=true \
+    -Dsetup.addNativeBroadcom=true \
+    $TARGET 2>&1 | tee -a $LOGF
 
--- make/scripts/junit-linux-x86_64-console.sh
+++ make/scripts/junit-linux-x86_64-console.sh
@@ -6,10 +6,8 @@ if [ -e $SDIR/../../../gluegen/make/scripts/setenv-build-jogl-x86_64.sh ] ; then
     . $SDIR/../../../gluegen/make/scripts/setenv-build-jogl-x86_64.sh
 fi
 
-LOGF=junit.jogl.all.linux-armv6hf-headless.log
+LOGF=junit.jogl.all.linux-x86_64-console.log
 rm -f $LOGF
-
-export GLUEGEN_CPPTASKS_FILE="../../gluegen/make/lib/gluegen-cpptasks-linux-armv6hf.xml"
 
 export SOURCE_LEVEL=1.8
 export TARGET_LEVEL=1.8
@@ -18,14 +16,14 @@ export TARGET_RT_JAR=/opt-share/jre1.8.0_212/lib/rt.jar
 #export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
 export JOGAMP_JAR_CODEBASE="Codebase: *.goethel.localnet"
 
+#TARGET=junit.run
+TARGET=junit.run.console
+
 # BUILD_ARCHIVE=true \
 ant  \
     -Dsetup.noAWT=true \
     -Dsetup.noSWT=true \
-    -Drootrel.build=build-linux-armv6hf \
+    -Drootrel.build=build-x86_64 \
     -Djunit.run.arg0="--illegal-access=warn" \
-    -Dsetup.addNativeKD=true \
-    -Dsetup.addNativeOpenMAX=true \
-    -Dsetup.addNativeBroadcom=true \
-    junit.run 2>&1 | tee -a $LOGF
+    $TARGET 2>&1 | tee -a $LOGF
 
