d91f8581eedf73663abab25524147d2bcab9109b
Sven Gothel <sgothel@jausoft.com>
2019-12-26 05:02:50 +0100

    Bug 1417 - Android: Add adb scripts for x86 [re-]install

---
+++ make/scripts/adb-install-all-x86.sh
@@ -0,0 +1,4 @@
+#adb $* install ../build-android-x86/jogamp-android-launcher.apk
+#adb $* install ../build-android-x86/gluegen-rt-android-x86.apk
+adb $* install ../build-android-x86/jar/jogl-all-android-x86.apk
+adb $* install ../build-android-x86/jar/jogl-test-android.apk
