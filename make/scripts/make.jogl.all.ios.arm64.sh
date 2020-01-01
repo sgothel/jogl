4d070e6be4f6f5b2eeb4f29e5d3fe7d0fdc4c7ad
Sven Gothel <sgothel@jausoft.com>
2020-01-02 00:34:56 +0100

    iOS build scripts: Bump iOS 12.2 -> 13.2; Use GLUEGEN_CPPTASKS_FILE instead of -DisIOS*=true

--- make/scripts/make.jogl.all.ios.arm64.sh
+++ make/scripts/make.jogl.all.ios.arm64.sh
@@ -9,7 +9,7 @@ fi
 # Force OSX SDK 10.6, if desired
 # export SDKROOT=macosx10.6
 
-export SDKROOT=iphoneos12.2
+export SDKROOT=iphoneos13.2
 xcrun --show-sdk-path
 
 JAVA_HOME=`/usr/libexec/java_home -version 11`
@@ -22,10 +22,11 @@ export SOURCE_LEVEL=1.8
 export TARGET_LEVEL=1.8
 export TARGET_RT_JAR=/usr/local/jre1.8.0_212/lib/rt.jar
 
+export GLUEGEN_CPPTASKS_FILE=make/lib/gluegen-cpptasks-ios-aarch64.xml
+
 #export JOGAMP_JAR_CODEBASE="Codebase: *.jogamp.org"
 export JOGAMP_JAR_CODEBASE="Codebase: *.goethel.localnet"
 
 ant \
     -Drootrel.build=build-ios-arm64 \
-    -DisIOSArm64=true \
     $* 2>&1 | tee make.jogl.all.ios-arm64.log
