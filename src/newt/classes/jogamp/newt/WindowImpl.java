0209655c26e9240639c5f0a76ca6ca54ae0584b1
Sven Gothel <sgothel@jausoft.com>
2020-01-06 18:31:44 +0100

    Bug 1421: Minor commenting

--- src/newt/classes/jogamp/newt/WindowImpl.java
+++ src/newt/classes/jogamp/newt/WindowImpl.java
@@ -2912,8 +2912,7 @@ public abstract class WindowImpl implements Window, NEWTEventConsumer
                 }
                 // Let the window be positioned if !fullscreen and position changed or being a child window.
                 if ( ( isReconfigureMaskSupported(STATE_MASK_REPOSITIONABLE) || !isNativeValid() ) &&
-                     !isFullscreen() &&
-                     ( getX() != x || getY() != y || null != getParent())
+                     !isFullscreen() && ( getX() != x || getY() != y || null != getParent() /* Bug 969 comment 2 */ )
                    )
                 {
                     if(isNativeValid()) {
