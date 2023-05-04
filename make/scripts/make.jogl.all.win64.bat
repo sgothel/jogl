1e9adf43f23b6780a6885dfe6bf3d06e1147f0c3
Sven Gothel <sgothel@jausoft.com>
2023-05-04 03:26:31 +0200

    HowToBuild: Windows: We use MinGW64 w/ 'posix threads' instead of 'win32 threads', allowing C++ std::mutex etc to be used (openal-soft)

--- make/scripts/make.jogl.all.win64.bat
+++ make/scripts/make.jogl.all.win64.bat
@@ -6,7 +6,7 @@ set ANT_PATH=C:\apache-ant-1.10.5
 set GIT_PATH=C:\cygwin64\bin
 set SEVENZIP=C:\Program Files\7-Zip
 
-set CMAKE_PATH=C:\cmake-3.15.2-win64-x64
+set CMAKE_PATH=C:\cmake-3.25.1-windows-x86_64
 set CMAKE_C_COMPILER=c:\mingw64\bin\gcc
 
 set PATH=%J2RE_HOME%\bin;%JAVA_HOME%\bin;%ANT_PATH%\bin;c:\mingw64\bin;%CMAKE_PATH%\bin;%GIT_PATH%;%SEVENZIP%;%PATH%
