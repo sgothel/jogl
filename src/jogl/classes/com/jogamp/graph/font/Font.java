sgothel/jogl

commit 920e529516bb264f04138ed1caca80d4925e3773

Graph Font + Glyph: More robust detetection and API definition of non-contour/whitespace Glyphs (detect and allow to skip 'em)
We also drop shapes for both, but for id 0 (unknown).

src/jogl/classes/com/jogamp/graph/font/Font.java
@@ -175,11 +175,42 @@ public interface Font {
         /** Return the glyph's name, source from `post` table */
         String getName();
 
-        /** Return true if the underlying {@link #getShape()} is a whitespace, otherwise false. */
-        boolean isWhiteSpace();
+        /**
+         * Return true if the glyph is a whitespace, otherwise false.
+         * <p>
+         * A whitespace glyph has no {@link #getShape()}, but a valid {@link #getBounds()} and {@link #getAdvance()}.
+         * </p>
+         * Being a whitespace glyph excludes {@link #isUndefined()}.
+         * @see #isUndefined()
+         * @see #isNonContour()
+         */
+        boolean isWhitespace();
 
-        /** Return true if the Glyph denotes an undefined {@link #getID()} symbol, i.e. {@link #getName()} == `.notdef`. */
+        /**
+         * Return true if the Glyph denotes an undefined {@link #getID()} symbol as follows
+         * <ul>
+         *   <li>it's glyph index is {@link #ID_UNKNOWN}, i.e. {@code 0x00}</li>
+         *   <li>has the {@link #getName() name} `.notdef`, `NULL`, `null` or `.null`</li>
+         *   <li>has no original underlying shape</li>
+         * </ul>
+         * <p>
+         * An undefined glyph has no {@link #getShape()}  if glyph index is not {@link #ID_UNKNOWN}.
+         * </p>
+         * <p>
+         * An undefined glyph has a default {@link #getBounds()} and {@link #getAdvance()}.
+         * </p>
+         * Being an undefined shape excludes {@link #isWhitespace()}.
+         * @see #isWhitespace()
+         * @see #isNonContour()
+         */
         boolean isUndefined();
+
+        /**
+         * Returns true if {@link #isWhitespace()} or {@link #isUndefined()}.
+         * @see #isWhitespace()
+         * @see #isUndefined()
+         */
+        boolean isNonContour();
 
         /**
          * Return the AABBox in font-units, borrowing internal instance.

src/jogl/classes/jogamp/graph/font/typecast/TypecastFont.java
@@ -48,9 +48,10 @@ import jogamp.graph.font.typecast.ot.table.KernSubtableFormat0;
 import jogamp.graph.font.typecast.ot.table.KernTable;
 import jogamp.graph.font.typecast.ot.table.KerningPair;
 import jogamp.graph.font.typecast.ot.table.PostTable;
+import jogamp.opengl.Debug;
 
 class TypecastFont implements Font {
-    static final boolean DEBUG = false;
+    private static final boolean DEBUG = Debug.debug("graph.font.Font");
 
     // private final OTFontCollection fontset;
     /* pp */ final TTFont font;
@@ -203,34 +204,36 @@ class TypecastFont implements Font {
             } else {
                 glyph_name = "";
             }
+            final boolean isUndefined = Glyph.ID_UNKNOWN == glyph_id || null == glyph || TypecastGlyph.isUndefName(glyph_name);
             final int glyph_height = metrics.getAscentFU() - metrics.getDescentFU();
             final int glyph_advance;
             final int glyph_leftsidebearings;
+            final boolean isWhitespace;
             final AABBox glyph_bbox;
             final OutlineShape shape;
-            final boolean isWhiteSpace;
             if( null != glyph ) {
                 glyph_advance = glyph.getAdvanceWidth();
                 glyph_leftsidebearings = glyph.getLeftSideBearing();
                 final AABBox sb = glyph.getBBox();
-                final OutlineShape s = TypecastRenderer.buildShape(metrics.getUnitsPerEM(), glyph);
-                if( 0 < s.getVertexCount() ) {
+                final OutlineShape os = TypecastRenderer.buildShape(metrics.getUnitsPerEM(), glyph);
+                if( 0 < os.getVertexCount() ) {
+                    // Case 1: Either valid contour glyph, undefined or a whitespace (Case 2 with zero-area shape)
+                    isWhitespace = isUndefined ? false : os.getBounds().hasZero2DArea();
                     glyph_bbox = sb;
-                    shape = s;
-                    isWhiteSpace = false;
+                    shape = ( !isWhitespace && !isUndefined ) || Glyph.ID_UNKNOWN == glyph_id ? os : null;
                 } else {
-                    // non-contour glyph -> whitespace
+                    // Case 2: Non-contour glyph -> whitespace or undefined
+                    isWhitespace = !isUndefined;
                     glyph_bbox = new AABBox(0f,0f,0f, glyph_advance, glyph_height, 0f);
-                    shape = TypecastRenderer.buildEmptyShape(metrics.getUnitsPerEM(), glyph_bbox);
-                    isWhiteSpace = true;
+                    shape = Glyph.ID_UNKNOWN == glyph_id ? TypecastRenderer.buildEmptyShape(metrics.getUnitsPerEM(), glyph_bbox) : null;
                 }
             } else {
-                // non-contour glyph -> whitespace
+                // Case 3: Non-contour glyph -> undefined
                 glyph_advance = getAdvanceWidthFU(glyph_id);
                 glyph_leftsidebearings = 0;
+                isWhitespace = false;
                 glyph_bbox = new AABBox(0f,0f,0f, glyph_advance, glyph_height, 0f);
-                shape = TypecastRenderer.buildEmptyShape(metrics.getUnitsPerEM(), glyph_bbox);
-                isWhiteSpace = true;
+                shape = Glyph.ID_UNKNOWN == glyph_id ? TypecastRenderer.buildEmptyShape(metrics.getUnitsPerEM(), glyph_bbox) : null;
             }
             KernSubtable kernSub = null;
             {
@@ -239,8 +242,9 @@ class TypecastFont implements Font {
                     kernSub = kern.getSubtable0();
                 }
             }
-            result = new TypecastGlyph(this, glyph_id, glyph_name, glyph_bbox, glyph_advance, glyph_leftsidebearings, kernSub, shape, isWhiteSpace);
-            if(DEBUG) {
+            result = new TypecastGlyph(this, glyph_id, glyph_name, glyph_bbox, glyph_advance, glyph_leftsidebearings, kernSub, shape,
+                                       isUndefined, isWhitespace);
+            if( DEBUG || TypecastRenderer.DEBUG ) {
                 System.err.println("New glyph: " + glyph_id + "/'"+glyph_name+"', shape " + (null != shape));
                 System.err.println("  tc_glyph "+glyph);
                 System.err.println("     glyph "+result);
@@ -340,22 +344,24 @@ class TypecastFont implements Font {
                 temp1.setToIdentity();
                 final int glyph_id = getGlyphID(character);
                 final Font.Glyph glyph = getGlyph(glyph_id);
-                final OutlineShape glyphShape = glyph.getShape();
-                if( null == glyphShape ) { // also covers 'space' and all non-contour symbols
+                if( glyph.isUndefined() ) {
+                    // break kerning, drop undefined
                     advanceTotal += glyph.getAdvanceFU();
-                    left_glyph = null; // break kerning
-                    continue;
-                } else if( glyph.isWhiteSpace() ) { // covers 'space' and all non-contour symbols
-                    left_glyph = null; // break kerning
-                }
-                if( null != left_glyph ) {
-                    advanceTotal += left_glyph.getKerningFU(glyph_id);
-                }
-                temp1.translate(advanceTotal, y, temp2);
-                res.resize(temp1.transform(glyph.getBoundsFU(), temp_box));
-
-                advanceTotal += glyph.getAdvanceFU();
-                if( !glyph.isWhiteSpace() ) {
+                    left_glyph = null;
+                } else if( glyph.isWhitespace() ) {
+                    // break kerning, but include its bounding box space
+                    left_glyph = null;
+                    temp1.translate(advanceTotal, y, temp2);
+                    res.resize(temp1.transform(glyph.getBoundsFU(), temp_box));
+                    advanceTotal += glyph.getAdvanceFU();
+                } else {
+                    // regular contour
+                    if( null != left_glyph ) {
+                        advanceTotal += left_glyph.getKerningFU(glyph_id);
+                    }
+                    temp1.translate(advanceTotal, y, temp2);
+                    res.resize(temp1.transform(glyph.getBoundsFU(), temp_box));
+                    advanceTotal += glyph.getAdvanceFU();
                     left_glyph = glyph;
                 }
             }
@@ -421,23 +427,26 @@ class TypecastFont implements Font {
                 final int glyph_id = getGlyphID(character);
 
                 final Font.Glyph glyph = getGlyph(glyph_id);
-                final OutlineShape glyphShape = glyph.getShape();
-
-                if( null == glyphShape ) { // also covers 'space' and all non-contour symbols
+                if( glyph.isUndefined() ) {
+                    // break kerning, drop undefined
                     advanceTotal += glyph.getAdvance();
-                    left_glyph = null; // break kerning
-                    continue;
-                } else if( glyph.isWhiteSpace() ) { // covers 'space' and all non-contour symbols
-                    left_glyph = null; // break kerning
-                }
-                if( null != left_glyph ) {
-                    advanceTotal += left_glyph.getKerning(glyph_id);
-                }
-                temp1.translate(advanceTotal, y, temp2);
-                res.resize(temp1.transform(glyphShape.getBounds(), temp_box));
-                visitor.visit(character, glyph, temp1);
-                advanceTotal += glyph.getAdvance();
-                if( !glyph.isWhiteSpace() ) {
+                    left_glyph = null;
+                } else if( glyph.isWhitespace() ) {
+                    // break kerning, but include its bounding box space and visit the visitor
+                    left_glyph = null;
+                    temp1.translate(advanceTotal, y, temp2);
+                    res.resize(temp1.transform(glyph.getBounds(), temp_box));
+                    visitor.visit(character, glyph, temp1);
+                    advanceTotal += glyph.getAdvance();
+                } else {
+                    // regular contour
+                    if( null != left_glyph ) {
+                        advanceTotal += left_glyph.getKerning(glyph_id);
+                    }
+                    temp1.translate(advanceTotal, y, temp2);
+                    res.resize(temp1.transform(glyph.getShape().getBounds(), temp_box));
+                    visitor.visit(character, glyph, temp1);
+                    advanceTotal += glyph.getAdvance();
                     left_glyph = glyph;
                 }
             }

src/graphui/classes/com/jogamp/graph/ui/AnimGroup.java
@@ -406,7 +406,7 @@ public class AnimGroup extends Group {
         final Font.GlyphVisitor fgv = new Font.GlyphVisitor() {
             @Override
             public void visit(final char symbol, final Glyph glyph, final AffineTransform t) {
-                if( !glyph.isWhiteSpace() && null != glyph.getShape() ) {
+                if( !glyph.isNonContour() ) {
                     final GlyphShape gs = new GlyphShape(renderModes, symbol, glyph, t.getTranslateX(), t.getTranslateY());
                     gs.setScale(fontScale, fontScale, 1f);
                     gs.moveTo(gs.getOrigPos().x()*fontScale, gs.getOrigPos().y()*fontScale, gs.getOrigPos().z());

src/graphui/classes/com/jogamp/graph/ui/shapes/GlyphShape.java
@@ -71,7 +71,7 @@ public class GlyphShape extends GraphShape {
         this.symbol = symbol;
         this.glyph = glyph;
         this.origPos = new Vec3f(x, y, 0f);
-        if( glyph.isWhiteSpace() || null == glyph.getShape() ) {
+        if( glyph.isNonContour() ) {
             setEnabled(false);
         }
         final int[/*2*/] vertIndexCount = Region.countOutlineShape(glyph.getShape(), new int[2]);
@@ -139,7 +139,7 @@ public class GlyphShape extends GraphShape {
         final Font.GlyphVisitor fgv = new Font.GlyphVisitor() {
             @Override
             public void visit(final char symbol, final Glyph glyph, final AffineTransform t) {
-                if( !glyph.isWhiteSpace() && null != glyph.getShape() ) {
+                if( !glyph.isNonContour() ) {
                     res.add( new GlyphShape(renderModes, symbol, glyph, t.getTranslateX(), t.getTranslateY()) );
                 }
             }

src/graphui/classes/com/jogamp/graph/ui/shapes/Label.java
@@ -193,12 +193,11 @@ public class Label extends GraphShape {
     private final Font.GlyphVisitor glyphVisitor = new Font.GlyphVisitor() {
         @Override
         public void visit(final char symbol, final Glyph glyph, final AffineTransform t) {
-            if( glyph.isWhiteSpace() ) {
-                return;
+            if( !glyph.isNonContour() ) {
+                final OutlineShape shape = glyph.getShape();
+                shape.setSharpness(oshapeSharpness);
+                region.addOutlineShape(shape, t, rgbaColor);
             }
-            final OutlineShape shape = glyph.getShape();
-            shape.setSharpness(oshapeSharpness);
-            region.addOutlineShape(shape, t, rgbaColor);
         }
     };
 

src/jogl/classes/com/jogamp/graph/curve/opengl/GLRegion.java
@@ -188,7 +188,9 @@ public abstract class GLRegion extends Region {
         final Font.GlyphVisitor2 visitor = new Font.GlyphVisitor2() {
             @Override
             public final void visit(final char symbol, final Font.Glyph glyph) {
-                Region.countOutlineShape(glyph.getShape(), vertIndexCount);
+                if( !glyph.isNonContour() ) {
+                    Region.countOutlineShape(glyph.getShape(), vertIndexCount);
+                }
             } };
         font.processString(visitor, str);
         return GLRegion.create(glp, renderModes, colorTexSeq, vertIndexCount[0], vertIndexCount[1]);

src/jogl/classes/com/jogamp/graph/curve/opengl/TextRegionUtil.java
@@ -145,10 +145,9 @@ public class TextRegionUtil {
         final Font.GlyphVisitor visitor = new Font.GlyphVisitor() {
             @Override
             public void visit(final char symbol, final Glyph glyph, final AffineTransform t) {
-                if( glyph.isWhiteSpace() ) {
-                    return;
+                if( !glyph.isNonContour() ) {
+                    region.addOutlineShape(glyph.getShape(), t, rgbaColor);
                 }
-                region.addOutlineShape(glyph.getShape(), t, rgbaColor);
             }
         };
         if( preGrowRegion ) {
@@ -175,7 +174,9 @@ public class TextRegionUtil {
         final Font.GlyphVisitor2 visitor = new Font.GlyphVisitor2() {
             @Override
             public final void visit(final char symbol, final Font.Glyph glyph) {
-                Region.countOutlineShape(glyph.getShape(), vertIndexCount);
+                if( !glyph.isNonContour() ) {
+                    Region.countOutlineShape(glyph.getShape(), vertIndexCount);
+                }
             } };
         font.processString(visitor, str);
         return vertIndexCount;

src/jogl/classes/com/jogamp/opengl/math/geom/AABBox.java
@@ -784,6 +784,26 @@ public class AABBox {
         return high.z() - low.z();
     }
 
+    /** Returns the volume, i.e. width * height * depth */
+    public final float getVolume() {
+        return getWidth() * getHeight() * getDepth();
+    }
+
+    /** Return true if {@link #getVolume()} is {@link FloatUtil#isZero(float)}, considering epsilon. */
+    public final boolean hasZeroVolume() {
+        return FloatUtil.isZero(getVolume());
+    }
+
+    /** Returns the assumed 2D area, i.e. width * height while assuming low and high lies on same plane. */
+    public final float get2DArea() {
+        return getWidth() * getHeight();
+    }
+
+    /** Return true if {@link #get2DArea()} is {@link FloatUtil#isZero(float)}, considering epsilon. */
+    public final boolean hasZero2DArea() {
+        return FloatUtil.isZero(get2DArea());
+    }
+
     @Override
     public final boolean equals(final Object obj) {
         if( obj == this ) {

commit f6dd8e9562d7e0bbf681c268f40ff4c819057915

Graph Font + Glyph: Fix whitespace definition: Include 'no original underlying shape' and add API doc
Regression was introduced with commit 920e529516bb264f04138ed1caca80d4925e3773
'Robust detetection and API definition of non-contour/whitespace Glyphs'.

Issue was mistaken a glyph as undefined if not having an underlying shape,
which is true for some fonts (e.g. 'space').

+++

Also Use post table's name if no underlying shape exists.

src/jogl/classes/com/jogamp/graph/font/Font.java
@@ -176,22 +176,10 @@ public interface Font {
         String getName();
 
         /**
-         * Return true if the glyph is a whitespace, otherwise false.
-         * <p>
-         * A whitespace glyph has no {@link #getShape()}, but a valid {@link #getBounds()} and {@link #getAdvance()}.
-         * </p>
-         * Being a whitespace glyph excludes {@link #isUndefined()}.
-         * @see #isUndefined()
-         * @see #isNonContour()
-         */
-        boolean isWhitespace();
-
-        /**
-         * Return true if the Glyph denotes an undefined {@link #getID()} symbol as follows
+         * Return true if the Glyph denotes an undefined {@link #getID()} symbol, determined as follows
          * <ul>
          *   <li>it's glyph index is {@link #ID_UNKNOWN}, i.e. {@code 0x00}</li>
          *   <li>has the {@link #getName() name} `.notdef`, `NULL`, `null` or `.null`</li>
-         *   <li>has no original underlying shape</li>
          * </ul>
          * <p>
          * An undefined glyph has no {@link #getShape()}  if glyph index is not {@link #ID_UNKNOWN}.
@@ -204,6 +192,22 @@ public interface Font {
          * @see #isNonContour()
          */
         boolean isUndefined();
+
+        /**
+         * Return true if the Glyph denotes a whitespace, determined as follows
+         * <ul>
+         *   <li>is not {@link #isUndefined()}</li>
+         *   <li>has no original underlying shape</li>
+         *   <li>has an underlying shape with a zero sized area</li>
+         * </ul>
+         * <p>
+         * A whitespace glyph has no {@link #getShape()}, but a valid {@link #getBounds()} and {@link #getAdvance()}.
+         * </p>
+         * Being a whitespace glyph excludes {@link #isUndefined()}.
+         * @see #isUndefined()
+         * @see #isNonContour()
+         */
+        boolean isWhitespace();
 
         /**
          * Returns true if {@link #isWhitespace()} or {@link #isUndefined()}.

src/jogl/classes/jogamp/graph/font/typecast/TypecastFont.java
@@ -198,19 +198,18 @@ class TypecastFont implements Font {
         if (null == result) {
             final jogamp.graph.font.typecast.ot.Glyph glyph = font.getGlyph(glyph_id);
             final String glyph_name;
-            if( null != glyph ) {
+            {
                 final PostTable post = font.getPostTable();
                 glyph_name = null != post ? post.getGlyphName(glyph_id) : "";
-            } else {
-                glyph_name = "";
             }
-            final boolean isUndefined = Glyph.ID_UNKNOWN == glyph_id || null == glyph || TypecastGlyph.isUndefName(glyph_name);
+            final boolean isUndefined = Glyph.ID_UNKNOWN == glyph_id || TypecastGlyph.isUndefName(glyph_name);
             final int glyph_height = metrics.getAscentFU() - metrics.getDescentFU();
             final int glyph_advance;
             final int glyph_leftsidebearings;
             final boolean isWhitespace;
             final AABBox glyph_bbox;
             final OutlineShape shape;
+            final int mode;
             if( null != glyph ) {
                 glyph_advance = glyph.getAdvanceWidth();
                 glyph_leftsidebearings = glyph.getLeftSideBearing();
@@ -221,19 +220,22 @@ class TypecastFont implements Font {
                     isWhitespace = isUndefined ? false : os.getBounds().hasZero2DArea();
                     glyph_bbox = sb;
                     shape = ( !isWhitespace && !isUndefined ) || Glyph.ID_UNKNOWN == glyph_id ? os : null;
+                    mode = 1;
                 } else {
                     // Case 2: Non-contour glyph -> whitespace or undefined
                     isWhitespace = !isUndefined;
                     glyph_bbox = new AABBox(0f,0f,0f, glyph_advance, glyph_height, 0f);
                     shape = Glyph.ID_UNKNOWN == glyph_id ? TypecastRenderer.buildEmptyShape(metrics.getUnitsPerEM(), glyph_bbox) : null;
+                    mode = 2;
                 }
             } else {
-                // Case 3: Non-contour glyph -> undefined
+                // Case 3: Non-contour glyph -> whitespace or undefined
                 glyph_advance = getAdvanceWidthFU(glyph_id);
                 glyph_leftsidebearings = 0;
-                isWhitespace = false;
+                isWhitespace = !isUndefined;
                 glyph_bbox = new AABBox(0f,0f,0f, glyph_advance, glyph_height, 0f);
                 shape = Glyph.ID_UNKNOWN == glyph_id ? TypecastRenderer.buildEmptyShape(metrics.getUnitsPerEM(), glyph_bbox) : null;
+                mode = 3;
             }
             KernSubtable kernSub = null;
             {
@@ -245,7 +247,7 @@ class TypecastFont implements Font {
             result = new TypecastGlyph(this, glyph_id, glyph_name, glyph_bbox, glyph_advance, glyph_leftsidebearings, kernSub, shape,
                                        isUndefined, isWhitespace);
             if( DEBUG || TypecastRenderer.DEBUG ) {
-                System.err.println("New glyph: " + glyph_id + "/'"+glyph_name+"', shape " + (null != shape));
+                System.err.println("New glyph: " + glyph_id + "/'"+glyph_name+"', shape " + (null != shape)+", mode "+mode);
                 System.err.println("  tc_glyph "+glyph);
                 System.err.println("     glyph "+result);
             }