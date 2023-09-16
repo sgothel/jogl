b81cf8ca025f2a525a192ec24c63f95cb54b7444
Sven Gothel <sgothel@jausoft.com>
2023-09-17 01:46:25 +0200

    etc/test.bat: Override test.log logfile, don't append

--- etc/test.bat
+++ etc/test.bat
@@ -9,10 +9,10 @@ set X_ARGS="-Dsun.java2d.noddraw=true" "-Dsun.awt.noerasebackground=true"
 REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.opengl.awt.GLCanvas > java-win64.log 2>&1
 
 REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.common.GlueGenVersion > test.log 2>&1
-REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.nativewindow.NativeWindowVersion >> test.log 2>&1
-REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.opengl.JoglVersion >> test.log 2>&1
-REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.newt.NewtVersion >> test.log 2>&1
-REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.newt.opengl.GLCanvas >> test.log 2>&1
-java -classpath %CP_ALL% %X_ARGS% com.jogamp.newt.opengl.GLWindow >> test.log 2>&1
+REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.nativewindow.NativeWindowVersion > test.log 2>&1
+REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.opengl.JoglVersion > test.log 2>&1
+REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.newt.NewtVersion > test.log 2>&1
+REM java -classpath %CP_ALL% %X_ARGS% com.jogamp.newt.opengl.GLCanvas > test.log 2>&1
+java -classpath %CP_ALL% %X_ARGS% com.jogamp.newt.opengl.GLWindow > test.log 2>&1
 
 type test.log
