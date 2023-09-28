sgothel/jogl

commit ad38d1559854985b1131e5b6c7274a392b5bc265

NEWT X11: Fix positionChanged: We need to translate the 0/0 client position to the root window (global screen) ..
.. otherwise, we might end up receiving a client position of 0/0, while being positioned in a different absolute place within root.

src/newt/native/X11Display.c
@@ -758,13 +758,34 @@ JNIEXPORT void JNICALL Java_jogamp_newt_driver_x11_DisplayDriver_DispatchMessage
                 if ( evt.xconfigure.window == evt.xconfigure.event ) {
                     // ignore child window change notification
                     // insets: negative values are ignored
+                    int x_pos = evt.xconfigure.x;
+                    int y_pos = evt.xconfigure.y;
+                    {
+                        Window winRoot, winTopParent;
+                        Bool translated = False;
+                        if( 0 != NewtWindows_getRootAndParent(dpy, evt.xconfigure.window, &winRoot, &winTopParent) ) {
+                            int x_return=-1, y_return=-1;
+                            Window child;
+                            if( True == XTranslateCoordinates(dpy, evt.xconfigure.window, winRoot, 0, 0, &x_return, &y_return, &child) ) {
+                                DBG_PRINT( "X11: event . ConfigureNotify call %p POS (Xtrans) %d/%d -> %d/%d\n", 
+                                    (void*)evt.xconfigure.window, x_pos, y_pos, x_return, y_return);
+                                x_pos = x_return;
+                                y_pos = y_return;
+                                translated = True;
+                            }
+                        }
+                        if( False == translated ) {
+                            DBG_PRINT( "X11: event . ConfigureNotify call %p POS (raw) %d/%d\n", 
+                                (void*)evt.xconfigure.window, x_pos, y_pos);
+                        }
+                    }
                     int left=-1, right=-1, top=-1, bottom=-1;
                     uint32_t netWMState = NewtWindows_getNET_WM_STATE(dpy, jw);
                     int visibleChange = NewtWindows_updateVisibility(env, dpy, jw, netWMState, "ConfigureNotify");
                     NewtWindows_updateInsets(dpy, jw, False /* wait */, &left, &right, &top, &bottom);
                     Bool maxChanged = NewtWindows_updateMaximized(dpy, jw, netWMState);
                     (*env)->CallVoidMethod(env, jw->jwindow, sizePosMaxInsetsVisibleChangedID, JNI_FALSE, JNI_FALSE,
-                                            (jint) evt.xconfigure.x, (jint) evt.xconfigure.y,
+                                            (jint) x_pos, (jint) y_pos,
                                             (jint) evt.xconfigure.width, (jint) evt.xconfigure.height,
                                             (jint)(maxChanged ? ( jw->maxHorz ? 1 : 0 ) : -1), 
                                             (jint)(maxChanged ? ( jw->maxVert ? 1 : 0 ) : -1),

commit a0e6d5883de009d4649d011633090d7660fa229a

Bug 1431: Fix NewtCanvasAWT resize on X11, broken since commit ad38d1559854985b1131e5b6c7274a392b5bc265
Commit ad38d1559854985b1131e5b6c7274a392b5bc265 introduced XTranslateCoordinates(..) to savely validate
the client-space window position against the parent (root).

Totally missing in this change was the NEWT child window case
since it always used the root-window as the destination.

This change tracks the parent-window (valid parent Window or NULL)
within the JavaWindow struct and either uses the parent-window
if available or the root-window for XTranslateCoordinates(..).
This results in the proper client-space position.

Validated against
- TestGearsES2NewtCanvasAWT
- TestBug1431NewtCanvasAWT

on Debian 12 w/ Java17.

make/scripts/tests-x64.sh
@@ -21,6 +21,8 @@ fi
 export SWT_CLASSPATH=`pwd`/lib/swt/gtk-linux-x86_64/swt.jar
 #export SWT_CLASSPATH=/usr/local/projects/JOGL/SWT/swt-4.3.0/gtk-linux-x86_64/swt-debug.jar
 
+#export OPENJFX_CLASSPATH=/usr/share/openjfx/lib/javafx.base.jar:/usr/share/openjfx/lib/javafx.graphics.jar
+
 #JAVA_CMD=/opt-linux-x86_64/zulu19.32.13-ca-jdk19.0.2-linux_x64/bin/java
 JAVA_CMD=`which java`
 

make/scripts/tests.sh
@@ -91,6 +91,7 @@ echo LIBGL_DEBUG: $LIBGL_DEBUG 2>&1 | tee -a java-run.log
 echo LIBGL_ALWAYS_INDIRECT: $LIBGL_ALWAYS_INDIRECT 2>&1 | tee -a java-run.log
 echo LIBGL_ALWAYS_SOFTWARE: $LIBGL_ALWAYS_SOFTWARE 2>&1 | tee -a java-run.log
 echo SWT_CLASSPATH: $SWT_CLASSPATH 2>&1 | tee -a java-run.log
+echo OPENJFX_CLASSPATH: $OPENJFX_CLASSPATH 2>&1 | tee -a java-run.log
 echo MacOsX $MOSX 2>&1 | tee -a java-run.log
 echo DISPLAY $DISPLAY 2>&1 | tee -a java-run.log
 echo WAYLAND_DISPLAY $WAYLAND_DISPLAY 2>&1 | tee -a java-run.log
@@ -109,6 +110,7 @@ function jrun() {
 
     #X_ARGS="-Dsun.java2d.noddraw=True -Dsun.java2d.opengl=True -Dsun.java2d.xrender=false"
     #X_ARGS="-Dsun.java2d.noddraw=True -Dsun.java2d.opengl=false -Dsun.java2d.xrender=false"
+    #X_ARGS="-Dsun.awt.noerasebackground=true"
     #X_ARGS="-verbose:jni"
     #X_ARGS="-Xcheck:jni"
     #X_ARGS="-Xcheck:jni -verbose:jni"
@@ -329,7 +331,7 @@ function jrun() {
     #D_ARGS="-Dnativewindow.debug.NativeWindow"
     #D_ARGS="-Dnativewindow.osx.calayer.bugfree"
     #D_ARGS="-Dnativewindow.debug.ToolkitLock"
-    #D_ARGS="-Djogl.debug.graph.curve.Instance"
+    #D_ARGS="-Djogl.debug.graph.curve"
     #D_ARGS="-Djogl.debug.graph.curve.Instance -Djogl.debug.graph.curve.Triangulation"
     #D_ARGS="-Djogl.debug.graph.curve -Djogl.debug.graph.curve.Instance -Djogl.debug.graph.curve.Triangulation"
     #D_ARGS="-Djogl.debug.graph.curve"
@@ -420,6 +422,9 @@ function jrun() {
 
     if [ $swton -eq 1 ] ; then
         export USE_CLASSPATH=$USE_CLASSPATH:$JOGL_SWT_CLASSPATH
+    fi
+    if [ ! -z "$OPENJFX_CLASSPATH" ] ; then
+        export USE_CLASSPATH=$USE_CLASSPATH:$OPENJFX_CLASSPATH
     fi
     if [ ! -z "$CUSTOM_CLASSPATH" ] ; then
         export USE_CLASSPATH=$CUSTOM_CLASSPATH:$USE_CLASSPATH
@@ -532,6 +537,7 @@ function testawtswt() {
 #testawt com.jogamp.opengl.test.junit.jogl.awt.ManualHiDPIBufferedImage01AWT $*
 #testawt com.jogamp.opengl.test.junit.jogl.demos.es2.awt.TestGearsES2GLJPanelsAWT $*
 #testawt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestGearsES2NewtCanvasAWT $*
+#testawt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestBug1431NewtCanvasAWT $*
 #testawt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestLandscapeES2NewtCanvasAWT $*
 #testnoawt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestGearsES2NEWT $*
 #testawt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestGearsES2NEWT $*
@@ -919,6 +925,10 @@ function testawtswt() {
 #testawtswt com.jogamp.opengl.test.junit.newt.parenting.TestParenting04SWT $*
 #testawt com.jogamp.opengl.test.junit.newt.parenting.TestTranslucentParentingAWT $*
 #testnoawt com.jogamp.opengl.test.junit.newt.parenting.TestTranslucentChildWindowBug632NEWT $*
+#testawt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestBug1431NewtCanvasAWT $*
+testawt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestGearsES2NewtCanvasAWT $*
+#testnoawt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestGearsES2NEWT $*
+#testswt com.jogamp.opengl.test.junit.jogl.demos.es2.newt.TestGearsES2NewtCanvasSWT $*
 
 #
 # JavaFX (testjfx)
@@ -1004,7 +1014,7 @@ function testawtswt() {
 #testnoawt com.jogamp.opengl.demos.graph.ui.UISceneDemo10 $*
 #testnoawt com.jogamp.opengl.demos.graph.ui.UISceneDemo11 $*
 #testnoawt com.jogamp.opengl.demos.graph.ui.UISceneDemo20 $*
-testnoawt com.jogamp.opengl.demos.graph.ui.UIMediaGrid01 $*
+#testnoawt com.jogamp.opengl.demos.graph.ui.UIMediaGrid01 $*
 #testnoawt com.jogamp.opengl.demos.graph.ui.FontView01 $*
 #testawt com.jogamp.opengl.demos.graph.ui.UISceneDemo20 $*
 #testawt com.jogamp.opengl.demos.es2.GearsES2 $*

src/newt/native/X11Common.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011 JogAmp Community. All rights reserved.
+ * Copyright 2011-2023 JogAmp Community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
@@ -83,6 +83,7 @@ typedef struct {
 #define XI_TOUCHCOORD_COUNT 10
 
 typedef struct {
+    Window parentWindow;
     Window window;
     jobject jwindow;
     Atom * allAtoms;

src/newt/native/X11Display.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011 JogAmp Community. All rights reserved.
+ * Copyright 2011-2023 JogAmp Community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
@@ -764,11 +764,14 @@ JNIEXPORT void JNICALL Java_jogamp_newt_driver_x11_DisplayDriver_DispatchMessage
                         Window winRoot, winTopParent;
                         Bool translated = False;
                         if( 0 != NewtWindows_getRootAndParent(dpy, evt.xconfigure.window, &winRoot, &winTopParent) ) {
+                            DBG_PRINT( "X11: event . ConfigureNotify call %p (root %p, top %p)\n", 
+                                (void*)evt.xconfigure.window, (void*)winRoot, (void*)winTopParent);
                             int x_return=-1, y_return=-1;
+                            Window winDest = ( winTopParent == jw->parentWindow ) ? winTopParent : winRoot;
                             Window child;
-                            if( True == XTranslateCoordinates(dpy, evt.xconfigure.window, winRoot, 0, 0, &x_return, &y_return, &child) ) {
-                                DBG_PRINT( "X11: event . ConfigureNotify call %p POS (Xtrans) %d/%d -> %d/%d\n", 
-                                    (void*)evt.xconfigure.window, x_pos, y_pos, x_return, y_return);
+                            if( True == XTranslateCoordinates(dpy, evt.xconfigure.window, winDest, 0, 0, &x_return, &y_return, &child) ) {
+                                DBG_PRINT( "X11: event . ConfigureNotify call %p of dest %p POS (Xtrans) %d/%d -> %d/%d (child %p)\n", 
+                                    (void*)evt.xconfigure.window, winDest, x_pos, y_pos, x_return, y_return, child);
                                 x_pos = x_return;
                                 y_pos = y_return;
                                 translated = True;

src/newt/native/X11Window.c
@@ -1,6 +1,6 @@
 /*
+ * Copyright (c) 2010-2023 JogAmp Community. All rights reserved.
  * Copyright (c) 2008 Sun Microsystems, Inc. All Rights Reserved.
- * Copyright (c) 2010 JogAmp Community. All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -198,7 +198,7 @@ uint32_t NewtWindows_getNET_WM_STATE(Display *dpy, JavaWindow *w) {
     return res;
 }
 
-static JavaWindow* createJavaWindowProperty(JNIEnv *env, Display *dpy, Window root, Window window, 
+static JavaWindow* createJavaWindowProperty(JNIEnv *env, Display *dpy, Window root, Window parentWindow, Window window, 
                                             jlong javaObjectAtom, jlong windowDeleteAtom, jobject obj, Bool verbose) {
     jobject jwindow = (*env)->NewGlobalRef(env, obj);
     JavaWindow * res;
@@ -210,6 +210,7 @@ static JavaWindow* createJavaWindowProperty(JNIEnv *env, Display *dpy, Window ro
             return NULL;
         }
         res = calloc(1, sizeof(JavaWindow));
+        res->parentWindow = parentWindow;
         res->window = window;
         res->jwindow = jwindow;
         res->allAtoms = allAtoms;
@@ -920,7 +921,7 @@ JNIEXPORT jlongArray JNICALL Java_jogamp_newt_driver_x11_WindowDriver_CreateWind
     // XClearWindow(dpy, window);
 
     XSetWMProtocols(dpy, window, &wm_delete_atom, 1); // windowDeleteAtom
-    javaWindow = createJavaWindowProperty(env, dpy, root, window, javaObjectAtom, windowDeleteAtom, obj, verbose);
+    javaWindow = createJavaWindowProperty(env, dpy, root, (Window)parent, window, javaObjectAtom, windowDeleteAtom, obj, verbose);
 
     NewtWindows_setWindowTypeEWMH(dpy, javaWindow, _NET_WM_WINDOW_TYPE_NORMAL_IDX);
     NewtWindows_setDecorations(dpy, javaWindow, TST_FLAG_IS_UNDECORATED(flags) ? False : True );
@@ -1215,8 +1216,8 @@ JNIEXPORT void JNICALL Java_jogamp_newt_driver_x11_WindowDriver_reconfigureWindo
         fsEWMHFlags |= _MASK_NET_WM_STATE_BELOW;         // toggle below
     }
 
-    DBG_PRINT( "X11: reconfigureWindow0 dpy %p, scrn %d, parent %p/%p, win %p, %d/%d %dx%d, parentChange %d, isChild %d, undecorated[change %d, val %d], fullscreen[change %d, val %d (span %d)], alwaysOn[Top[change %d, val %d], Bottom[change %d, val %d]], visible[change %d, val %d, tempInvisible %d], resizable[change %d, val %d], sticky[change %d, val %d], fsEWMHFlags %d\n",
-        (void*)dpy, screen_index, (void*) jparent, (void*)parent, (void*)jw->window,
+    DBG_PRINT( "X11: reconfigureWindow0 dpy %p, scrn %d, parent %p -> %p (%p), win %p, %d/%d %dx%d, parentChange %d, isChild %d, undecorated[change %d, val %d], fullscreen[change %d, val %d (span %d)], alwaysOn[Top[change %d, val %d], Bottom[change %d, val %d]], visible[change %d, val %d, tempInvisible %d], resizable[change %d, val %d], sticky[change %d, val %d], fsEWMHFlags %d\n",
+        (void*)dpy, screen_index, (void*)jw->parentWindow, (void*) jparent, (void*)parent, (void*)jw->window,
         x, y, width, height, 
         TST_FLAG_CHANGE_PARENTING(flags),   TST_FLAG_IS_CHILD(flags),
         TST_FLAG_CHANGE_DECORATION(flags),  TST_FLAG_IS_UNDECORATED(flags),
@@ -1267,7 +1268,8 @@ JNIEXPORT void JNICALL Java_jogamp_newt_driver_x11_WindowDriver_reconfigureWindo
 
     if( TST_FLAG_CHANGE_PARENTING(flags) && !TST_FLAG_IS_CHILD(flags) ) {
         // TOP: in -> out
-        DBG_PRINT( "X11: reconfigureWindow0 PARENTING in->out\n");
+        DBG_PRINT( "X11: reconfigureWindow0 PARENTING in->out %p -> %p\n", (void*)jw->parentWindow, (void*)jparent);
+        jw->parentWindow = (Window)jparent;
         XReparentWindow( dpy, jw->window, parent, x, y ); // actual reparent call
         #ifdef REPARENT_WAIT_FOR_REPARENT_NOTIFY
             XIfEvent( dpy, &event, WaitForReparentNotify, (XPointer) jw->window );
@@ -1313,7 +1315,8 @@ JNIEXPORT void JNICALL Java_jogamp_newt_driver_x11_WindowDriver_reconfigureWindo
 
     if( TST_FLAG_CHANGE_PARENTING(flags) && TST_FLAG_IS_CHILD(flags) ) {
         // CHILD: out -> in
-        DBG_PRINT( "X11: reconfigureWindow0 PARENTING out->in\n");
+        DBG_PRINT( "X11: reconfigureWindow0 PARENTING out->in %p -> %p\n", (void*)jw->parentWindow, (void*)jparent);
+        jw->parentWindow = (Window)jparent;
         XReparentWindow( dpy, jw->window, parent, x, y ); // actual reparent call
         XFlush(dpy);
         #ifdef REPARENT_WAIT_FOR_REPARENT_NOTIFY

src/test/com/jogamp/opengl/test/junit/jogl/demos/es2/newt/TestBug1431NewtCanvasAWT.java
@@ -1,0 +1,237 @@
+package com.jogamp.opengl.test.junit.jogl.demos.es2.newt;
+
+import java.awt.Frame;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.jogamp.nativewindow.util.DimensionImmutable;
+import com.jogamp.newt.awt.NewtCanvasAWT;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.GL;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GL2ES1;
+import com.jogamp.opengl.GL2ES3;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.fixedfunc.GLLightingFunc;
+import com.jogamp.opengl.fixedfunc.GLMatrixFunc;
+import com.jogamp.opengl.test.junit.jogl.demos.es2.RedSquareES2;
+import com.jogamp.opengl.test.junit.util.AWTRobotUtil;
+import com.jogamp.opengl.test.junit.util.MiscUtils;
+import com.jogamp.opengl.test.junit.util.NewtTestUtil;
+import com.jogamp.opengl.test.junit.util.UITestCase;
+import com.jogamp.opengl.util.Animator;
+
+/**
+ * Self-contained example (within a single class only to keep it simple)
+ * displaying a rotating quad
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestBug1431NewtCanvasAWT extends UITestCase {
+
+    public static class JOGLQuadNewt implements GLEventListener {
+        final int[] exp_gl_viewport = { -1, -1, -1, -1 };
+        final int[] has_gl_viewport = { -1, -1, -1, -1 };
+        private float rotateT = 0.0f;
+
+        @Override
+        public void display(final GLAutoDrawable gLDrawable)
+        {
+            final GL2 gl = gLDrawable.getGL().getGL2();
+            gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+            gl.glClear(GL.GL_DEPTH_BUFFER_BIT);
+            gl.glLoadIdentity();
+            gl.glTranslatef(0.0f, 0.0f, -5.0f);
+
+            // rotate about the three axes
+            gl.glRotatef(rotateT, 1.0f, 0.0f, 0.0f);
+            gl.glRotatef(rotateT, 0.0f, 1.0f, 0.0f);
+            gl.glRotatef(rotateT, 0.0f, 0.0f, 1.0f);
+
+            // Draw A Quad
+            gl.glBegin(GL2ES3.GL_QUADS);
+            gl.glColor3f(0.0f, 1.0f, 1.0f); // set the color of the quad
+            gl.glVertex3f(-1.0f, 1.0f, 0.0f); // Top Left
+            gl.glVertex3f(1.0f, 1.0f, 0.0f); // Top Right
+            gl.glVertex3f(1.0f, -1.0f, 0.0f); // Bottom Right
+            gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
+            // Done Drawing The Quad
+            gl.glEnd();
+
+            // increasing rotation for the next iteration
+            rotateT += 0.2f;
+        }
+
+        @Override
+        public void init(final GLAutoDrawable glDrawable)
+        {
+            final GL2 gl = glDrawable.getGL().getGL2();
+            gl.glShadeModel(GLLightingFunc.GL_SMOOTH);
+            gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            gl.glClearDepth(1.0f);
+            gl.glEnable(GL.GL_DEPTH_TEST);
+            gl.glDepthFunc(GL.GL_LEQUAL);
+            gl.glHint(GL2ES1.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
+        }
+
+        @Override
+        public void reshape(final GLAutoDrawable glDrawable, final int x, final int y, final int width, final int height)
+        {
+            final GL2 gl = glDrawable.getGL().getGL2();
+            exp_gl_viewport[0] = x;
+            exp_gl_viewport[1] = y;
+            exp_gl_viewport[2] = width;
+            exp_gl_viewport[3] = height;
+            gl.glGetIntegerv(GL.GL_VIEWPORT, has_gl_viewport, 0);
+            System.err.println("GLEL reshape: Surface "+glDrawable.getSurfaceWidth()+"x"+glDrawable.getSurfaceWidth()+
+                             ", reshape "+x+"/"+y+" "+width+"x"+height);
+            System.err.println("GLEL reshape: Viewport "+has_gl_viewport[0]+"/"+has_gl_viewport[1]+", "+has_gl_viewport[2]+"x"+has_gl_viewport[3]);
+
+            final float aspect = (float) width / (float) height;
+            gl.glMatrixMode(GLMatrixFunc.GL_PROJECTION);
+            gl.glLoadIdentity();
+            final float fh = 0.5f;
+            final float fw = fh * aspect;
+            gl.glFrustumf(-fw, fw, -fh, fh, 1.0f, 1000.0f);
+            gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
+            gl.glLoadIdentity();
+        }
+
+        @Override
+        public void dispose(final GLAutoDrawable gLDrawable)
+        {}
+    }
+
+    static long duration = 500; // ms
+
+    static void setFrameSize(final Frame frame, final boolean frameLayout, final int newWith, final int newHeight) {
+        try {
+            javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    final java.awt.Dimension d = new java.awt.Dimension(newWith, newHeight);
+                    frame.setSize(d);
+                    if( frameLayout ) {
+                        frame.validate();
+                    }
+                } } );
+        } catch( final Throwable throwable ) {
+            throwable.printStackTrace();
+            Assume.assumeNoException( throwable );
+        }
+    }
+
+    @Test
+    public void test01() throws InterruptedException, InvocationTargetException {
+        // System.setProperty("sun.awt.noerasebackground", "true");
+        final GLProfile profile = GLProfile.getDefault();
+        final GLCapabilities glCapabilities = new GLCapabilities(profile);
+        final GLWindow glWindow = GLWindow.create(glCapabilities);
+        final GLEventListener demo = new JOGLQuadNewt();
+        // final GLEventListener demo = new RedSquareES2(1);
+        glWindow.addGLEventListener( demo );
+
+        final SnapshotGLEventListener snap = new SnapshotGLEventListener();
+        snap.setPostSNDetail(getClass().getSimpleName());
+        glWindow.addGLEventListener(snap);
+
+        glWindow.addWindowListener(new com.jogamp.newt.event.WindowAdapter() {
+            @Override
+            public void windowResized(final com.jogamp.newt.event.WindowEvent e) {
+                System.err.println("window resized: "+glWindow.getX()+"/"+glWindow.getY()+" "+glWindow.getSurfaceWidth()+"x"+glWindow.getSurfaceHeight());
+            }
+            @Override
+            public void windowMoved(final com.jogamp.newt.event.WindowEvent e) {
+                System.err.println("window moved:   "+glWindow.getX()+"/"+glWindow.getY()+" "+glWindow.getSurfaceWidth()+"x"+glWindow.getSurfaceHeight());
+            }
+        });
+
+        final NewtCanvasAWT canvas = new NewtCanvasAWT(glWindow);
+        final Frame frame = new Frame(getClass().getSimpleName());
+        final Animator animator = new Animator(glWindow);
+        final boolean[] isClosed = { false };
+
+        frame.add(canvas);
+        frame.setSize(640, 480);
+        // frame.setResizable(false);
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(final java.awt.event.WindowEvent e)
+            {
+                isClosed[0] = true;
+                animator.stop();
+                frame.dispose();
+            }
+        });
+
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                frame.validate();
+                frame.setVisible(true);
+            }
+        });
+        Assert.assertEquals(true,  AWTRobotUtil.waitForVisible(frame, true, null));
+        Assert.assertEquals(true,  NewtTestUtil.waitForRealized(glWindow, true, null));
+
+        animator.start();
+        Assert.assertTrue(animator.isAnimating());
+        Assert.assertTrue(animator.isStarted());
+
+        System.err.println("NW chosen: "+glWindow.getDelegatedWindow().getChosenCapabilities());
+        System.err.println("GL chosen: "+glWindow.getChosenCapabilities());
+        System.err.println("window pos/siz: "+glWindow.getX()+"/"+glWindow.getY()+" "+glWindow.getSurfaceWidth()+"x"+glWindow.getSurfaceHeight()+", "+glWindow.getInsets());
+        System.err.println("XXXX");
+
+        canvas.requestFocus();
+        snap.setMakeSnapshot();
+
+        Thread.sleep(100);
+        setFrameSize(frame, false /* frameLayout */, 800, 600);
+        snap.setMakeSnapshot();
+        Thread.sleep(10);
+        snap.setMakeSnapshot();
+
+        final long t0 = System.currentTimeMillis();
+        long t1 = t0;
+        while(!isClosed[0] && t1-t0<duration) {
+            Thread.sleep(100);
+            t1 = System.currentTimeMillis();
+        }
+
+        if( !isClosed[0] ) {
+            animator.stop();
+            Assert.assertFalse(animator.isAnimating());
+            Assert.assertFalse(animator.isStarted());
+            SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    frame.dispose();
+                }
+            });
+            glWindow.destroy();
+            Assert.assertEquals(true,  NewtTestUtil.waitForRealized(glWindow, false, null));
+        }
+    }
+
+    public static void main(final String[] args)
+    {
+        for(int i=0; i<args.length; i++) {
+            if(args[i].equals("-time")) {
+                i++;
+                duration = MiscUtils.atol(args[i], duration);
+            }
+        }
+        System.err.println("duration "+duration);
+        org.junit.runner.JUnitCore.main(TestBug1431NewtCanvasAWT.class.getName());
+    }
+}

src/test/com/jogamp/opengl/test/junit/jogl/demos/es2/newt/TestGearsES2NewtCanvasAWT.java
@@ -116,6 +116,9 @@ public class TestGearsES2NewtCanvasAWT extends UITestCase {
         if(null == wsize) {
             wsize = new Dimension(640, 480);
         }
+        if(null == rwsize) {
+            rwsize = new Dimension(800, 600);
+        }
     }
 
     @AfterClass