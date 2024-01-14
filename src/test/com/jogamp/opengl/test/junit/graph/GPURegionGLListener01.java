e0c5eed5149a868e202b128c990bb1a4c5259b2f
Sven Göthel <sgothel@jausoft.com>
2024-01-14 04:31:35 +0100

    Bump (c) -2024 for edited files + LICENSE.txt

--- src/test/com/jogamp/opengl/test/junit/graph/GPURegionGLListener01.java
+++ src/test/com/jogamp/opengl/test/junit/graph/GPURegionGLListener01.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010 JogAmp Community. All rights reserved.
+ * Copyright 2010-2024 JogAmp Community. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
