342b4d4426d88f5b70ac1a2446e42750314e6431
Sven Göthel <sgothel@jausoft.com>
2025-07-14 02:40:35 +0200

    Bug 1520: Fix adb-[un]install scripts

--- make/scripts/adb-install-all-armv6.sh
+++ make/scripts/adb-install-all-armv6.sh
@@ -1,4 +1,6 @@
 #adb $* install ../../gluegen/build-android-armv6/jogamp-android-launcher.apk
 #adb $* install ../../gluegen/build-android-armv6/gluegen-rt-android-armv6.apk
+#adb $* install ../../joal/build-android-x86/jar/joal-android-armv6.apk
+#adb $* install ../../joal/build-android-x86/jar/joal-test-android.apk
 adb $* install ../build-android-armv6/jar/jogl-all-android-armv6.apk
 adb $* install ../build-android-armv6/jar/jogl-test-android.apk
---
+++ make/scripts/adb-install-all-x86_64.sh
@@ -0,0 +1,6 @@
+#adb $* install ../../gluegen/build-android-x86_64/jogamp-android-launcher.apk
+#adb $* install ../../gluegen/build-android-x86_64/gluegen-rt-android-x86_64.apk
+#adb $* install ../../joal/build-android-x86_64/jar/joal-android-x86_64.apk
+#adb $* install ../../joal/build-android-x86_64/jar/joal-test-android.apk
+adb $* install ../build-android-x86_64/jar/jogl-all-android-x86_64.apk
+adb $* install ../build-android-x86_64/jar/jogl-demos-android.apk
